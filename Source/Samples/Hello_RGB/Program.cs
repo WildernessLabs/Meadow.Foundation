9f83b31b0cdf23266b318e5287a44fa358bc7fef
bryan costanich <b@wowzer.net>
2018-11-09 18:38:57 -0800

    Redid PWM Port stuff.

---
+++ Source/Samples/Hello_RGB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_RGB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Source/Samples/Hello_RGB/RGBApp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+using Meadow.Foundation;
+using Meadow.Foundation.LEDs;
+
+namespace Hello_RGB
+{
+    public class RGBApp : AppBase<F7Micro, RGBApp>
+    {
+        protected bool _running;
+
+        protected RgbPwmLed _onboardLed;
+
+        public RGBApp()
+        {
+            Console.WriteLine("Got here.");
+
+            // new up our onboard RGB LED
+            _onboardLed = new RgbPwmLed(
+                new PWMPort(Device.Pins.OnboardLEDRed), 
+                new PWMPort(Device.Pins.OnboardLEDGreen), 
+                new PWMPort(Device.Pins.OnboardLEDBlue));
+            //_onboardLed = new RgbPwmLed(Device.Pins.OnboardLEDRed, Device.Pins.OnboardLEDGreen, Device.OnboardLEDBlue);
+        }
+
+        public override void Run()
+        {
+            StartRunningColors();
+        }
+
+        public void StartRunningColors()
+        {
+            _running = true;
+
+            Task stuff = new Task(async () => {
+                while (_running)
+                {
+                    // loop through the entire hue spectrum (360 degrees)
+                    for (int i = 0; i < 360; i++)
+                    {
+                        var hue = ((double)i / 360F);
+                        Debug.Print(hue.ToString());
+
+                        // set the color of the RGB
+                        // TODO TODO TODO TODO: Need to add extension method for this
+                        //_onboardLed.SetColor(Color.FromHsba(((double)i / 360F), 1, 1));
+
+                        // for a fun, fast rotation through the hue spectrum:
+                        //Thread.Sleep (1);
+                        // for a gentle walk through the forest of colors;
+                        Thread.Sleep(18);
+                    }
+                }
+            });
+            stuff.Start();
+        }
+    }
+}
