b4865f007794b4a6c41932dff1d57559db03fe99
bryan costanich <b@wowzer.net>
2018-11-26 17:04:47 -0800

    moved some stuff into core.

--- Source/Meadow.Foundation/Enums/Enums.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Meadow.Foundation
-{
-    /// <summary>
-    /// Indicate the byte ordering for muli-byte words.
-    /// </summary>
-    public enum ByteOrder { LittleEndian, BigEndian };
-}
