08c1cc8b81838b215e9b9c8c2f79bb6869e0e585
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-02-08 23:09:02 -0800

    Add Capacitive and FC28 Soil Moisture Sensors

---
+++ Source/Peripheral_Libs/Sensors.Moisture.Capacitive/Capacitive.cs
@@ -0,0 +1,77 @@
+﻿using Meadow.Hardware;
+using Meadow.Peripherals.Sensors.Moisture;
+using System.Threading.Tasks;
+
+namespace Sensors.Moisture
+{
+    /// <summary>
+    /// Capacitive Soil Moisture Sensor
+    /// 
+    /// Note: this class is not yet implemented.
+    /// </summary>
+    public class Capacitive : IMoistureSensor
+    {
+        #region Properties
+
+        /// <summary>
+        /// Returns the analog input port
+        /// </summary>
+        public AnalogInputPort AnalogPort { get; protected set; }
+
+        /// <summary>
+        /// Last value read from the moisture sensor.
+        /// </summary>
+        public float Moisture { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor is private to prevent it being called.
+        /// </summary>
+        private Capacitive()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a Capacitive soil moisture sensor object with the especified analog pin.
+        /// </summary>
+        /// <param name="analogPort"></param>
+        public Capacitive(IAnalogPin analogPort)
+        {
+            AnalogPort = new AnalogInputPort(analogPort);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns the soil moisture current value.
+        /// </summary>
+        /// <returns>Value ranges from 0 - 100</returns>
+        public async Task<float> Read()
+        {
+            Moisture = await AnalogPort.Read();
+            return 100 - Map(Moisture, 0, 1023, 0, 100);
+        }
+
+        /// <summary>
+        /// Re-maps a value from one range (fromLow - fromHigh) to another (toLow - toHigh).
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="fromLow"></param>
+        /// <param name="fromHigh"></param>
+        /// <param name="toLow"></param>
+        /// <param name="toHigh"></param>
+        /// <returns></returns>
+        protected float Map(float value, float fromLow, float fromHigh, float toLow, float toHigh)
+        {
+            return (((toHigh - toLow) * (value - fromLow)) / (fromHigh - fromLow)) - toLow;
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Peripheral_Libs/Sensors.Moisture.FC28/FC28.cs
@@ -0,0 +1,92 @@
+﻿using Meadow.Hardware;
+using Meadow.Peripherals.Sensors.Moisture;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sensors.Moisture
+{
+    /// <summary>
+    /// FC-28-D Soil Hygrometer Detection Module + Soil Moisture Sensor
+    /// 
+    /// Note: This class is not yet implemented.
+    /// </summary>
+    public class FC28 : IMoistureSensor
+    {
+        #region Properties
+
+        /// <summary>
+        /// Returns the analog input port
+        /// </summary>
+        public AnalogInputPort AnalogPort { get; protected set; }
+
+        /// <summary>
+        /// Returns the digital output port
+        /// </summary>
+        public DigitalOutputPort DigitalPort { get; protected set; }
+
+        /// <summary>
+        /// Last value read from the moisture sensor.
+        /// </summary>
+        public float Moisture { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor is private to prevent it being called.
+        /// </summary>
+        private FC28()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a FC28 soil moisture sensor object with the especified analog pin and digital pin.
+        /// </summary>
+        /// <param name="analogPort"></param>
+        /// <param name="digitalPort"></param>
+        public FC28(IAnalogPin analogPort, IDigitalPin digitalPort)
+        {
+            AnalogPort = new AnalogInputPort(analogPort);
+            DigitalPort = new DigitalOutputPort(digitalPort, false);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns the soil moisture current value.
+        /// </summary>
+        /// <returns>Value ranges from 0 - 100</returns>
+        public async Task<float> Read()
+        {
+            int sample;
+
+            DigitalPort.State = true;
+            Thread.Sleep(5);
+            sample = await AnalogPort.Read();
+            DigitalPort.State = false;
+
+            Moisture = 100 - Map(sample, 0, 1023, 0, 100);
+            return Moisture;
+        }
+
+        /// <summary>
+        /// Re-maps a value from one range (fromLow - fromHigh) to another (toLow - toHigh).
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="fromLow"></param>
+        /// <param name="fromHigh"></param>
+        /// <param name="toLow"></param>
+        /// <param name="toHigh"></param>
+        /// <returns></returns>
+        protected float Map(float value, float fromLow, float fromHigh, float toLow, float toHigh)
+        {
+            return (((toHigh - toLow) * (value - fromLow)) / (fromHigh - fromLow)) - toLow;
+        }
+
+        #endregion
+    }
+}
