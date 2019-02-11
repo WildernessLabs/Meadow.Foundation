703bb8cabf3c709988f1db3f49649f5099951c86
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-02-10 23:17:33 -0800

    Add SegmentedLedBar project sample

---
+++ Source/Samples/LedBarGraph/Program.cs
@@ -0,0 +1,15 @@
+﻿using Meadow;
+
+namespace LedBarGraph
+{
+    class Program
+    {
+        static IApp _app;
+
+        static void Main(string[] args)
+        {
+            // instantiate and run new meadow app
+            _app = new App();
+        }
+    }
+}
