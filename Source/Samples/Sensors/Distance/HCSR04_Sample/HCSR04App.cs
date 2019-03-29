7da3cb72bd92fd4437aa248ccb5caa60002657cf
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-03-28 23:47:28 -0700

    Add HCSR04_Sample project

---
+++ Source/Samples/Sensors/Distance/HCSR04_Sample/HCSR04App.cs
@@ -0,0 +1,32 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Distance;
+using System;
+using System.Threading;
+
+namespace HCSR04_Sample
+{
+    public class HCSR04App : AppBase<F7Micro, HCSR04App>
+    {
+        public HCSR04App()
+        {
+            var HCSR04 = new HCSR04(Device, Device.Pins.D05, Device.Pins.D06);
+            HCSR04.DistanceDetected += HCSR04DistanceDetected; ;
+
+            while (true)
+            {
+                // Sends a trigger signal
+                HCSR04.MeasureDistance();
+                Thread.Sleep(500);
+            }
+
+        }
+
+        // Prints the measured distance after sending a trigger signal
+        // Valid distance ranges from 2cm to 400cm. Prints -1 otherwise.
+        private void HCSR04DistanceDetected(object sender, Meadow.Peripherals.Sensors.Distance.DistanceEventArgs e)
+        {
+            Console.WriteLine(e.Distance.ToString());
+        }
+    }
+}
---
+++ Source/Samples/Sensors/Distance/HCSR04_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace HCSR04_Sample
+{
+    class Program
+    {
+        static IApp app;
+
+        static void Main(string[] args)
+        {
+            app = new HCSR04App();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
