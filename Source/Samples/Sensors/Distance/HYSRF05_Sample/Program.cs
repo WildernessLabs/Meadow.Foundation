fc4aaf0866314d43827b9a525fe746e711662dde
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-03-28 23:58:41 -0700

    Add HYSRF05_Sample project

---
+++ Source/Samples/Sensors/Distance/HYSRF05_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace HYSRF05_Sample
+{
+    class Program
+    {
+        static IApp app;
+
+        static void Main(string[] args)
+        {
+            app = new HYSRF05App();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
