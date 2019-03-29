9305c58166941a1ebf42f041737179f44ceb6aef
adrianstevens <adrian.stevens@gmail.com>
2019-03-29 08:50:12 -0700

    move ParrallaxPir sample to correct folder

---
+++ Source/Samples/Sensors/Motion/ParallaxPir_Sample/ParallaxPirApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Motion;
+using Meadow.Hardware;
+
+namespace ParallaxPir_Sample
+{
+    public class ParallaxPirApp : AppBase<F7Micro, ParallaxPirApp>
+    {
+        ParallaxPir parallaxPir;
+
+        public ParallaxPirApp()
+        {
+            parallaxPir = new ParallaxPir(Device.CreateDigitalInputPort(Device.Pins.D15, InterruptMode.LevelHigh, ResistorMode.PullDown));
+            parallaxPir.OnMotionStart += ParallaxPirOnMotionStart;
+            parallaxPir.OnMotionEnd += ParallaxPirOnMotionEnd;
+        }
+
+        void ParallaxPirOnMotionEnd(object sender)
+        {
+            Console.WriteLine("Motion started");
+
+        }
+
+
+        void ParallaxPirOnMotionStart(object sender)
+        {
+            Console.WriteLine("Motion ended");
+        }
+    }
+}
---
+++ Source/Samples/Sensors/Motion/ParallaxPir_Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Meadow;
+using System;
+using System.Threading;
+
+namespace ParallaxPir_Sample
+{
+    class Program
+    {
+        static IApp app;
+
+        static void Main(string[] args)
+        {
+            app = new ParallaxPirApp();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
