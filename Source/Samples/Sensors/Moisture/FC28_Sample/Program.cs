d82625f1cdde9b30112d9fb68c291947878eb0cc
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-03-29 22:57:36 -0700

    Add FC28_Sample project

---
+++ Source/Samples/Sensors/Moisture/FC28_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace FC28_Sample
+{
+    class Program
+    {
+        static IApp app;
+
+        static void Main(string[] args)
+        {
+            app = new FC28App();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
