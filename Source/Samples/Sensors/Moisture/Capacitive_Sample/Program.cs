f1fc42f5e1943a812ba9d7e3e5605af705c6f463
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-03-29 22:33:55 -0700

    Add Capacitive_Sample project

---
+++ Source/Samples/Sensors/Moisture/Capacitive_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace Capacitive_Sample
+{
+    class Program
+    {
+        static IApp app;
+
+        static void Main(string[] args)
+        {
+            app = new CapacitiveApp();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
