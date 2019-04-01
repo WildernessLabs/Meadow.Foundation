ce1d3555847e1023cac3f2bdd6b1d48ff68ba4b8
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-03-31 19:43:12 -0700

    Small update for LedBarGraph_Sample using math.Min and math.Max for percentage iterations

--- Source/Samples/Leds/LedBarGraph_Sample/LedBarGraphApp.cs
+++ Source/Samples/Leds/LedBarGraph_Sample/LedBarGraphApp.cs
@@ -20,8 +20,8 @@ namespace LedBarGraph_Sample
             ports[3] = Device.CreateDigitalOutputPort(Device.Pins.D09);
             ports[4] = Device.CreateDigitalOutputPort(Device.Pins.D10);
             ports[5] = Device.CreateDigitalOutputPort(Device.Pins.D11);
-            ports[6] = Device.CreateDigitalOutputPort(Device.Pins.D12);
-            ports[7] = Device.CreateDigitalOutputPort(Device.Pins.D13);
+            ports[6] = Device.CreateDigitalOutputPort(Device.Pins.D01);
+            ports[7] = Device.CreateDigitalOutputPort(Device.Pins.D00);
             ports[8] = Device.CreateDigitalOutputPort(Device.Pins.D14);
             ports[9] = Device.CreateDigitalOutputPort(Device.Pins.D15);
             ledBarGraph = new LedBarGraph(ports);
@@ -31,6 +31,8 @@ namespace LedBarGraph_Sample
 
         protected void TestLedBarGraph()
         {
+            float percentage = 0;
+
             while (true)
             {
                 Console.WriteLine("Turning them on using SetLed...");
@@ -49,24 +51,22 @@ namespace LedBarGraph_Sample
                     Thread.Sleep(300);
                 }
 
-                float percentage = 0;
                 Console.WriteLine("Turning them on using Percentage...");
-                while (percentage < 1)
+                while (percentage <= 1)
                 {
-                    ledBarGraph.Percentage = percentage;
                     percentage += 0.10f;
-                    Thread.Sleep(200);
+                    ledBarGraph.Percentage = Math.Min(1.0f, percentage);                    
+                    Thread.Sleep(100);
                 }
 
                 Thread.Sleep(1000);
 
-                percentage = 1.0f;
                 Console.WriteLine("Turning them off using Percentage...");
-                while (percentage > 0)
+                while (percentage >= 0)
                 {
-                    ledBarGraph.Percentage = percentage;
                     percentage -= 0.10f;
-                    Thread.Sleep(200);
+                    ledBarGraph.Percentage = Math.Max(0.0f, percentage); ;                    
+                    Thread.Sleep(100);
                 }
 
                 Thread.Sleep(1000);
