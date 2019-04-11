8d905d130e55259c8330717f047ce2ccdaa4100b
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-04-11 00:03:34 -0700

    Update CharacterDisplay_Sample constructor to match Fritzing diagram of the Docs

--- Source/Samples/Displays/CharacterDisplay_Sample/CharacterDisplayApp.cs
+++ Source/Samples/Displays/CharacterDisplay_Sample/CharacterDisplayApp.cs
@@ -10,9 +10,16 @@ namespace CharacterDisplay_Sample
 
         public CharacterDisplayApp()
         {
-            display = new CharacterDisplay(Device, Device.Pins.D05, Device.Pins.D07,
-                Device.Pins.D11, Device.Pins.D12, Device.Pins.D13, Device.Pins.D14,
-                16, 2);
+            display = new CharacterDisplay
+            (
+                device: Device, 
+                pinRS: Device.Pins.D05,
+                pinE: Device.Pins.D07,
+                pinD4: Device.Pins.D08, 
+                pinD5: Device.Pins.D09, 
+                pinD6: Device.Pins.D10, 
+                pinD7: Device.Pins.D11                
+            );
 
             int count = 0;
             display.WriteLine("CharacterDisplay", 0);
