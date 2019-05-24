ec84d6a3fce9f85aa36979fecf655a1d2082b8bd
adrianstevens <adrian.stevens@gmail.com>
2019-05-23 21:21:11 -0700

    Add FeatherWing JoyWing driver (WIP)

---
+++ Source/Samples/FeatherWings/JoyWing_Sample/JoyWingApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.FeatherWings;
+
+namespace JoyWing_Sample
+{
+    public class JoyWingApp : App<F7Micro, JoyWingApp>
+    {
+        JoyWing joyWing;
+
+        public JoyWingApp()
+        {
+            joyWing = new JoyWing(Device, Device.Pins.D15, Device.Pins.D14, 
+                                    Device.Pins.D13, Device.Pins.D12, Device.Pins.D11, 
+                                    Device.Pins.D10, Device.Pins.D09);
+
+            joyWing.OnA += (sender, e) => Console.WriteLine("A");
+            joyWing.OnB += (sender, e) => Console.WriteLine("B");
+            joyWing.OnX += (sender, e) => Console.WriteLine("X");
+            joyWing.OnY += (sender, e) => Console.WriteLine("Y");
+            joyWing.OnSelect += (sender, e) => Console.WriteLine("Select");
+        }
+    }
+}
---
+++ Source/Samples/FeatherWings/JoyWing_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace JoyWing_Sample
+{
+    class Program
+    {
+        static IApp app;
+
+        static void Main(string[] args)
+        {
+            app = new JoyWingApp();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
