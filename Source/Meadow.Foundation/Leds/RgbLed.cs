7258600b5670e0627053165be411706c8743bec6
adrianstevens <adrian.stevens@gmail.com>
2019-05-30 20:39:52 -0700

    Update RGBLed to use a color enum

--- Source/Meadow.Foundation/Leds/RgbLed.cs
+++ Source/Meadow.Foundation/Leds/RgbLed.cs
@@ -1,15 +1,26 @@
 ﻿using Meadow.Hardware;
-using System.Drawing;
 using System.Threading.Tasks;
 
 namespace Meadow.Foundation.Leds
 {
     public class RgbLed
     {
+        public enum Colors
+        {
+            Red,
+            Green,
+            Blue,
+            Yellow,
+            Magenta,
+            Cyan,
+            White,
+            Black,
+        }
+
         protected Task _animationTask = null;
         protected bool _running = false;
 
-        public Color Color { get; protected set; }
+        public Colors Color { get; protected set; }
 
         public IDigitalOutputPort RedPort { get; set; }
 
@@ -53,42 +64,66 @@ namespace Meadow.Foundation.Leds
         public void TurnOff()
         {
             _running = false;
-            SetColor(Color.Black);
+            SetColor(Colors.Black);
         }
 
         public void TurnOn()
         {
-            SetColor(Color);
+            SetColor(Colors.White);
         }
 
-        public void SetColor(Color color)
+        public void SetColor(Colors color)
         {
-            if (color != Color.Red && color != Color.Green && color != Color.Blue &&
-                color != Color.Magenta && color != Color.Cyan && color != Color.Yellow &&
-                color != Color.White && color != Color.Black)
+            switch (color)
             {
-                return;
-            }
-
-            Color = color;
-
-            RedPort.State = color.R > 0;
-            GreenPort.State = color.G > 0;
-            BluePort.State = color.B > 0;
-
-            if (!IsCommonCathode)
-            {
-                RedPort.State = !RedPort.State;
-                GreenPort.State = !GreenPort.State;
-                BluePort.State = !BluePort.State;
+                case Colors.Red:
+                    RedPort.State = !IsCommonCathode;
+                    GreenPort.State = IsCommonCathode;
+                    BluePort.State = IsCommonCathode;
+                    break;
+                case Colors.Green:
+                    RedPort.State = IsCommonCathode;
+                    GreenPort.State = !IsCommonCathode;
+                    BluePort.State = IsCommonCathode;
+                    break;
+                case Colors.Blue:
+                    RedPort.State = !IsCommonCathode;
+                    GreenPort.State = IsCommonCathode;
+                    BluePort.State = IsCommonCathode;
+                    break;
+                case Colors.Yellow:
+                    RedPort.State = !IsCommonCathode;
+                    GreenPort.State = !IsCommonCathode;
+                    BluePort.State = IsCommonCathode;
+                    break;
+                case Colors.Magenta:
+                    RedPort.State = !IsCommonCathode;
+                    GreenPort.State = IsCommonCathode;
+                    BluePort.State = !IsCommonCathode;
+                    break;
+                case Colors.Cyan:
+                    RedPort.State = IsCommonCathode;
+                    GreenPort.State = !IsCommonCathode;
+                    BluePort.State = !IsCommonCathode;
+                    break;
+                case Colors.White:
+                    RedPort.State = !IsCommonCathode;
+                    GreenPort.State = !IsCommonCathode;
+                    BluePort.State = !IsCommonCathode;
+                    break;
+                case Colors.Black:
+                    RedPort.State = IsCommonCathode;
+                    GreenPort.State = IsCommonCathode;
+                    BluePort.State = IsCommonCathode;
+                    break;
             }
         }
 
-        public void StartBlink(Color color, uint onDuration = 200, uint offDuration = 200)
+        public void StartBlink(Colors color, uint onDuration = 200, uint offDuration = 200)
         {
             _running = true;
 
-            SetColor(Color.Black);
+            SetColor(Colors.Black);
             //TODO: Make this cancellable via Cancellation token
             _animationTask = new Task(async () =>
             {
@@ -96,7 +131,7 @@ namespace Meadow.Foundation.Leds
                 {
                     SetColor(color);
                     await Task.Delay((int)onDuration);
-                    SetColor(Color.Black);
+                    SetColor(Colors.Black);
                     await Task.Delay((int)offDuration);
                 }
             });
