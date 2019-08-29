1d3bf3f0aeaef7516fcf0d2d5125a3d298a64124
ctacke <ctacke@opennetcf.com>
2019-08-29 12:35:01 -0500

    Added DS1307 driver and sample

---
+++ Source/Samples/RTCs/DS1307_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace DS1307_Sample
+{
+    class Program
+    {
+        static IApp app;
+
+        static void Main(string[] args)
+        {
+            app = new DS1307App();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
