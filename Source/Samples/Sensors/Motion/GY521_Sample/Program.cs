705ba4ea43ef8b76865931b824b3b5fa2caafc8c
ctacke <ctacke@opennetcf.com>
2019-08-29 12:26:34 -0500

    Added GY521 driver and sample

---
+++ Source/Samples/Sensors/Motion/GY521_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace GY521_Sample
+{
+    class Program
+    {
+        static IApp app;
+
+        static void Main(string[] args)
+        {
+            app = new GY521App();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
