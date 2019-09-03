51093f6c822c6eb62826e2e930b84c4552be4c64
ctacke <ctacke@opennetcf.com>
2019-09-03 10:47:47 -0500

    BME280 clean-up

--- Source/Peripheral_Libs/Sensors.Atmospheric.BME280/BME280SPI.cs
+++ Source/Peripheral_Libs/Sensors.Atmospheric.BME280/BME280SPI.cs
@@ -17,18 +17,14 @@ namespace Meadow.Foundation.Sensors.Atmospheric
 
         public override byte[] ReadRegisters(byte startRegister, int readCount)
         {
-            //            _spi.SendData(null, startRegister);
-            //            var rx = _spi.ReceiveData(null, readCount);
-            //            return rx;
-
+            // the buffer needs to be big enough for the output and response
             var buffer = new byte[readCount + 1];
             buffer[0] = startRegister;
 
             var rx = _spi.ExchangeData(_chipSelect, buffer);
 
+            // skip past the byte where we clocked out the register address
             var registerData = rx.Skip(1).Take(readCount).ToArray();
-
-            Console.WriteLine($" BME Register Data {BitConverter.ToString(registerData)}");
 
             return registerData;
         }
