306119b5a7fc5476564c3db781bab8aa2568d400
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-09-04 23:48:31 -0700

    Add Capacitive Soil Moisture sample project

---
+++ Source/Samples/Moisture/Capacitive_Sample/CapacitiveApp.cs
@@ -0,0 +1,29 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Moisture;
+using System;
+using System.Threading;
+
+namespace Capacitive_Sample
+{
+    public class CapacitiveApp : App<F7Micro, CapacitiveApp>
+    {
+        Capacitive capacitive;
+
+        public CapacitiveApp()
+        {
+            capacitive = new Capacitive(Device.CreateAnalogInputPort(Device.Pins.A00));
+
+            TestCapacitive();
+        }
+
+        protected void TestCapacitive()
+        {
+            while(true)
+            {
+                Console.WriteLine("Humidity:" + capacitive.Read());
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
