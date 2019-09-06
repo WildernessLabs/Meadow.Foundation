2ac7764af167d09f4aff3fcd1791c625f633ba4d
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-09-05 23:17:56 -0700

    Update LED TypicalForwardVoltage

--- Source/Meadow.Foundation/Leds/TypicalForwardVoltage.cs
+++ Source/Meadow.Foundation/Leds/TypicalForwardVoltage.cs
@@ -5,11 +5,11 @@ namespace Meadow.Foundation.Leds
     /// </summary>
     public static class TypicalForwardVoltage
     {
-        public const float Red = 1.8F;
-        public const float Green = 2.1F;
-        public const float Blue = 3.5F;
-        public const float Yellow = 3.5F;
-        public const float White = 2.15F;
+        public const float Red = 1.7F;
+        public const float Green = 2.2F;
+        public const float Blue = 3.2F;
+        public const float Yellow = 2.1F;
+        public const float White = 3.2F;
 
         public const float ResistorLimited = 3.3F;
     }
