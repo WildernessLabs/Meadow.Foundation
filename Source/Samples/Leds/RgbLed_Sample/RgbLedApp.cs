69763d615ce6c2b5d813ad8c95dd3f3510109064
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-09-06 00:18:59 -0700

    Update RgbLed_Sample

--- Source/Samples/Leds/RgbLed_Sample/RgbLedApp.cs
+++ Source/Samples/Leds/RgbLed_Sample/RgbLedApp.cs
@@ -2,46 +2,71 @@
 using Meadow.Devices;
 using Meadow.Foundation.Leds;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace RgbLed_Sample
 {
     class RgbLedApp : App<F7Micro, RgbLedApp>
     {
-        RgbLed rgbLed;
+        List<RgbLed> rgbLeds;
 
         public RgbLedApp()
         {
-            rgbLed = new RgbLed(
-                Device.CreateDigitalOutputPort(Device.Pins.D11),
-                Device.CreateDigitalOutputPort(Device.Pins.D05),
-                Device.CreateDigitalOutputPort(Device.Pins.D02));
+            Console.WriteLine("Initializing...");
 
-            TestRgbLed();
+            rgbLeds = new List<RgbLed>();
+            rgbLeds.Add(new RgbLed(
+                Device.CreateDigitalOutputPort(Device.Pins.D02),
+                Device.CreateDigitalOutputPort(Device.Pins.D03),
+                Device.CreateDigitalOutputPort(Device.Pins.D04))
+            );
+            rgbLeds.Add(new RgbLed(
+                Device.CreateDigitalOutputPort(Device.Pins.D05),
+                Device.CreateDigitalOutputPort(Device.Pins.D06),
+                Device.CreateDigitalOutputPort(Device.Pins.D07))
+            );
+            rgbLeds.Add(new RgbLed(
+                Device.CreateDigitalOutputPort(Device.Pins.D08),
+                Device.CreateDigitalOutputPort(Device.Pins.D09),
+                Device.CreateDigitalOutputPort(Device.Pins.D10))
+            );
+            rgbLeds.Add(new RgbLed(
+                Device.CreateDigitalOutputPort(Device.Pins.D11),
+                Device.CreateDigitalOutputPort(Device.Pins.D12),
+                Device.CreateDigitalOutputPort(Device.Pins.D13))
+            );
+
+            TestRgbLeds();
         }
 
-        protected void TestRgbLed()
+        protected void TestRgbLeds()
         {
+            Console.WriteLine("TestRgbLeds...");
+
             while (true)
             {
-                //for (int i = 0; i < (int)RgbLed.Colors.count; i++)
-                //{
-                //    rgbLed.SetColor((RgbLed.Colors)i);
-                //    Console.WriteLine(((RgbLed.Colors)i).ToString());
-                //    Thread.Sleep(1000);
-                //}
+                Console.WriteLine("Going through each color on each RGB LED...");
+                foreach (var rgbLed in rgbLeds)
+                {
+                    for (int i = 0; i < (int)RgbLed.Colors.count; i++)
+                    {
+                        rgbLed.SetColor((RgbLed.Colors)i);
+                        Thread.Sleep(500);
+                    }
+                    rgbLed.Stop();
+                }
 
-                rgbLed.SetColor(RgbLed.Colors.Red);
-                Console.WriteLine("Red");
-                Thread.Sleep(1000);
-
-                rgbLed.SetColor(RgbLed.Colors.Green);
-                Console.WriteLine("Green");
-                Thread.Sleep(1000);
-
-                rgbLed.SetColor(RgbLed.Colors.Blue);
-                Console.WriteLine("Blue");
-                Thread.Sleep(1000);
+                Console.WriteLine("Blinking through each color on each RGB LED...");
+                foreach (var rgbLed in rgbLeds)
+                {
+                    for (int i = 0; i < (int)RgbLed.Colors.count; i++)
+                    {
+                        rgbLed.StartBlink((RgbLed.Colors)i);
+                        Thread.Sleep(3000);
+                    }
+                    rgbLed.Stop();
+                }
             }
         }
     }
