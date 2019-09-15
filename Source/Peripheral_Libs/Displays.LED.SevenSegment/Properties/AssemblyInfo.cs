f534f2b6f8a03ecd542fe59905ba4fcaad671923
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-09-15 12:13:34 -0700

    Meadow.Foundation.Displays.LED.SevenSegment Version 0.5.0.0

--- Source/Peripheral_Libs/Displays.LED.SevenSegment/Properties/AssemblyInfo.cs
+++ Source/Peripheral_Libs/Displays.LED.SevenSegment/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Displays.LED.SevenSegment")]
+[assembly: AssemblyTitle("Meadow.Foundation.Displays.LED.SevenSegment")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
