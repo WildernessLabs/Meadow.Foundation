a44c17567fe90b9fe7b3c588aa094752a65ff5f3
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-09-15 19:33:05 -0700

    Meadow.Foundation.Displays.GraphicsLibrary Version 0.5.0.0

--- Source/Peripheral_Libs/Displays.GraphicsLibrary/Properties/AssemblyInfo.cs
+++ Source/Peripheral_Libs/Displays.GraphicsLibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Displays.GraphicsLibrary")]
+[assembly: AssemblyTitle("Meadow.Foundation.Displays.GraphicsLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Displays.GraphicsLibrary")]
+[assembly: AssemblyProduct("Meadow.Foundation.Displays.GraphicsLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
