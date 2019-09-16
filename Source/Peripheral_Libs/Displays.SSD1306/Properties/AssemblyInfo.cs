88f7e1ca75fd157539a65fef9a2f790d230b7f0b
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-09-15 19:31:06 -0700

    Meadow.Foundation.Displays.SSD1306 Version 0.5.0.0

--- Source/Peripheral_Libs/Displays.SSD1306/Properties/AssemblyInfo.cs
+++ Source/Peripheral_Libs/Displays.SSD1306/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Displays.SSD1306")]
+[assembly: AssemblyTitle("Meadow.Foundation.Displays.SSD1306")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Displays.SSD1306")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Meadow.Foundation.Displays.SSD1306")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
