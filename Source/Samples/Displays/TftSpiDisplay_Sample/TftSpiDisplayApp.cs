f542f0b2037ee0ab127a83c4101a8dd871c3b58f
Adrian Stevens <adrian.stevens@gmail.com>
2019-09-15 18:11:47 -0700

    Update TftSpiDisplay sample to new project type and is now using GraphicsLibrary

--- Source/Samples/Displays/TftSpiDisplay_Sample/TftSpiDisplayApp.cs
+++ Source/Samples/Displays/TftSpiDisplay_Sample/TftSpiDisplayApp.cs
@@ -2,13 +2,14 @@
 using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation.Displays.Tft;
+using Meadow.Foundation.Graphics;
 using Meadow.Hardware;
 
 namespace TftSpiDisplay_Sample
 {
     public class TftSpiDisplayApp : App<F7Micro, TftSpiDisplayApp>
     {
-        SSD1351 display;
+        SSD1331 display;
         ISpiBus spiBus;
 
         public TftSpiDisplayApp()
@@ -16,18 +17,31 @@ namespace TftSpiDisplay_Sample
             Console.WriteLine("TftSpi sample");
             Console.WriteLine("Create Spi bus");
 
-            spiBus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, 2000);
+            spiBus = Device.CreateSpiBus();// Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, 2000);
             
 
             Console.WriteLine("Create SSD1351 driver instance");
-            display = new SSD1351(device: Device, spiBus: spiBus,
+            display = new SSD1331(device: Device, spiBus: spiBus,
                 chipSelectPin: Device.Pins.D02,
                 dcPin: Device.Pins.D01,
                 resetPin: Device.Pins.D00,
-                width: 128, height: 128);
-                
+                width: 96, height: 64);
 
-            Console.WriteLine("Clear display");
+            var graphicsLib = new GraphicsLibrary(display);
+            graphicsLib.CurrentFont = new Font8x12();
+
+            graphicsLib.Clear();
+
+            graphicsLib.DrawTriangle(10, 10, 50, 50, 10, 50, Meadow.Foundation.Color.Red);
+
+            graphicsLib.DrawRectangle(20, 15, 40, 20, Meadow.Foundation.Color.Yellow, true);
+
+            graphicsLib.DrawText(5, 5, "Meadow F7 SPI");
+
+            graphicsLib.Show();
+
+
+            /*Console.WriteLine("Clear display");
             display.ClearScreen(250);
             display.Refresh();
 
@@ -39,7 +53,7 @@ namespace TftSpiDisplay_Sample
                 display.DrawPixel(60 + i, i, true);
             }
 
-            display.Show();
+            display.Show(); */
         }
     }
 }
