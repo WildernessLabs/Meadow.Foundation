77f1f7f6cd59963930bfd178859d79ca61edb5ec
Adrian Stevens <adrian.stevens@gmail.com>
2019-09-19 19:48:10 -0700

    Add TEA5767 sample

---
+++ Source/Samples/Audio/TEA5767_Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using Meadow;
+
+namespace BasicTEA5767
+{
+    class Program
+    {
+        static IApp app;
+        public static void Main(string[] args)
+        {
+            // instantiate and run new meadow app
+            app = new TEA5767App();
+        }
+    }
+}
---
+++ Source/Samples/Audio/TEA5767_Sample/TEA5767App.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Audio.Radio;
+using Meadow.Hardware;
+
+namespace BasicTEA5767
+{
+    public class TEA5767App : App<F7Micro, TEA5767App>
+    {
+        public TEA5767App()
+        {
+            Console.WriteLine("Initialize App");
+
+            Console.WriteLine("Create I2C bus");
+            var i2cBus = Device.CreateI2cBus();
+
+            Console.WriteLine("Create TEA5767 instance");
+            var radio = new TEA5767(i2cBus);
+            
+
+            Console.WriteLine($"Current frequency: {radio.GetFrequency()}");
+
+            for (int i = 0; i < 8; i++)
+            {
+                Thread.Sleep(1000);
+
+                radio.SearchNextSilent();
+
+                Console.WriteLine($"Current frequency: {radio.GetFrequency()}");
+            }
+
+            radio.SelectFrequency(94.5f);
+        }
+    }
+}
