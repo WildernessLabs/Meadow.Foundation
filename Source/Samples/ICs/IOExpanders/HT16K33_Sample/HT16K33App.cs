f70ade1078d363440248fb7c2c94e00d1358abc7
adrianstevens <adrian.stevens@gmail.com>
2019-09-19 18:35:17 -0700

    Add HT16K33 sample app

---
+++ Source/Samples/ICs/IOExpanders/HT16K33_Sample/HT16K33App.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+using Meadow.Foundation.ICs.IOExpanders;
+
+namespace BasicHT16K33_Sample
+{
+    public class HT16K33App : App<F7Micro, HT16K33App>
+    {
+        HT16K33 ht16k33;
+
+        public HT16K33App()
+        {
+            var i2cBus = Device.CreateI2cBus();
+
+            ht16k33 = new HT16K33(i2cBus);
+
+            int index = 0;
+            bool on = true;
+
+            // write your code here
+            while (true)
+            {
+                ht16k33.ToggleLed((byte)index, on);
+                ht16k33.UpdateDisplay();
+                index++;
+
+                if(index >= 128)
+                {
+                    index = 0;
+                    on = !on;
+                }
+
+                Thread.Sleep(100);
+            }
+
+
+
+
+        }
+    }
+
+}
---
+++ Source/Samples/ICs/IOExpanders/HT16K33_Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using Meadow;
+
+namespace BasicHT16K33_Sample
+{
+    class Program
+    {
+        static IApp app;
+        public static void Main(string[] args)
+        {
+            // instantiate and run new meadow app
+            app = new HT16K33App();
+        }
+    }
+}
