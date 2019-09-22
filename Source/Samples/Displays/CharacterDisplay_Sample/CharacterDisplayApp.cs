4a2b75074fb532c344c0a27db2c77adafd6a1cc1
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-09-21 23:37:59 -0700

    Update CharacterDisplay_Sample project referencing NuGet packages

--- Source/Samples/Displays/CharacterDisplay_Sample/CharacterDisplayApp.cs
+++ Source/Samples/Displays/CharacterDisplay_Sample/CharacterDisplayApp.cs
@@ -13,12 +13,12 @@ namespace CharacterDisplay_Sample
             display = new CharacterDisplay
             (
                 device: Device, 
-                pinRS: Device.Pins.D05,
-                pinE: Device.Pins.D07,
-                pinD4: Device.Pins.D08, 
-                pinD5: Device.Pins.D09, 
-                pinD6: Device.Pins.D10, 
-                pinD7: Device.Pins.D11                
+                pinRS: Device.Pins.D15,
+                pinE: Device.Pins.D14,
+                pinD4: Device.Pins.D13, 
+                pinD5: Device.Pins.D12, 
+                pinD6: Device.Pins.D11, 
+                pinD7: Device.Pins.D10
             );
 
             int count = 0;
