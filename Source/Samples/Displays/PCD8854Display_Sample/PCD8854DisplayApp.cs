3553faf6ee251871548bb9b82bb5a8c062b0cfa5
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-10-10 11:20:35 -0700

    Update PCD8864_Sample and HBridgeMotor_Sample

--- Source/Samples/Displays/PCD8854Display_Sample/PCD8854DisplayApp.cs
+++ Source/Samples/Displays/PCD8854Display_Sample/PCD8854DisplayApp.cs
@@ -1,36 +1,51 @@
-﻿using System;
-using System.Threading;
-using Meadow;
+﻿using Meadow;
 using Meadow.Devices;
+using Meadow.Foundation.Displays;
 using Meadow.Foundation.Graphics;
-using Meadow.Hardware;
+using System;
+using System.Threading;
 
 namespace BasicPCD8843Display_Sample
 {
     public class PCD8854DisplayApp : App<F7Micro, PCD8854DisplayApp>
     {
 
-        Meadow.Foundation.Displays.PCD8544 display;
-        Meadow.Foundation.Graphics.GraphicsLibrary graphics;
+        PCD8544 display;
+        GraphicsLibrary graphics;
 
         public PCD8854DisplayApp()
         {
             var spiBus = Device.CreateSpiBus();
 
-            display = new Meadow.Foundation.Displays.PCD8544(Device, spiBus,
-                Device.Pins.D01, Device.Pins.D00, Device.Pins.D02);
+            display = new PCD8544
+            (
+                device: Device, 
+                spiBus: spiBus,
+                chipSelectPin: Device.Pins.D01, 
+                dcPin: Device.Pins.D00, 
+                resetPin: Device.Pins.D02
+            );
+            Console.WriteLine("1");
 
             for (int i = 0; i < 35; i++)
             {
                 display.DrawPixel(i, i, true);
             }
 
-            graphics = new Meadow.Foundation.Graphics.GraphicsLibrary(display);
+            Console.WriteLine("2");
 
-            graphics.Clear();
+            graphics = new GraphicsLibrary(display);
+
+            Console.WriteLine("3");
+
+            graphics.Clear(true);
             graphics.CurrentFont = new Font8x12();
             graphics.DrawText(0, 0, "Meadow F7");
             graphics.DrawRectangle(5, 14, 30, 10, true);
+
+            Console.WriteLine("4");
+
+            Thread.Sleep(-1);
 
             /*
             graphics = new Meadow.Foundation.Graphics.GraphicsLibrary(display);
