1bbb9a09664817f050fbbf140e18072dba1d482a
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-10-18 16:24:30 -0700

    Relocate JoyWing_Sample project

--- Source/Samples/External_Peripherals_Samples/FeatherWings.JoyWing_Sample/MeadowApp.cs
+++ Source/Samples/External_Peripherals_Samples/FeatherWings.JoyWing_Sample/MeadowApp.cs
@@ -3,16 +3,16 @@ using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation.FeatherWings;
 
-namespace JoyWing_Sample
+namespace FeatherWings.JoyWing_Sample
 {
-    public class JoyWingApp : App<F7Micro, JoyWingApp>
+    public class MeadowApp : App<F7Micro, MeadowApp>
     {
         JoyWing joyWing;
 
-        public JoyWingApp()
+        public MeadowApp()
         {
-            joyWing = new JoyWing(Device, Device.Pins.D15, Device.Pins.D14, 
-                                    Device.Pins.D13, Device.Pins.D12, Device.Pins.D11, 
+            joyWing = new JoyWing(Device, Device.Pins.D15, Device.Pins.D14,
+                                    Device.Pins.D13, Device.Pins.D12, Device.Pins.D11,
                                     Device.Pins.D10, Device.Pins.D09);
 
             joyWing.OnA += (sender, e) => Console.WriteLine("A");
