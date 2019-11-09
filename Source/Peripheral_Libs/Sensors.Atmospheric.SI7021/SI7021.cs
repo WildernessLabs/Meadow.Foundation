8a6a6294ee58db7186b7dde61d7f6bb608be1981
Bryan Costanich <b@wowzer.net>
2019-11-09 11:24:21 -0800

    Updated Si7021 to new pattern. it still doesn't work though. waiting on tacke's changes to get meaningful errs

--- Source/Peripheral_Libs/Sensors.Atmospheric.SI7021/SI7021.cs
+++ Source/Peripheral_Libs/Sensors.Atmospheric.SI7021/SI7021.cs
@@ -1,148 +1,69 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Meadow.Hardware;
 using Meadow.Peripherals.Sensors;
 using Meadow.Peripherals.Sensors.Atmospheric;
-using Meadow.Peripherals.Temperature;
+using Meadow.Peripherals.Sensors.Temperature;
 
 namespace Meadow.Foundation.Sensors.Atmospheric
 {
     /// <summary>
     /// Provide access to the SI7021 temperature and humidity sensor.
     /// </summary>
-    public class SI7021 : ITemperatureSensor, IHumiditySensor
+    // TODO: shouldn't this be Si70xx? Since it supports the Si7013, 7020, and 7021?
+    public class SI7021 :
+        FilterableObservableBase<AtmosphericConditionChangeResult, AtmosphericConditions>,
+        ICompositeAtmosphericSensor
     {
-        #region Constants
-
         /// <summary>
-        ///     Minimum value that should be used for the polling frequency.
         /// </summary>
-        public const ushort MinimumPollingPeriod = 100;
-
-        #endregion Constants
-
-        #region Enums
-
-        /// <summary>
-        ///     Specific device type / model.
-        /// </summary>
-        public enum DeviceType
-        {
-            Unknown = 0x00,
-            Si7013 = 0x0d,
-            Si7020 = 0x14,
-            Si7021 = 0x15,
-            EngineeringSample = 0xff
-        }
-
-        #endregion Enums
-
-        #region Classes / structures
-
-        /// <summary>
-        ///     Device registers.
-        /// </summary>
-        private static class Registers
-        {
-            public static readonly byte MeasureHumidityWithHold = 0xe5;
-            public static readonly byte MeasureHumidityNoHold = 0xf5;
-            public static readonly byte MeasureTemperatureWithHold = 0xe3;
-            public static readonly byte MeasureTemperatureNoHold = 0xf3;
-            public static readonly byte ReadPreviousTemperatureMeasurement = 0xe0;
-            public static readonly byte Reset = 0xfe;
-            public static readonly byte WriteUserRegister1 = 0xe6;
-            public static readonly byte ReadUserRegister1 = 0xe7;
-            public static readonly byte ReadIDFirstBytePart1 = 0xfa;
-            public static readonly byte ReadIDFirstBytePart2 = 0x0f;
-            public static readonly byte ReadIDSecondBytePart1 = 0xfc;
-            public static readonly byte ReadIDSecondBytePart2 = 0xc9;
-            public static readonly byte ReadFirmwareRevisionPart1 = 0x84;
-            public static readonly byte ReadFirmwareRevisionPart2 = 0xb8;
-        }
-
-        #endregion Classes / Structures
-
-        #region Member variables / fields
+        public event EventHandler<AtmosphericConditionChangeResult> Updated = delegate { };
 
         /// <summary>
         ///     SI7021 is an I2C device.
         /// </summary>
-        private readonly II2cPeripheral _si7021;
+        protected readonly II2cPeripheral _si7021;
 
         /// <summary>
         ///     Update interval in milliseconds
         /// </summary>
         private readonly ushort _updateInterval = 100;
 
-        #endregion Member variables / fields
+        // internal thread lock
+        private object _lock = new object();
+        private CancellationTokenSource SamplingTokenSource;
 
         #region Properties
 
         /// <summary>
-        ///     Get the last humidity reading from the sensor.
+        /// Gets a value indicating whether the sensor is currently in a sampling
+        /// loop. Call StartSampling() to spin up the sampling process.
         /// </summary>
-        /// <remarks>
-        ///     The Update method should be called before the data in this property
-        ///     contains valid data.
-        /// </remarks>
-        public float Humidity
-        {
-            get { return _humidity; }
-            private set
-            {
-                _humidity = value;
-                //
-                //  Check to see if the change merits raising an event.
-                //
-                if ((_updateInterval > 0) && (Math.Abs(_lastNotifiedHumidity - value) >= HumidityChangeNotificationThreshold))
-                {
-                    HumidityChanged(this, new SensorFloatEventArgs(_lastNotifiedHumidity, value));
-                    _lastNotifiedHumidity = value;
-                }
+        /// <value><c>true</c> if sampling; otherwise, <c>false</c>.</value>
+        public bool IsSampling { get; protected set; } = false;
+
+        /// <summary>
+        /// The AtmosphericConditions from the last reading.
+        /// </summary>
+        public AtmosphericConditions Conditions { get; protected set; } = new AtmosphericConditions();
+
+        /// <summary>
+        /// The temperature, in degrees celsius (ºC), from the last reading.
+        /// </summary>
+        public float Temperature {
+            get {
+                return Conditions.Temperature;
             }
         }
-        private float _humidity;
-        private float _lastNotifiedHumidity = 0.0F;
-
         /// <summary>
-        ///     Get the last temperature reading.
+        /// The humidity, in percent relative humidity, from the last reading..
         /// </summary>
-        /// <remarks>
-        ///     The Update method should be called before the data in this property
-        ///     contains valid data.
-        /// </remarks>
-        public float Temperature
-        {
-            get { return _temperature; }
-            private set
-            {
-                _temperature = value;
-                //
-                //  Check to see if the change merits raising an event.
-                //
-                if ((_updateInterval > 0) && (Math.Abs(_lastNotifiedTemperature - value) >= TemperatureChangeNotificationThreshold))
-                {
-                    TemperatureChanged(this, new SensorFloatEventArgs(_lastNotifiedTemperature, value));
-                    _lastNotifiedTemperature = value;
-                }
+        public float Humidity {
+            get {
+                return Conditions.Humidity;
             }
         }
-        private float _temperature;
-        private float _lastNotifiedTemperature = 0.0F;
-
-        /// <summary>
-        ///     Any changes in the temperature that are greater than the temperature
-        ///     threshold will cause an event to be raised when the instance is
-        ///     set to update automatically.
-        /// </summary>
-        public float TemperatureChangeNotificationThreshold { get; set; } = 0.001F;
-
-        /// <summary>
-        ///     Any changes in the humidity that are greater than the humidity
-        ///     threshold will cause an event to be raised when the instance is
-        ///     set to update automatically.
-        /// </summary>
-        public float HumidityChangeNotificationThreshold { get; set; } = 0.001F;
 
         /// <summary>
         ///     Serial number of the device.
@@ -187,22 +108,6 @@ namespace Meadow.Foundation.Sensors.Atmospheric
 
         #endregion Properties
 
-        #region Events and delegates
-
-        /// <summary>
-        ///     Event raised when the temperature change is greater than the 
-        ///     TemperatureChangeNotificationThreshold value.
-        /// </summary>
-        public event SensorFloatEventHandler TemperatureChanged = delegate { };
-
-        /// <summary>
-        ///     Event raised when the humidity change is greater than the
-        ///     HumidityChangeNotificationThreshold value.
-        /// </summary>
-        public event SensorFloatEventHandler HumidityChanged = delegate { };
-
-        #endregion Events and delegates
-
         #region Constructors
 
         /// <summary>
@@ -217,29 +122,17 @@ namespace Meadow.Foundation.Sensors.Atmospheric
         /// </summary>
         /// <param name="address">Sensor address (default to 0x40).</param>
         /// <param name="i2cBus">I2CBus (default to 100 KHz).</param>
-        /// <param name="updateInterval">Number of milliseconds between samples (0 indicates polling to be used)</param>
-        /// <param name="humidityChangeNotificationThreshold">Changes in humidity greater than this value will trigger an event when updatePeriod > 0.</param>
-        /// <param name="temperatureChangeNotificationThreshold">Changes in temperature greater than this value will trigger an event when updatePeriod > 0.</param>
-        public SI7021(II2cBus i2cBus, byte address = 0x40, ushort updateInterval = MinimumPollingPeriod,
-            float humidityChangeNotificationThreshold = 0.001F, float temperatureChangeNotificationThreshold = 0.001F)
+        public SI7021(II2cBus i2cBus, byte address = 0x40)
         {
-            if (humidityChangeNotificationThreshold < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(humidityChangeNotificationThreshold), "Humidity threshold should be >= 0");
-            }
-            if (temperatureChangeNotificationThreshold < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(temperatureChangeNotificationThreshold), "Temperature threshold should be >= 0");
-            }
-            if ((updateInterval != 0) && (updateInterval < MinimumPollingPeriod))
-            {
-                throw new ArgumentOutOfRangeException(nameof(updateInterval), "Update period should be 0 or >= than " + MinimumPollingPeriod);
-            }
-
-            TemperatureChangeNotificationThreshold = temperatureChangeNotificationThreshold;
-            HumidityChangeNotificationThreshold = humidityChangeNotificationThreshold;
-            _updateInterval = updateInterval;
             _si7021 = new I2cPeripheral(i2cBus, address);
+            Init();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected void Init()
+        {
             //
             //  Get the device ID.
             //
@@ -254,8 +147,7 @@ namespace Meadow.Foundation.Sensors.Atmospheric
                 Registers.ReadIDSecondBytePart2
             }, 6);
             SerialNumber = 0;
-            for (var index = 0; index < 4; index++)
-            {
+            for (var index = 0; index < 4; index++) {
                 SerialNumber <<= 8;
                 SerialNumber += part1[index * 2];
             }
@@ -267,31 +159,20 @@ namespace Meadow.Foundation.Sensors.Atmospheric
             SerialNumber += part2[3];
             SerialNumber <<= 8;
             SerialNumber += part2[4];
-            if ((part2[0] == 0) || (part2[0] == 0xff))
-            {
+            if ((part2[0] == 0) || (part2[0] == 0xff)) {
                 SensorType = DeviceType.EngineeringSample;
-            }
-            else
-            {
-                SensorType = (DeviceType) part2[0];
+            } else {
+                SensorType = (DeviceType)part2[0];
             }
             //
             //  Update the firmware revision.
-            //
+            // TODO: why? 
             var firmware = _si7021.WriteRead(new[]
             {
                 Registers.ReadFirmwareRevisionPart1,
                 Registers.ReadFirmwareRevisionPart2
             }, 1);
             FirmwareRevision = firmware[0];
-            if (updateInterval > 0)
-            {
-                StartUpdating();
-            }
-            else
-            {
-                Update();
-            }
         }
 
         #endregion Constructors
@@ -299,48 +180,45 @@ namespace Meadow.Foundation.Sensors.Atmospheric
         #region Methods
 
         /// <summary>
-        ///     Start the update process.
+        /// Convenience method to get the current sensor readings. For frequent reads, use
+        /// StartSampling() and StopSampling() in conjunction with the SampleBuffer.
         /// </summary>
-        private void StartUpdating()
+        public async Task<AtmosphericConditions> Read()
         {
-            Thread t = new Thread(() => {
-                while (true)
-                {
-                    Update();
-                    Thread.Sleep(_updateInterval);
-                }
-            });
-            t.Start();
+            // update confiruation for a one-off read
+            this.Conditions = await ReadSensor();
+
+            return Conditions;
         }
 
-        /// <summary>
-        ///     Make a temperature and humidity reading.
-        /// </summary>
-        public void Update()
+        protected async Task<AtmosphericConditions> ReadSensor()
         {
-            _si7021.WriteByte(Registers.MeasureHumidityNoHold);
-            //
-            //  Maximum conversion time is 12ms (page 5 of the datasheet).
-            //
-            Thread.Sleep(25);
-            var data = _si7021.ReadBytes(3);
-            var humidityReading = (ushort) ((data[0] << 8) + data[1]);
-            Humidity = ((125 * (float) humidityReading) / 65536) - 6;
-            if (Humidity < 0)
-            {
-                Humidity = 0;
-            }
-            else
-            {
-                if (Humidity > 100)
-                {
-                    Humidity = 100;
+            AtmosphericConditions conditions = new AtmosphericConditions();
+
+            return await Task.Run(() => {
+                _si7021.WriteByte(Registers.MeasureHumidityNoHold);
+                //
+                //  Maximum conversion time is 12ms (page 5 of the datasheet).
+                //
+                Thread.Sleep(25);
+                var data = _si7021.ReadBytes(3);
+                var humidityReading = (ushort)((data[0] << 8) + data[1]);
+                conditions.Humidity = ((125 * (float)humidityReading) / 65536) - 6;
+                if (conditions.Humidity < 0) {
+                    conditions.Humidity = 0;
+                } else {
+                    if (conditions.Humidity > 100) {
+                        conditions.Humidity = 100;
+                    }
                 }
-            }
-            data = _si7021.ReadRegisters(Registers.ReadPreviousTemperatureMeasurement, 2);
-            var temperatureReading = (short) ((data[0] << 8) + data[1]);
-            Temperature = (float) (((175.72 * temperatureReading) / 65536) - 46.85);
+                data = _si7021.ReadRegisters(Registers.ReadPreviousTemperatureMeasurement, 2);
+                var temperatureReading = (short)((data[0] << 8) + data[1]);
+                conditions.Temperature = (float)(((175.72 * temperatureReading) / 65536) - 46.85);
+
+                return conditions;
+            });
         }
+
 
         /// <summary>
         ///     Reset the sensor and take a fresh reading.
@@ -349,7 +227,76 @@ namespace Meadow.Foundation.Sensors.Atmospheric
         {
             _si7021.WriteByte(Registers.Reset);
             Thread.Sleep(50);
-            Update();
+            ReadSensor(); // is this needed? why are we doing that?
+        }
+
+        public void StartUpdating(
+            int standbyDuration = 1000)
+        {
+            // thread safety
+            lock (_lock) {
+                if (IsSampling) return;
+
+                // state muh-cheen
+                IsSampling = true;
+
+                SamplingTokenSource = new CancellationTokenSource();
+                CancellationToken ct = SamplingTokenSource.Token;
+
+                AtmosphericConditions oldConditions;
+                AtmosphericConditionChangeResult result;
+                Task.Factory.StartNew(async () => {
+                    while (true) {
+                        // TODO: someone please review; is this the correct
+                        // place to do this?
+                        // check for cancel (doing this here instead of 
+                        // while(!ct.IsCancellationRequested), so we can perform 
+                        // cleanup
+                        if (ct.IsCancellationRequested) {
+                            // do task clean up here
+                            _observers.ForEach(x => x.OnCompleted());
+                            break;
+                        }
+                        // capture history
+                        oldConditions = Conditions;
+
+                        // read
+                        await Read();
+
+                        // build a new result with the old and new conditions
+                        result = new AtmosphericConditionChangeResult(oldConditions, Conditions);
+
+                        // let everyone know
+                        RaiseChangedAndNotify(result);
+
+                        // sleep for the appropriate interval
+                        await Task.Delay(standbyDuration);
+                    }
+                }, SamplingTokenSource.Token);
+            }
+        }
+
+        protected void RaiseChangedAndNotify(AtmosphericConditionChangeResult changeResult)
+        {
+            Updated?.Invoke(this, changeResult);
+            base.NotifyObservers(changeResult);
+        }
+
+        /// <summary>
+        /// Stops sampling the temperature.
+        /// </summary>
+        public void StopUpdating()
+        {
+            lock (_lock) {
+                if (!IsSampling) return;
+
+                if (SamplingTokenSource != null) {
+                    SamplingTokenSource.Cancel();
+                }
+
+                // state muh-cheen
+                IsSampling = false;
+            }
         }
 
         /// <summary>
@@ -368,5 +315,47 @@ namespace Meadow.Foundation.Sensors.Atmospheric
         }
 
         #endregion Methods
+
+        #region Enums
+
+        /// <summary>
+        ///     Specific device type / model.
+        /// </summary>
+        public enum DeviceType
+        {
+            Unknown = 0x00,
+            Si7013 = 0x0d,
+            Si7020 = 0x14,
+            Si7021 = 0x15,
+            EngineeringSample = 0xff
+        }
+
+        #endregion Enums
+
+        #region Classes / structures
+
+        /// <summary>
+        ///     Device registers.
+        /// </summary>
+        private static class Registers
+        {
+            public static readonly byte MeasureHumidityWithHold = 0xe5;
+            public static readonly byte MeasureHumidityNoHold = 0xf5;
+            public static readonly byte MeasureTemperatureWithHold = 0xe3;
+            public static readonly byte MeasureTemperatureNoHold = 0xf3;
+            public static readonly byte ReadPreviousTemperatureMeasurement = 0xe0;
+            public static readonly byte Reset = 0xfe;
+            public static readonly byte WriteUserRegister1 = 0xe6;
+            public static readonly byte ReadUserRegister1 = 0xe7;
+            public static readonly byte ReadIDFirstBytePart1 = 0xfa;
+            public static readonly byte ReadIDFirstBytePart2 = 0x0f;
+            public static readonly byte ReadIDSecondBytePart1 = 0xfc;
+            public static readonly byte ReadIDSecondBytePart2 = 0xc9;
+            public static readonly byte ReadFirmwareRevisionPart1 = 0x84;
+            public static readonly byte ReadFirmwareRevisionPart2 = 0xb8;
+        }
+
+        #endregion Classes / Structures
+
     }
 }
