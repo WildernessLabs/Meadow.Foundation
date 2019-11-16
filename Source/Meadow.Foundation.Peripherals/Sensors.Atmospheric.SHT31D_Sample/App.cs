4682784619a54eda11f903823f498c50ca550aec
Adrian Stevens <adrian.stevens@gmail.com>
2019-11-16 09:32:28 -0800

    Add SHT31D sample

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.SHT31D_Sample/App.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Atmospheric;
+using Meadow.Peripherals.Sensors;
+
+namespace BasicSensors.Atmospheric.SHT31D_Sample
+{
+    public class App : App<F7Micro, App>
+    {
+        SHT31D sensor;
+
+        public App()
+        {
+            InitHardware();
+
+            Console.WriteLine($"Temp: {sensor.Temperature}");
+            Console.WriteLine($"Humidity: {sensor.Humidity}");
+
+
+            Thread.Sleep(-1);
+        }
+
+        public void InitHardware()
+        {
+            Console.WriteLine("Init sensor...");
+
+            sensor = new SHT31D(Device.CreateI2cBus());
+            sensor.TemperatureChanged += Sensor_TemperatureChanged;
+            sensor.HumidityChanged += Sensor_HumidityChanged;
+        }
+
+        private void Sensor_HumidityChanged(object sender, SensorFloatEventArgs e)
+        {
+            Console.WriteLine($"Humidity: {e.CurrentValue}");
+        }
+
+        private void Sensor_TemperatureChanged(object sender, SensorFloatEventArgs e)
+        {
+            Console.WriteLine($"Temp: {e.CurrentValue}");
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.SHT31D_Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace BasicSensors.Atmospheric.SHT31D_Sample
+{
+    class Program
+    {
+        static IApp app;
+        public static void Main(string[] args)
+        {
+            // instantiate and run new meadow app
+            app = new App();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
