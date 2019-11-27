f894511200670768044fe40271b6be35d6181a45
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-11-26 22:28:02 -0800

    Update Sensors.Motion.Mpu6050_Sample project with new NuGet package reference

--- Source/Meadow.Foundation.Peripherals/Sensors.Motion.Mpu6050/Samples/Sensors.Motion.Mpu6050_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Motion.Mpu6050/Samples/Sensors.Motion.Mpu6050_Sample/MeadowApp.cs
@@ -2,19 +2,19 @@
 using System.Threading;
 using Meadow;
 using Meadow.Devices;
-//using Meadow.Foundation.Sensors.Motion;
+using Meadow.Foundation.Sensors.Motion;
 
 namespace Sensors.Motion.mpu5060_Sample
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
-        //Mpu6050 mpu5060;
+        Mpu6050 mpu5060;
 
         public MeadowApp()
         {
             Console.WriteLine("Initializing");
 
-            //mpu5060 = new Mpu6050(Device.CreateI2cBus());
+            mpu5060 = new Mpu6050(Device.CreateI2cBus());
 
             TestMpu6050();
         }
@@ -24,14 +24,17 @@ namespace Sensors.Motion.mpu5060_Sample
             Console.WriteLine("TestMpu6050...");
 
             // Wake
-            //mpu5060.Wake();
-            //while (true)
-            //{
-            //    Console.WriteLine("Reading...");
-            //    mpu5060.Refresh();
-            //    Console.WriteLine($" ({mpu5060.AccelerationX:X4},{mpu5060.AccelerationY:X4},{mpu5060.AccelerationZ:X4}) ({mpu5060.GyroX:X4},{mpu5060.GyroY:X4},{mpu5060.GyroZ:X4}) {mpu5060.Temperature}");
-            //    Thread.Sleep(2000);
-            //}
+            mpu5060.Wake();
+
+            while (true)
+            {
+                Console.WriteLine("Reading...");
+
+                mpu5060.Refresh();
+                Console.WriteLine($" ({mpu5060.AccelerationX:X4},{mpu5060.AccelerationY:X4},{mpu5060.AccelerationZ:X4}) ({mpu5060.GyroX:X4},{mpu5060.GyroY:X4},{mpu5060.GyroZ:X4}) {mpu5060.Temperature}");
+                
+                Thread.Sleep(2000);
+            }
         }
     }
 }
