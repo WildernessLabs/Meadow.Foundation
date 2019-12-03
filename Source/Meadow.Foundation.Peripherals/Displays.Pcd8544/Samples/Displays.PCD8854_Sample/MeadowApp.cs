955acc565724724e14de2e68b9191c7e30f60b2c
adrianstevens <adrian.stevens@gmail.com>
2019-12-02 17:45:28 -0800

    Update PCD8544 driver

--- Source/Meadow.Foundation.Peripherals/Displays.Pcd8544/Samples/Displays.PCD8854_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Displays.Pcd8544/Samples/Displays.PCD8854_Sample/MeadowApp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation.Displays;
@@ -8,46 +9,61 @@ namespace Displays.PCD8854_Sample
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
-        PCD8544 pCD8544;
+        PCD8544 display;
         GraphicsLibrary graphics;
 
         public MeadowApp()
         {
             Console.WriteLine("Initializing...");
 
-            pCD8544 = new PCD8544
+            var config = new Meadow.Hardware.SpiClockConfiguration(PCD8544.DEFAULT_SPEED, Meadow.Hardware.SpiClockConfiguration.Mode.Mode0);
+
+            display = new PCD8544
             (
                 device: Device,
-                spiBus: Device.CreateSpiBus(),
+                spiBus: Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, config),
                 chipSelectPin: Device.Pins.D01,
                 dcPin: Device.Pins.D00,
                 resetPin: Device.Pins.D02
             );
 
-            graphics = new GraphicsLibrary(pCD8544);
+            graphics = new GraphicsLibrary(display);
+            graphics.Rotation = GraphicsLibrary.RotationType._180Degrees;
 
             TestPCD8544();
+
+            Thread.Sleep(10000);
+
+            CounterDemo();
+        }
+
+        void CounterDemo()
+        {
+            int count = 0;
+
+            graphics.CurrentFont = new Font12x20();
+
+            while(true)
+            {
+                graphics.Clear();
+                graphics.DrawText(0, 0, $"Count:");
+                graphics.DrawText(0, 24, $"{count}");
+                graphics.Show();
+                count++;
+            }
         }
         
         void TestPCD8544() 
         {
             Console.WriteLine("TestPCD8544...");
 
-            // Drawing natively in the display
-            pCD8544.Clear();
-            for (int i = 0; i < 30; i++)
-            {
-                pCD8544.DrawPixel(i, i, true);
-                pCD8544.DrawPixel(30 + i, i, true);
-                pCD8544.DrawPixel(60 + i, i, true);
-            }
-            pCD8544.Show();
-
             // Drawing with Display Graphics Library
-            //graphics.Clear(true);
-            //graphics.CurrentFont = new Font8x12();
-            //graphics.DrawText(0, 0, "Meadow F7");
-            //graphics.DrawRectangle(5, 14, 30, 10, true);
+            graphics.Clear(true);
+            graphics.CurrentFont = new Font8x12();
+            graphics.DrawText(0, 0, "PCD8544");
+            graphics.DrawRectangle(5, 14, 30, 10, true);
+
+            graphics.Show();
         }
     }
 }
