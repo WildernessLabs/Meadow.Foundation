6dbc439c8873e8938f367b0693ec999facaf0edb
adrianstevens <adrian.stevens@gmail.com>
2019-12-02 19:55:39 -0800

    Minor update to Apds9960 (WIP)

--- Source/Meadow.Foundation.Peripherals/Sensors.Motion.Apds9960/Driver/Sensors.Motion.APDS9960/APDS9960.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Motion.Apds9960/Driver/Sensors.Motion.APDS9960/APDS9960.cs
@@ -211,12 +211,15 @@ namespace Meadow.Foundation.Sensors.Motion
         #endregion Constructors
 
         #region methods
+
         void Apds9960Init()
         {
-            var result = apds9960.ReadRegister(0);// APDS9960_ID);
+            var result = apds9960.ReadRegister(APDS9960_ID);
 
             if (result != 0xAB)
-                throw new Exception("APDS9960 isn't connected");
+            {
+                throw new Exception($"APDS9960 isn't connected: {result}");
+            }
         }
 
         //Sets the integration time for the ADC of the APDS9960, in millis
