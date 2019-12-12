f59a4987eb705156dac2e5cf35384c1318169262
adrianstevens <adrian.stevens@gmail.com>
2019-12-11 21:40:05 -0800

    Add GP2D12 driver, sample and datasheet

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Distance.GP2D12/Samples/Sensors.Distance.GP2D12_Sample/MeadowApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Sensors.Distance;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        GP2D12 sensor;
+
+        public MeadowApp()
+        {
+            Initalize();
+
+            sensor.DistanceDetected += Sensor_DistanceDetected;
+        }
+
+        private void Sensor_DistanceDetected(object sender, Meadow.Peripherals.Sensors.Distance.DistanceEventArgs e)
+        {
+            Console.WriteLine($"Distance: {e.Distance}");
+        }
+
+        public void Initalize()
+        {
+            Console.WriteLine("Initialize...");
+
+            sensor = new GP2D12(Device, Device.Pins.A01);
+        }
+    }
+}
