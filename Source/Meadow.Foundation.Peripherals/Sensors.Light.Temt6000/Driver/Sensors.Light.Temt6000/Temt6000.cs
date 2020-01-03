6995a58ffbec4b12e845c502c56b212d8d10367e
adrianstevens <adrian.stevens@gmail.com>
2020-01-03 13:19:32 -0800

    Add Temt6000 light sensor driver, sample, and spec sheet

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Light.Temt6000/Driver/Sensors.Light.Temt6000/Temt6000.cs
@@ -0,0 +1,45 @@
+﻿using Meadow.Hardware;
+using System.Threading.Tasks;
+
+namespace Meadow.Foundation.Sensors.Light
+{
+    public class Temt6000
+    {
+        #region Member variables / fields
+
+        /// <summary>
+        ///     Analog port connected to the sensor.
+        /// </summary>
+        private readonly IAnalogInputPort sensor;
+
+        /// <summary>
+        ///     Voltage being output by the sensor.
+        /// </summary>
+        public Task<float> GetVoltage()
+        {
+            return sensor.Read();
+        }
+
+        #endregion Member variables / fields
+
+        #region Constructors
+
+        /// <summary>
+        ///     Default constructor (private to prevent it being used).
+        /// </summary>
+        private Temt6000()
+        {
+        }
+
+        /// <summary>
+        ///     Create a new light sensor object using a static reference voltage.
+        /// </summary>
+        /// <param name="pin">AnalogChannel connected to the sensor.</param>
+        public Temt6000(IIODevice device, IPin pin)
+        {
+            sensor = device.CreateAnalogInputPort(pin);
+        }
+
+        #endregion Constructors
+    }
+}
