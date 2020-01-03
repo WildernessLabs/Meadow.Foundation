1f11b23ae61c931d9601ba55bc5bb5efccde4d63
adrianstevens <adrian.stevens@gmail.com>
2020-01-03 14:29:55 -0800

    Add SD323x sample and driver tweaks

---
+++ Source/Meadow.Foundation.Peripherals/RTCs.DS323x/Samples/RTCs.DS323x_Sample/MeadowApp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.RTCs;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        Ds3231 sensor;
+
+        public MeadowApp()
+        {
+            Initialize();
+
+            sensor.CurrentDateTime = new DateTime(2020, 1, 1);
+
+            Console.WriteLine("Read from sensor");
+
+            Console.WriteLine($"Current time: {sensor.CurrentDateTime}");
+            Console.WriteLine($"Temperature: {sensor.Temperature}");
+
+            sensor.ClearInterrupt(Ds323x.Alarm.BothAlarmsRaised);
+            sensor.DisplayRegisters();
+
+            sensor.DisplayRegisters();
+            sensor.SetAlarm(Ds323x.Alarm.Alarm1Raised, new DateTime(2020, 1, 1, 1, 0, 0),
+                         Ds323x.AlarmType.WhenSecondsMatch);
+
+            sensor.OnAlarm1Raised += Sensor_OnAlarm1Raised;
+            sensor.DisplayRegisters();
+        }
+
+        private void Sensor_OnAlarm1Raised(object sender)
+        {
+            var rtc = (Ds3231)sender;
+            Console.WriteLine("Alarm 1 has been activated: " + rtc.CurrentDateTime.ToString("dd MMM yyyy HH:mm:ss"));
+            rtc.ClearInterrupt(Ds323x.Alarm.Alarm1Raised);
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            sensor = new Ds3231(Device, Device.CreateI2cBus(), null);
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/RTCs.DS323x/Samples/RTCs.DS323x_Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using Meadow;
+
+namespace MeadowApp
+{
+    class Program
+    {
+        static IApp app;
+        public static void Main(string[] args)
+        {
+            // instantiate and run new meadow app
+            app = new MeadowApp();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
