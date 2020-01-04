7739707b4af341c3c28a7e22327cb5b0e814f5ff
adrianstevens <adrian.stevens@gmail.com>
2020-01-03 19:27:29 -0800

    Add AT24Cxx eeprom sample app

---
+++ Source/Meadow.Foundation.Peripherals/ICs.EEPROM.AT24Cxx/Samples/ICS.EEPROM.AT24Cxx/MeadowApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.ICs.EEPROM;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        At24Cxx eeprom;
+
+        public MeadowApp()
+        {
+            Initialize();
+
+            eeprom.Write(0, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+
+            var memory = eeprom.Read(0, 16);
+
+            for (ushort index = 0; index < 16; index++)
+            {
+                Console.WriteLine("Byte: " + index + ", Value: " + memory[index]);
+            }
+
+            eeprom.Write(3, new byte[] { 10 });
+            eeprom.Write(7, new byte[] { 1, 2, 3, 4 });
+            memory = eeprom.Read(0, 16);
+            for (ushort index = 0; index < 16; index++)
+            {
+                Console.WriteLine("Byte: " + index + ", Value: " + memory[index]);
+            }
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            //256kbit = 256*1024 bits = 262144 bits = 262144 / 8 bytes = 32768 bytes
+            eeprom = new At24Cxx(i2cBus: Device.CreateI2cBus(),
+                                 memorySize: 32768);
+
+        }
+    }
+}
