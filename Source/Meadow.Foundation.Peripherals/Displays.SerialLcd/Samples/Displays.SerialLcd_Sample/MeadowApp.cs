3b9c354200e36f672a2706c22487f590002d74c1
adrianstevens <adrian.stevens@gmail.com>
2020-01-08 17:07:30 -0800

    Add serial LCD sample

---
+++ Source/Meadow.Foundation.Peripherals/Displays.SerialLcd/Samples/Displays.SerialLcd_Sample/MeadowApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Displays;
+using Meadow.Hardware;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        SerialLcd display;
+
+        public MeadowApp()
+        {
+            Initialize();
+
+            Console.WriteLine("Clear");
+            display.Clear();
+
+            Console.WriteLine("Set brightness");
+            display.SetBrightness(0.5f);
+
+            Console.WriteLine("WriteLine");
+            display.WriteLine("Hello LCD", 0);
+
+        //    Console.WriteLine("Toggle splash");
+        //    display.ToggleSplashScreen();
+        }
+
+        void Initialize()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            display = new SerialLcd(Device, Device.SerialPortNames.Com4);
+        }
+    }
+}
