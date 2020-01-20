13de4a025ae339aec576f52c6a2c95548224843d
Adrian Stevens <adrian.stevens@gmail.com>
2020-01-19 21:35:27 -0800

    Add Dht10 & Dht12 drivers, sample and datasheets

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Dhtxx/Samples/Sensors.Atmospheric.Dht12_Sample/MeadowApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Atmospheric;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        Dht12 sensor;
+
+        public MeadowApp()
+        {
+            Initialize();
+
+            sensor.StartUpdating();
+
+            sensor.Updated += Sensor_Updated;
+        }
+
+        private void Sensor_Updated(object sender, Meadow.Peripherals.Sensors.Atmospheric.AtmosphericConditionChangeResult e)
+        {
+            Console.WriteLine($"Humidity: {e.New.Humidity}%, Temperature: {e.New.Temperature}°C");
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine("Init...");
+
+            sensor = new Dht12(Device.CreateI2cBus());
+        }
+    }
+}
