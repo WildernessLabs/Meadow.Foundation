f4228fec9fb19bfb6dc0174e70b3eefb2a3587c6
Adrian Stevens <adrian.stevens@gmail.com>
2020-02-01 10:52:47 -0800

    featherwing progress

---
+++ Source/Meadow.Foundation.Core/Sensors/Hid/CapacitiveTouchScreen.cs
@@ -0,0 +1,31 @@
+﻿using Meadow.Hardware;
+
+namespace Meadow.Foundation.Sensors.Hid
+{
+    public class CapacitiveTouchScreen
+    {
+        #region Fields / member variables
+
+        IAnalogInputPort portYPos;
+        IAnalogInputPort portXNeg;
+
+        #endregion Fields / member variables
+
+        #region Constructors
+
+        private CapacitiveTouchScreen() { }
+
+        public CapacitiveTouchScreen(IIODevice device, IPin pinXPos, IPin pinXNeg, IPin pinYPos, IPin pinYNeg)
+        {
+            portXNeg = device.CreateAnalogInputPort(pinXNeg);
+
+            
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        #endregion Methods
+    }
+}
