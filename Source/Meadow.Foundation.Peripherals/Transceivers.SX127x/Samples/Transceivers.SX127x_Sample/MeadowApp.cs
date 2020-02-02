02f39a3495e61fdc15c832814e1427a21e7dacf1
ctacke <ctacke@opennetcf.com>
2020-02-02 14:49:58 -0600

    framework for SX127x

---
+++ Source/Meadow.Foundation.Peripherals/Transceivers.SX127x/Samples/Transceivers.SX127x_Sample/MeadowApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Transceivers;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        SX127x radio;
+
+        public MeadowApp()
+        {
+            InitHardware();
+        }
+
+        public void InitHardware()
+        {
+            Console.WriteLine("Initialize...");
+            var bus = Device.CreateSpiBus();
+            var chipSelect = Device.CreateDigitalOutputPort(Device.Pins.D00);
+            radio = new SX127x(bus, chipSelect);
+
+            Console.WriteLine($"Silicon Revision: 0x{radio.GetVersion():x2}");
+        }
+    }
+}
