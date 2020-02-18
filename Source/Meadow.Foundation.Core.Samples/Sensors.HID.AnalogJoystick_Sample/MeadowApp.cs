48e0c901c5c43c973c9ac29c224588d48b297ec2
Jorge Ramirez <ramirez.jorgea@outlook.com>
2020-02-18 00:05:03 -0800

    Add Leds to light up depending on GetPosition()

--- Source/Meadow.Foundation.Core.Samples/Sensors.HID.AnalogJoystick_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Core.Samples/Sensors.HID.AnalogJoystick_Sample/MeadowApp.cs
@@ -1,5 +1,6 @@
 ﻿using Meadow;
 using Meadow.Devices;
+using Meadow.Foundation.Leds;
 using Meadow.Foundation.Sensors.Hid;
 using System;
 using System.Threading;
@@ -10,11 +11,17 @@ namespace MeadowApp
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
+        Led Up, Down, Left, Right;
         AnalogJoystick joystick;
 
         public MeadowApp()
         {
             Console.WriteLine("MeadowApp()...");
+
+            Up = new Led(Device.CreateDigitalOutputPort(Device.Pins.D07));
+            Down = new Led(Device.CreateDigitalOutputPort(Device.Pins.D04));
+            Left = new Led(Device.CreateDigitalOutputPort(Device.Pins.D02));
+            Right = new Led(Device.CreateDigitalOutputPort(Device.Pins.D03));
 
             var calibration = new JoystickCalibration(
                 1.58f, 0.02f, 3.29f,
@@ -23,7 +30,7 @@ namespace MeadowApp
             joystick = new AnalogJoystick(
                 Device.CreateAnalogInputPort(Device.Pins.A01), 
                 Device.CreateAnalogInputPort(Device.Pins.A00),
-                calibration);
+                calibration, true);
 
             var t = TestAnalogJoystick();
         }
@@ -31,16 +38,6 @@ namespace MeadowApp
         async Task TestAnalogJoystick() 
         {
             Console.WriteLine("TestAnalogJoystick()...");
-
-            //JoystickPosition position;
-
-            //await joystick.SetCenterPosition();
-
-            //while (true)
-            //{
-            //    position = await joystick.GetPosition();
-            //    Console.WriteLine($"{position}");
-            //}
 
             float x, y;
 
@@ -50,10 +47,49 @@ namespace MeadowApp
                 //y = await joystick.GetVerticalValue();
                 //Console.WriteLine($"({x},{y})");
 
+                
                 var position = await joystick.GetPosition();
+                switch (position)
+                {
+                    case JoystickPosition.Up:
+                        Down.IsOn = Left.IsOn = Right.IsOn = false;
+                        Up.IsOn = true;
+                        break;
+                    case JoystickPosition.Down:
+                        Up.IsOn = Left.IsOn = Right.IsOn = false;
+                        Down.IsOn = true;
+                        break;
+                    case JoystickPosition.Left:
+                        Up.IsOn = Down.IsOn = Right.IsOn = false;
+                        Left.IsOn = true;
+                        break;
+                    case JoystickPosition.Right:
+                        Up.IsOn = Down.IsOn = Left.IsOn = false;
+                        Right.IsOn = true;
+                        break;
+                    case JoystickPosition.Center:
+                        Up.IsOn = Down.IsOn = Left.IsOn = Right.IsOn = false;
+                        break;
+                    case JoystickPosition.UpLeft:
+                        Down.IsOn = Right.IsOn = false;
+                        Up.IsOn = Left.IsOn = true;
+                        break;
+                    case JoystickPosition.UpRight:
+                        Down.IsOn = Left.IsOn = false;
+                        Up.IsOn = Right.IsOn = true;
+                        break;
+                    case JoystickPosition.DownLeft:
+                        Up.IsOn = Right.IsOn = false;
+                        Down.IsOn = Left.IsOn = true;
+                        break;
+                    case JoystickPosition.DownRight:
+                        Up.IsOn = Left.IsOn = false;
+                        Down.IsOn = Right.IsOn = true;
+                        break;
+                }
                 Console.WriteLine($"{position.ToString()}");
 
-                Thread.Sleep(1000);
+                Thread.Sleep(50);
             }
         }
     }
