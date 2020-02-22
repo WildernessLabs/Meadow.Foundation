8240ff5da105337c0df8035914b4e5413e7807e0
ctacke <ctacke@opennetcf.com>
2020-02-22 10:41:06 -0600

    Missed a check-in

--- Source/Meadow.Foundation.Peripherals/Sensors.Power.Ina260/Driver/INA260.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Power.Ina260/Driver/INA260.cs
@@ -53,7 +53,7 @@ namespace Meadow.Foundation.Sensors.Power
         public float CurrentChangeThreshold { get; set; }
         public float PowerChangeThreshold { get; set; }
         public bool IsSampling { get; private set; }
-        public int Address { get; private set; }
+        public byte Address { get; private set; }
 
         public INA260(II2cBus i2cBus, byte address = (byte)Addresses.Default)
         {
@@ -152,24 +152,6 @@ namespace Meadow.Foundation.Sensors.Power
                 if (!IsSampling) return;
                 SamplingTokenSource.Cancel();
             }
-        }
-
-        private void Initialize(I2cConnectionSettings settings)
-        {
-            if (settings == null) throw new ArgumentNullException(nameof(settings));
-
-            switch (settings.DeviceAddress)
-            {
-                case 0x40:
-                case 0x41:
-                    // valid;
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException("INA200 device address must be either 0x40 or 0x41");
-            }
-
-            Bus = I2cDevice.Create(settings);
-            Address = settings.DeviceAddress;
         }
 
         /// <summary>
@@ -321,18 +303,14 @@ namespace Meadow.Foundation.Sensors.Power
         {
             lock (SyncRoot)
             {
-                Span<byte> buffer = stackalloc byte[2];
-
                 if (register != RegisterPointer)
                 {
                     // write the pointer
-                    buffer[0] = (byte)register;
-                    Bus.Write(buffer.Slice(0, 1));
-
+                    Bus.WriteData(Address, (byte)register);
                     RegisterPointer = register;
                 }
 
-                Bus.Read(buffer);
+                var buffer = Bus.ReadData(Address, 2);
 
                 unchecked
                 {
@@ -345,157 +323,21 @@ namespace Meadow.Foundation.Sensors.Power
         {
             lock (SyncRoot)
             {
-                Span<byte> buffer = stackalloc byte[2];
-
                 if (register != RegisterPointer)
                 {
                     // write the pointer
-                    buffer[0] = (byte)register;
-                    Bus.Write(buffer.Slice(0, 1));
+                    Bus.WriteData(Address, (byte)register);
+                    RegisterPointer = register;
 
                     RegisterPointer = register;
                 }
 
-                Bus.Read(buffer);
+                var buffer = Bus.ReadData(Address, 2);
 
                 unchecked
                 {
                     return (short)((buffer[0] << 8) | buffer[1]);
                 }
-            }
-        }
-
-        public class INA260 : IDisposable
-    {
-        /// <summary>
-        ///     Valid addresses for the sensor.
-        /// </summary>
-        public enum Addresses : byte
-        {
-            Address0 = 0x40,
-            Address1 = 0x41,
-            Default = Address0
-        }
-
-        private enum Register : byte
-        {
-            Config = 0x00,
-            Current = 0x01,
-            Voltage = 0x02,
-            Power = 0x03,
-            MaskEnable = 0x06,
-            AlertLimit = 0x07,
-            ManufacturerID = 0xFE,
-            DieID = 0xFF
-        }
-
-        private const float MeasurementScale = 0.00125f;
-
-        private Register RegisterPointer { get; set; }
-        private object SyncRoot { get; } = new object();
-        private II2cBus Bus { get; set; }
-
-        public byte Address { get; private set; }
-
-        public INA260(II2cBus i2cBus, byte address = (byte)Addresses.Default)
-        {
-            if (i2cBus == null) throw new ArgumentNullException(nameof(i2cBus));
-
-            switch (address)
-            {
-                case 0x40:
-                case 0x41:
-                    // valid;
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException("INA200 device address must be either 0x40 or 0x41");
-            }
-
-            Bus = i2cBus;
-            Address = address;
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                
-            }
-        }
-
-        /// <summary>
-        /// Dispose managed resources
-        /// </summary>
-        public void Dispose()
-        {
-            Dispose(true);
-        }
-
-        /// <summary>
-        /// Reads the unique manufacturer identification number
-        /// </summary>
-        public int ManufacturerID
-        {
-            get => ReadRegister(Register.ManufacturerID);
-        }
-
-        /// <summary>
-        /// Reads the unique die identification number
-        /// </summary>
-        public int DieID
-        {
-            get => ReadRegister(Register.ManufacturerID);
-        }
-
-        /// <summary>
-        /// Reads the value of the current (in Amps) flowing through the shunt resistor
-        /// </summary>
-        public float Current
-        {
-            get
-            {
-                unchecked
-                {
-                    var c = (short)ReadRegister(Register.Current);
-                    return c * MeasurementScale;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Reads bus voltage measurement (in Volts) data
-        /// </summary>
-        public float Voltage
-        {
-            get => ReadRegister(Register.Voltage) * MeasurementScale;
-        }
-
-        /// <summary>
-        /// Reads the value of the calculated power being delivered to the load
-        /// </summary>
-        public float Power
-        {
-            get => ReadRegister(Register.Power);
-        }
-
-        private ushort ReadRegister(Register register)
-        {
-            lock (SyncRoot)
-            {
-                Span<byte> buffer = stackalloc byte[2];
-
-                if (register != RegisterPointer)
-                {
-                    // write the pointer
-                    buffer[0] = (byte)register;
-                    Bus.WriteData(Address, buffer[0]);
-
-                    RegisterPointer = register;
-                }
-
-                var data = Bus.ReadData(Address, 2);
-
-                return (ushort)((data[0] << 8) | data[1]);
             }
         }
     }
