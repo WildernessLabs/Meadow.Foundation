0fed160213b7b9e2336b45f5d3b818a846a683e1
TheGreatCO <thegreatco@thegreatco.com>
2020-03-06 08:47:50 -0500

    initial BME680 support, missing VOC

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Bme680/Driver/Sensors.Atmospheric.BME680/TemperatureUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensors.Atmospheric.Bme680
+{
+    public enum TemperatureUnit
+    {
+        C,
+        F
+    }
+}
