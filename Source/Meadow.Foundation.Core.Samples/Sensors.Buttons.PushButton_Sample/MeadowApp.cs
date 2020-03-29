d89abef23283f4851d83abe7ae529f75b4b95aa0
Jorge Ramirez <ramirez.jorgea@outlook.com>
2020-03-29 00:47:41 -0700

    Update PushButton project sample

--- Source/Meadow.Foundation.Core.Samples/Sensors.Buttons.PushButton_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Core.Samples/Sensors.Buttons.PushButton_Sample/MeadowApp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation.Sensors.Buttons;
@@ -7,31 +8,43 @@ namespace Sensors.Buttons.PushButton_Sample
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
-        protected PushButton pushButton;
+        List<PushButton> pushButtons;
 
         public MeadowApp()
         {
             Console.WriteLine("Initializing...");
 
-            pushButton = new PushButton(Device, Device.Pins.D02);
-            pushButton.PressStarted += (s, e) => 
+            pushButtons = new List<PushButton> 
             {
-                Console.WriteLine("Press started");
-            };
-            pushButton.PressEnded += (s, e) => 
-            {
-                Console.WriteLine("Press ended");
-            };
-            pushButton.Clicked += (s, e) => 
-            {
-                Console.WriteLine("Button Clicked");
-            };
-            pushButton.LongPressClicked += (s, e) => 
-            {
-                Console.WriteLine("Long pressed!");
+                new PushButton(Device, Device.Pins.D00), // <- Not working (D00 - D04) (D00 - D07) (*)
+                new PushButton(Device, Device.Pins.D01), 
+                new PushButton(Device, Device.Pins.D02), // <- (*)
+                new PushButton(Device, Device.Pins.D03),
+                new PushButton(Device, Device.Pins.D04), // <- (*)
+                new PushButton(Device, Device.Pins.D05), // <- Not working (D04 - D07) (D00 - D07) (*)
+                new PushButton(Device, Device.Pins.D06),
+                new PushButton(Device, Device.Pins.D07),
+                new PushButton(Device, Device.Pins.D08),
+                new PushButton(Device, Device.Pins.D09),
+                new PushButton(Device, Device.Pins.D10),
+                new PushButton(Device, Device.Pins.D11),
+                new PushButton(Device, Device.Pins.D12),
+                new PushButton(Device, Device.Pins.D13),
+                new PushButton(Device, Device.Pins.D14), // <- Not working (D12 - D15) (D08 - D15) (*)
+                new PushButton(Device, Device.Pins.D15)
             };
 
-            Console.WriteLine("PushButton ready...");
+            for(int i = 0; i < pushButtons.Count; i++)
+            {
+                pushButtons[i].Clicked += PushbuttonClicked;
+            }
+
+            Console.WriteLine("PushButton ready!!!..");
+        }
+
+        void PushbuttonClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine($"PushButton Clicked!");
         }
     }
 }
