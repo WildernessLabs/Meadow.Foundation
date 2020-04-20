954baf7d902c859428a666bf11aca8cff48621f1
ctacke <ctacke@opennetcf.com>
2020-04-20 11:22:55 -0500

    Initial NAU7802 driver

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.LoadCell.Nau7802/Samples/Sensors.LoadCell.Nau7802_Sample/MeadowApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Light;
+
+namespace Sensors.LoadCell.Nau7802_Sample
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        private Nau7802 _loadSensor;
+
+        public MeadowApp()
+        {
+            Console.WriteLine($"Creating I2C Bus...");
+            var bus = Device.CreateI2cBus();
+            Console.WriteLine($"Creating Sensor...");
+            using (_loadSensor = new Nau7802(bus))
+            {
+                _loadSensor.Tare();
+
+                while (true)
+                {
+                    var c = _loadSensor.GetWeight();
+                    Console.WriteLine($"Conversion returned {c}");
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
