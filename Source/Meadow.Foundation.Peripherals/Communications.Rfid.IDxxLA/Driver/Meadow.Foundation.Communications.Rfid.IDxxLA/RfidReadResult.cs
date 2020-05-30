f66880cdd5a5d3d8fd7be2e3a33367a70b932f9a
Ben Edwards <bene@pageuppeople.com>
2020-05-30 17:23:00 +1000

    Add IDxxLA driver and sample project

---
+++ Source/Meadow.Foundation.Peripherals/Communications.Rfid.IDxxLA/Driver/Meadow.Foundation.Communications.Rfid.IDxxLA/RfidReadResult.cs
@@ -0,0 +1,15 @@
+﻿// RfidReadResult.cs
+// Author: Ben Edwards
+// Licensed under MIT
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Meadow.Foundation.Communications.Rfid
+{
+    public class RfidReadResult : EventArgs
+    {
+        public RfidValidationStatus Status { get; set; }
+        public byte[] RfidTag { get; set; }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Communications.Rfid.IDxxLA/Driver/Meadow.Foundation.Communications.Rfid.IDxxLA/RfidValidationStatus.cs
@@ -0,0 +1,25 @@
+﻿// RfidValidationStatus.cs
+// Author: Ben Edwards
+// Licensed under MIT
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Meadow.Foundation.Communications.Rfid
+{
+    public enum RfidValidationStatus
+    {
+        /// <summary>
+        /// Rfid read was successful
+        /// </summary>
+        Ok = 0,
+
+        /// <summary>
+        /// Rfid was read but failed checksum validation
+        /// </summary>
+        ChecksumFailed,
+
+        /// <summary>
+        /// Attempt to read the Rfid failed due to the data read from the serial port not being in an expected format.
+        /// </summary>
+        InvalidDataFormat
+    }
+}
