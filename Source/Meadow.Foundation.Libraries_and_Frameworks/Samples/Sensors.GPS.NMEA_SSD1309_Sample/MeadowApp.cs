91e1a494118266305b8469821dee1401be34d686
bryan costanich <b@wowzer.net>
2020-06-17 18:39:01 -0700

    updated sample to new API

--- Source/Meadow.Foundation.Libraries_and_Frameworks/Samples/Sensors.GPS.NMEA_SSD1309_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Samples/Sensors.GPS.NMEA_SSD1309_Sample/MeadowApp.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Threading;
 using Meadow;
 using Meadow.Devices;
-using Meadow.Foundation;
 using Meadow.Foundation.Communications;
 using Meadow.Foundation.Displays;
 using Meadow.Foundation.Graphics;
-using Meadow.Foundation.Leds;
-using Meadow.Foundation.Sensors.GPS;
+using Meadow.Foundation.Sensors.Location.Gnss.NmeaParsing;
 using Meadow.Hardware;
 
 namespace Sensors.GPS.NMEA_SSD1309_Sample
@@ -44,9 +41,9 @@ namespace Sensors.GPS.NMEA_SSD1309_Sample
             port.Open();
 
             nmea = new NmeaSentenceParser();
-            var ggaDecoder = new GGADecoder();
-            ggaDecoder.OnPositionReceived += GgaDecoder_OnPositionReceived;
-            nmea.AddDecoder(ggaDecoder);
+            var ggaParser = new GgaParser();
+            ggaParser.OnPositionReceived += GgaParser_OnPositionReceived;
+            nmea.AddParser(ggaParser);
 
             serialTextFile = new SerialTextFile(port, "\r\n");
             serialTextFile.OnLineReceived += SerialTextFile_OnLineReceived;
@@ -57,16 +54,16 @@ namespace Sensors.GPS.NMEA_SSD1309_Sample
             nmea.ParseNmeaMessage(line);
         }
 
-        private void GgaDecoder_OnPositionReceived(object sender, GPSLocation location)
+        private void GgaParser_OnPositionReceived(object sender, Meadow.Peripherals.Sensors.Location.Gnss.GnssPositionInfo location)
         {
             port.Close();
             graphics.Clear();
             graphics.DrawText(0, 0, "Latitude:");
-            graphics.DrawText(0, 11, $"{location.Latitude.Degrees} {location.Latitude.Direction}");
+            graphics.DrawText(0, 11, $"{location.Position.Latitude?.Degrees} {location.Position.Latitude?.Direction}");
             graphics.DrawText(0, 22, "Longitude:");
-            graphics.DrawText(0, 33,$"{location.Longitude.Degrees} {location.Longitude.Direction}");
+            graphics.DrawText(0, 33,$"{location.Position.Longitude?.Degrees} {location.Position.Longitude?.Direction}");
             graphics.DrawText(0, 44, "Altitude:");
-            graphics.DrawText(0, 55, $"{location.Altitude}m");
+            graphics.DrawText(0, 55, $"{location.Position.Altitude}m");
             graphics.Show();
             port.Open();
         }
