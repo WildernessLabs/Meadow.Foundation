c8f4d4e7eda10da6611609425ea2750a5c8d165d
ctacke <ctacke@opennetcf.com>
2020-06-24 11:36:45 -0500

    HX711 working

--- Source/Meadow.Foundation.Peripherals/Sensors.LoadCell.Hx711/Driver/Sensors.LoadCell.Hx711/Hx711.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.LoadCell.Hx711/Driver/Sensors.LoadCell.Hx711/Hx711.cs
@@ -10,24 +10,36 @@ namespace Meadow.Foundation.Sensors.Light
     /// </summary>
     public class Hx711 : FilterableChangeObservableBase<FloatChangeResult, float>,  IDisposable
     {
+        public enum AdcGain
+        {
+            Gain128 = 1,
+            Gain32 = 2,
+            Gain64 = 3,
+        }
+
         private const uint GPIO_BASE = 0x40020000;
         private const uint IDR_OFFSET = 0x10;
         private const uint BSSR_OFFSET = 0x18;
 
-        private uint _sckAddress;
-        private uint _doutAddress;
+        private uint _sck_address;
+        private int _sck_pin;
+        private uint _dout_address;
         private uint _sck_set;
         private uint _sck_clear;
         private uint _dout_mask;
+        private const int _timing_iterations = 3;
+        private uint _tareValue = 0;
+        private decimal _gramsPerAdcUnit;
 
         private IDigitalOutputPort _sck;
         private IDigitalInputPort _dout;
-
-        private bool _isDisposed;
+        private bool _createdPorts = false;
 
         private object SyncRoot { get; } = new object();
 
+        public bool IsDisposed { get; private set; }
         public bool IsSleeping { get; private set; }
+        public AdcGain Gain { get; private set; } = AdcGain.Gain128;
 
         /// <summary>
         /// Creates an instance of the NAU7802 Driver class
@@ -37,6 +49,7 @@ namespace Meadow.Foundation.Sensors.Light
         {
             _sck = device.CreateDigitalOutputPort(sck);
             _dout = device.CreateDigitalInputPort(dout);
+            _createdPorts = true; // we need to dispose what we create
 
             CalculateRegisterValues(sck, dout);
             Start();
@@ -55,9 +68,15 @@ namespace Meadow.Foundation.Sensors.Light
         {
             ClockLow(); // this resets the chip
             IsSleeping = false;
-            _ = ReadProc();
+
+            // auto-tare
+            Thread.Sleep(20);
+            Tare();
         }
 
+        /// <summary>
+        /// Puts the device into low-power sleep mode
+        /// </summary>
         public void Sleep()
         {
             if (IsSleeping) return;
@@ -69,6 +88,9 @@ namespace Meadow.Foundation.Sensors.Light
             }
         }
 
+        /// <summary>
+        /// Takes the device out of low-power sleep mode
+        /// </summary>
         public void Wake()
         {
             if (!IsSleeping) return;
@@ -80,23 +102,75 @@ namespace Meadow.Foundation.Sensors.Light
             }
         }
 
-        private async Task ReadProc()
+        /// <summary>
+        /// Tares the sensor, effectively setting the current weight reading to relative zero. 
+        /// </summary>
+        public void Tare()
         {
-            while(!_isDisposed)
-            {
-                if(IsSleeping || _dout.State)
-                {
-                    await Task.Delay(2000);
-                }
-                else
-                {
-                    // data is ready
-                    var count = ReadData(24);
+            _tareValue = ReadADC();
+            Console.WriteLine($"Tare base = {_tareValue}");
+        }
 
-                    Console.WriteLine($"Count: 0x{count:X8}");
-                    await Task.Delay(1000);
-                }
+        /// <summary>
+        /// Calculates the calibration factor of the load cell.  Call this method with a known weight on the sensor, and then use the returned value in a call to <see cref="SetCalibrationFactor(int, Weight)"/> before using the sensor.
+        /// </summary>
+        /// <returns></returns>
+        public int CalculateCalibrationFactor()
+        {
+            // do a few reads, then return the difference between tare (zero) and this value.
+            var reads = 5;
+            var sum = 0u;
+
+            for (int i = 0; i < reads; i++)
+            {
+                sum += ReadADC();
             }
+
+            return (int)((sum / reads) - _tareValue);
+        }
+
+        /// <summary>
+        /// Sets the sensor's calibration factor based on a factor calculated with a know weight by calling <see cref="CalculateCalibrationFactor"/>.
+        /// </summary>
+        /// <param name="factor"></param>
+        /// <param name="knownValue"></param>
+        public void SetCalibrationFactor(int factor, Weight knownValue)
+        {
+            _gramsPerAdcUnit = knownValue.ConvertTo(WeightUnits.Grams) / (decimal)factor;
+        }
+
+        /// <summary>
+        /// Gets the current sensor weight
+        /// </summary>
+        /// <returns></returns>
+        public Weight GetWeight()
+        {
+            if (_gramsPerAdcUnit == 0)
+            {
+                throw new Exception("Calibration factor has not been set");
+            }
+
+            // get an ADC conversion
+            var raw = ReadADC();
+            // subtract the tare
+            var adc = raw - _tareValue;
+
+            // two's complement
+            int value;
+            if ((raw & 0x800000) != 0)
+            {
+                value = (int)(~adc + 1) * -1;
+            }
+            else
+            {
+                value = (int)adc;
+            }
+
+            // convert to grams
+            var grams = value * _gramsPerAdcUnit;
+
+            // convert to desired units
+            return new Weight(grams, WeightUnits.Grams);
         }
 
         private void CalculateRegisterValues(IPin sck, IPin dout)
@@ -107,56 +181,82 @@ namespace Meadow.Foundation.Sensors.Light
             // Bits 15:0  set
             // Port offset = 0x0400 * index (with A being index 0)
             int gpio_port = sck.Key.ToString()[1] - 'A';
-            int gpio_pin = int.Parse(sck.Key.ToString().Substring(2));
-            _sckAddress = GPIO_BASE | (0x400u * (uint)gpio_port) | BSSR_OFFSET;
-            _sck_set = 1u << gpio_pin;
-            _sck_clear = 1u << (16 + gpio_pin);
+            _sck_pin = int.Parse(sck.Key.ToString().Substring(2));
+            _sck_address = GPIO_BASE | (0x400u * (uint)gpio_port) | BSSR_OFFSET;
+            _sck_set = 1u << _sck_pin;
+            _sck_clear = 1u << (16 + _sck_pin);
 
             gpio_port = dout.Key.ToString()[1] - 'A';
-            gpio_pin = int.Parse(dout.Key.ToString().Substring(2));
-            _doutAddress = GPIO_BASE | (0x400u * (uint)gpio_port) | IDR_OFFSET;
+            var gpio_pin = int.Parse(dout.Key.ToString().Substring(2));
+            _dout_address = GPIO_BASE | (0x400u * (uint)gpio_port) | IDR_OFFSET;
             _dout_mask = 1u << gpio_pin;
         }
 
         private unsafe void ClockLow()
         {
-            *(uint*)_sckAddress = _sck_clear; // low
+            // this seems convoluted, but it is intentionally so to keep the compiler from optimizing out out timing.
+            // A single call takes roughly 0.2us, but the part requires a minimum of 0.25us for the ADC to settle.  
+            // We don't have a simple micro-sleep, so we simply make multiple calls to assert state to suck up the required timing
+            for (int i = 0; i < _timing_iterations; i++)
+            {
+                var val = 1u << (16 + _sck_pin); // low
+                * (uint*)_sck_address = val;
+            }
         }
 
         private unsafe void ClockHigh()
         {
-            *(uint*)_sckAddress = _sck_set; // high
+            for (int i = 0; i < _timing_iterations; i++)
+            {
+                var val = 1u << _sck_pin; // high
+                *(uint*)_sck_address = val;
+            }
         }
 
-        private unsafe ulong ReadData(int cycles)
+        private unsafe uint ReadADC()
         {
-            ulong count = 0;
+            uint count = 0;
 
             lock (SyncRoot)
             {
-                for (int i = 0; i < cycles; i++)
+                // data line low indicates ready
+                while((*(uint*)_dout_address & _dout_mask) != 0)
                 {
-                    ClockHigh();
-                    ClockHigh();
+                    Thread.Sleep(0);
+                }
+
+                for (int i = 0; i < 24; i++) // 24 bits of data
+                {
                     ClockHigh();
                     count = count << 1;
                     ClockLow();
-                    ClockLow();
-                    ClockLow();
 
-                    if ((*(uint*)_doutAddress & _dout_mask) != 0) // read DOUT state
+                    if ((*(uint*)_dout_address & _dout_mask) != 0) // read DOUT state
                     {
                         count++;
                     }
                 }
+                count ^= 0x800000;
+
+                //set the gain for the next reading
+                for (int i = 0; i < (int)Gain; i++)
+                {
+                    ClockHigh();
+                    ClockLow();
+                }
             }
-            count = count ^ 0x800000;
+
             return count;
         }
 
         protected virtual void Dispose(bool disposing)
         {
-            _isDisposed = true;
+            if (_createdPorts)
+            {
+                _sck.Dispose();
+                _dout.Dispose();
+            }
+            IsDisposed = true;
         }
 
         /// <summary>
