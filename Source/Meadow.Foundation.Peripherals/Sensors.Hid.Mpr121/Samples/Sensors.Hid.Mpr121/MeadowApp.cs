6bd5191efb01d511198c5a194d7c7007d7a14b29
adrianstevens <adrian.stevens@gmail.com>
2020-07-21 22:12:15 -0700

    Add Mpr121 driver, sample & datasheet - WIP

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Hid.Mpr121/Samples/Sensors.Hid.Mpr121/MeadowApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Hid;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        Mpr121 sensor;
+
+        public MeadowApp()
+        {
+            Init();
+
+        }
+
+        public void Init()
+        {
+            Console.WriteLine("Init...");
+
+            sensor = new Mpr121(Device.CreateI2cBus(Meadow.Hardware.I2cBusSpeed.Standard), 90, 100);
+            sensor.ChannelStatusesChanged += Sensor_ChannelStatusesChanged;
+        }
+
+        private void Sensor_ChannelStatusesChanged(object sender, ChannelStatusChangedEventArgs e)
+        {
+            Console.WriteLine("Touched");
+        }
+    }
+}
