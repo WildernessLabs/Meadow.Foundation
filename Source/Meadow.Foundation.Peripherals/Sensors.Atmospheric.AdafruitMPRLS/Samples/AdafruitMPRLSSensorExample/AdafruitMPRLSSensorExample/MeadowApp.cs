2333b4cb4631ccc5d2fe65012d0cedd6cb5dd0b7
austin <three.sixteen@gmail.com>
2020-10-18 16:36:56 -0700

    Adding device driver for Adafruit MPRLS sensor, example code to use it, and datasheet.

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.AdafruitMPRLS/Samples/AdafruitMPRLSSensorExample/AdafruitMPRLSSensorExample/MeadowApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Atmospheric;
+
+namespace AdafruitMPRLSSensorExample
+{
+    /// <summary>
+    /// Connect VIN to 3.3v
+    /// Connect GND to ground
+    /// Connect SCL to pin D08
+    /// Connect SDA to pin D07
+    /// </summary>
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        AdafruitMPRLSSensor PressureSensor;
+
+        public MeadowApp()
+        {
+            Initialize();
+        }
+
+        void Initialize()
+        {
+            PressureSensor = new AdafruitMPRLSSensor(Device.CreateI2cBus());
+
+            PressureSensor.StartUpdating();
+
+            PressureSensor.Updated += PressureSensor_Updated;
+        }
+
+        private void PressureSensor_Updated(object sender, Meadow.Peripherals.Sensors.Atmospheric.AtmosphericConditionChangeResult e)
+        {
+            Console.WriteLine($"new pressure PSI: {e.New.Pressure}, old pressure PSI: {e.Old.Pressure}");
+
+            Console.WriteLine($"pressure in hPA: {PressureSensor.CalculatedhPAMeasurement}");
+
+            Console.WriteLine($"raw sensor value: {PressureSensor.RawPSIMeasurement}");
+        }
+    }
+}
