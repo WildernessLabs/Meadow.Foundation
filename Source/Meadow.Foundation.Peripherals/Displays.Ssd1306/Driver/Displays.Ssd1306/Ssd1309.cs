bd66ed250af255cca54b121aaad455ad68017da7
Adrian Stevens <adrian.stevens@gmail.com>
2020-11-15 17:49:33 -0800

    A couple of comment fixes for Ssd1309

--- Source/Meadow.Foundation.Peripherals/Displays.Ssd1306/Driver/Displays.Ssd1306/Ssd1309.cs
+++ Source/Meadow.Foundation.Peripherals/Displays.Ssd1306/Driver/Displays.Ssd1306/Ssd1309.cs
@@ -6,14 +6,13 @@ namespace Meadow.Foundation.Displays
     public class Ssd1309 : Ssd1306
     {
         /// <summary>
-        ///     Create a new SSD1306 object using the default parameters for
+        ///     Create a new SSD1309 object using the default parameters for
         /// </summary>
         /// <remarks>
         ///     Note that by default, any pixels out of bounds will throw and exception.
         ///     This can be changed by setting the <seealso cref="IgnoreOutOfBoundsPixels" />
         ///     property to true.
         /// </remarks>
-        /// <param name="displayType">Type of SSD1306 display (default = 128x64 pixel display).</param>
         ///
         public Ssd1309(IIODevice device, ISpiBus spiBus, IPin chipSelectPin, IPin dcPin, IPin resetPin) :
             base(device, spiBus, chipSelectPin, dcPin, resetPin, DisplayType.OLED128x64)
