632bb4b39238a8fdffbf93d2896f396becf23764
Adrian Stevens <adrian.stevens@gmail.com>
2020-12-06 22:11:58 -0800

    Add Mag3110 sample

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Motion.Mag3110/Samples/Sensors.Motion.Mag3110_Sample/MeadowApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Motion;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        Mag3110 sensor;
+
+        public MeadowApp()
+        {
+            InitHardware();
+
+            while (true)
+            {
+                sensor.Read();
+
+                Console.WriteLine($"X: {sensor.X}, Y: {sensor.Y}, Z: {sensor.Z}");
+                Console.WriteLine($"Temperature: {sensor.Temperature}");
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void InitHardware()
+        {
+            Console.WriteLine("Init...");
+
+            sensor = new Mag3110(Device.CreateI2cBus());
+        }
+    }
+}
