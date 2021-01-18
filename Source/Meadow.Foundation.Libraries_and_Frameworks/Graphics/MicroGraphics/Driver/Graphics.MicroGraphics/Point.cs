12a50c294d12595b1be05516c3726de2f8cfb664
Adrian Stevens <adrian.stevens@gmail.com>
2021-01-18 12:53:55 -0800

    Add Point struct

---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/MicroGraphics/Driver/Graphics.MicroGraphics/Point.cs
@@ -0,0 +1,33 @@
+﻿namespace MicroGraphics
+{
+    public struct Point
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public bool IsEmpty => X == 0 && Y == 0;
+
+        public Point (int x = 0, int y = 0)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Offset(int x, int y)
+        {
+            X += x;
+            Y += y;
+        }
+
+        public void Offset(Point point)
+        {
+            X += point.X;
+            Y += point.Y;
+        }
+
+        public override string ToString()
+        {
+            return $"X: {X}, Y: {Y}";
+        }
+    }
+}
