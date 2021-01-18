ad4676f10fa57d4ac1284b9f17a87c4d0d66f404
Adrian Stevens <adrian.stevens@gmail.com>
2021-01-18 13:20:32 -0800

    Add simple Path class and improve Point class

---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/MicroGraphics/Driver/Graphics.MicroGraphics/Path.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MicroGraphics
+{
+    public class Path
+    {
+        List<Point> points = new List<Point>();
+
+        public List<Point> Points => points;
+
+        public Path()
+        {
+        }
+
+        public Path(Point[] points)
+        {
+            AddPoints(points);
+        }
+
+        public void AddPoint(int x, int y)
+        {
+            points.Add(new Point(x, y));
+        }
+
+        public void AddPoint(Point point)
+        {
+            points.Add(point);
+        }
+
+        public void AddPoints(Point[] points)
+        {
+            foreach (var p in points)
+            {
+                this.points.Add(p);
+            }
+        }
+
+        public void AddPath(Path path)
+        {
+            AddPoints(path.Points.ToArray());
+        }
+
+        public void ClosePath()
+        {
+            if(points.Count < 2)
+            {
+                return;
+            }
+
+            if(points[0] != points.Last())
+            {
+                points.Add(points[0]);
+            }
+        }
+    }
+}
--- Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/MicroGraphics/Driver/Graphics.MicroGraphics/Point.cs
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/MicroGraphics/Driver/Graphics.MicroGraphics/Point.cs
@@ -2,8 +2,8 @@
 {
     public struct Point
     {
-        public int X { get; private set; }
-        public int Y { get; private set; }
+        public int X { get; set; }
+        public int Y { get; set; }
 
         public bool IsEmpty => X == 0 && Y == 0;
 
@@ -23,6 +23,62 @@
         {
             X += point.X;
             Y += point.Y;
+        }
+
+        /// <summary>
+        /// Translates the specified Point by the specified Size.
+        /// </summary>
+        /// <param name="point">
+        /// The <see cref="Point"/> instance to translate.
+        /// </param>
+        /// <param name="size">
+        /// The <see cref="Size"/> instance to translate point with.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="Point"/> instance translated by size.
+        /// </returns>
+        public static Point operator +(Point point, Point amount)
+        {
+            return new Point(point.X + amount.X, point.Y + amount.Y);
+        }
+
+        /// <summary>
+        /// Translates the specified Point by the negative of the specified Size.
+        /// </summary>
+        /// <param name="point">
+        /// The <see cref="Point"/> instance to translate.
+        /// </param>
+        /// <param name="size">
+        /// The <see cref="Size"/> instance to translate point with.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="Point"/> instance translated by size.
+        /// </returns>
+        public static Point operator -(Point point, Point amount)
+        {
+            return new Point(point.X - amount.X, point.Y - amount.Y);
+        }
+
+        /// <summary>
+        /// Compares two instances for equality.
+        /// </summary>
+        /// <param name="left">The first instance.</param>
+        /// <param name="right">The second instance.</param>
+        /// <returns>True, if left is equal to right; false otherwise.</returns>
+        public static bool operator ==(Point left, Point right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Compares two instances for inequality.
+        /// </summary>
+        /// <param name="left">The first instance.</param>
+        /// <param name="right">The second instance.</param>
+        /// <returns>True, if left is not equal to right; false otherwise.</returns>
+        public static bool operator !=(Point left, Point right)
+        {
+            return !left.Equals(right);
         }
 
         public override string ToString()
