1cbdc1b6f7555b653a7b4b096ddcded8c9a1ebe3
ctacke <ctacke@opennetcf.com>
2021-01-31 10:39:06 -0600

    Oops, forgot a file!

---
+++ Source/Meadow.Foundation.Core/Exceptions/DeviceConfigurationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Meadow.Foundation
+{
+    /// <summary>
+    /// Exception thrown when a device or peripheral is misconfigured for a requested action, activity or behavior.
+    /// </summary>
+    public class DeviceConfigurationException : Exception
+    {
+        public DeviceConfigurationException(string message)
+            : base(message)
+        {
+        }
+    }
+}
