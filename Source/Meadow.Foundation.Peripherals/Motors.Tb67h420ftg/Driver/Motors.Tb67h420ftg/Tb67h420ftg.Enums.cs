0e172768ffc1b5dc738aa95cfd713cee2d67df8a
bryan costanich <b@wowzer.net>
2021-02-05 11:59:22 -0800

    Driver/tb67h420ftg (#147)
    * Iniital stub of Tb67h420ftg driver
    
    * First cut of the driver, working!
    
    * Getting closer. fault pins in. but meadow bug.
    
    * Added some docs
    
    * more small docs
    
    * updated to new resistor mode enum

---
+++ Source/Meadow.Foundation.Peripherals/Motors.Tb67h420ftg/Driver/Motors.Tb67h420ftg/Tb67h420ftg.Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Meadow.Foundation.Motors
+{
+	public partial class Tb67h420ftg
+	{
+		public enum HBridgeMode
+		{
+			/// <summary>
+            /// Both H-Bridges are controlled separately.
+            /// </summary>
+			Dual,
+			/// <summary>
+            /// Both H-Bridges are controlled as one.
+            /// </summary>
+			Single
+		}
+	}
+}
---
+++ Source/Meadow.Foundation.Peripherals/Motors.Tb67h420ftg/Driver/Motors.Tb67h420ftg/Tb67h420ftg.Motor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Meadow.Hardware;
+using Meadow.Peripherals.Motors;
+
+namespace Meadow.Foundation.Motors
+{
+    public partial class Tb67h420ftg
+    {
+        public class Motor : HBridgeMotor
+        {
+            /// <summary>
+            /// Raised when the motors is over current.
+            /// </summary>
+            public event EventHandler<MotorOvercurrentEventArgs> MotorOvercurrentFault = delegate { };
+
+            protected Tb67h420ftg driver;
+            protected IDigitalInputPort? fault;
+
+            internal Motor(
+                Tb67h420ftg driver, IPwmPort in1, IPwmPort in2,
+                IDigitalOutputPort enable, IDigitalInputPort? fault)
+                : base(in1, in2, enable)
+            {
+                this.driver = driver;
+                this.fault = fault;
+
+                Init();
+            }
+
+            protected void Init() {
+                // wire up the fault event, if the LOx port is configured.
+                if (fault != null) {
+                    fault.Changed += (object sender, DigitalInputPortEventArgs e) => {
+                        this.RaiseMotorOvercurrentFault();
+                    };
+                }
+            }
+
+            /// <summary>
+            /// Raises the MotorOvercurrentFault event.
+            /// </summary>
+            protected void RaiseMotorOvercurrentFault()
+            {
+                MotorOvercurrentEventArgs args = new MotorOvercurrentEventArgs();
+                this.MotorOvercurrentFault(this, args);
+            }
+        }
+    }
+}
