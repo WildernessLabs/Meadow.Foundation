0ef0d297d99a2bdf96d1d5e5a09f9bd2be3d3a28
bryan costanich <b@wowzer.net>
2021-02-15 20:56:32 -0800

    Added WindVane driver and sample.

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Weather.WindVane/Driver/Sensors.Weather.WindVane/WindVane.ChangeResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Meadow;
+using Meadow.Units;
+
+namespace Meadow.Foundation.Sensors.Weather
+{
+	public partial class WindVane
+	{
+		public class WindVaneChangeResult : INumericChangeResult<Azimuth>
+		{
+			public Azimuth New { get; set; }
+			public Azimuth Old { get; set; }
+
+			public Azimuth DeltaPercent => (Old > 0) ? Delta / Old : 0;
+
+			public Azimuth Delta => New - Old;
+
+			public WindVaneChangeResult()
+			{
+			}
+		}
+	}
+}
