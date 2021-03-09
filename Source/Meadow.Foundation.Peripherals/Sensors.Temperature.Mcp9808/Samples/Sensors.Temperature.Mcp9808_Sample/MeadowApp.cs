696248b65f11e990d3fb50f613d10b30e2c5d2d1
Adrian Stevens <adrian.stevens@gmail.com>
2021-03-08 23:12:00 -0800

    Add Mcp9808 driver, sample & datasheet

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Temperature.Mcp9808/Samples/Sensors.Temperature.Mcp9808_Sample/MeadowApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Temperature;
+
+namespace Sensors.Temperature.TMP102_Sample
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        Mcp9808 sensor;
+
+        public MeadowApp()
+        {
+            InitHardware();
+
+            sensor.StartUpdating();
+        }
+
+        public void InitHardware()
+        {
+            Console.WriteLine("Init Mcp9808...");
+
+            sensor = new Mcp9808(Device.CreateI2cBus());
+
+            Console.WriteLine("Mcp9808 created");
+
+            Console.WriteLine($"Temp: {sensor.Temperature}");
+
+            sensor.Updated += Sensor_Updated;
+        }
+
+        private void Sensor_Updated(object sender, Meadow.Peripherals.Sensors.Atmospheric.AtmosphericConditionChangeResult e)
+        {
+            Console.WriteLine($"Temp: {e.New.Temperature}");
+        }
+    }
+}
