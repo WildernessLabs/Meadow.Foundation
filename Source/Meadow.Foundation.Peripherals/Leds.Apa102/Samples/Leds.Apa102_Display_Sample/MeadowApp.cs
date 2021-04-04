b72ed3a7e48e90b168c231ae7e9fd355a09ad9d8
bryan costanich <b@wowzer.net>
2021-04-04 14:42:31 -0700

    made the demo app a little more fun.

--- Source/Meadow.Foundation.Peripherals/Leds.Apa102/Samples/Leds.Apa102_Display_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Leds.Apa102/Samples/Leds.Apa102_Display_Sample/MeadowApp.cs
@@ -12,28 +12,63 @@ namespace Leds.Apa102_Display_Sample
     {
         Apa102 display;
 
-        GraphicsLibrary graphics;
+        GraphicsLibrary canvas;
 
         public MeadowApp()
         {
             Initialize();
-        
+
+            canvas.CurrentFont = new Font4x8();
+
+            while (true) {
+                canvas.Clear();
+                canvas.DrawText(0, 1, "MEADOW", Colors.AzureBlue);
+                canvas.DrawText(24, 1, "F7", Colors.ChileanFire);
+                canvas.Show();
+
+                Thread.Sleep(1000);
+
+                canvas.Clear();
+                canvas.DrawText(0, 1, "Rocks", Colors.PearGreen);
+                canvas.Show();
+
+                Thread.Sleep(1000);
+            }
         }
 
         void Initialize()
         {
             Console.WriteLine("Initialize hardware...");
 
-            display = new Apa102(Device.CreateSpiBus(), Apa102.PixelOrder.BGR, false, 32, 8);
+            display = new Apa102(Device.CreateSpiBus(48000), Apa102.PixelOrder.BGR, false, 32, 8);
+            canvas = new GraphicsLibrary(display);
 
-            graphics = new GraphicsLibrary(display);
+            Console.WriteLine("Hardware intitialized.");
+        }
 
-            graphics.CurrentFont = new Font4x8();
-
-            graphics.Clear();
-            graphics.DrawText(0, 1, "MEADOW", Color.FromRgb(0, 0.1, 0.1));
-            graphics.DrawText(24, 1, "F7", Color.FromRgb(0.1, 0, 0));
-            graphics.Show();
+        static class Colors
+        {
+            public static Color AzureBlue {
+                get {
+                    var azureBlue = Color.FromHex("#23abe3");
+                    // make it way less bright
+                    return Color.FromHsba(azureBlue.Hue, azureBlue.Saturation, 0.025);
+                }
+            }
+            public static Color ChileanFire {
+                get {
+                    var chileanFire = Color.FromHex("#ef7d3b");
+                    // make it way less bright
+                    return Color.FromHsba(chileanFire.Hue, chileanFire.Saturation, 0.025);
+                }
+            }
+            public static Color PearGreen {
+                get {
+                    var PearGreen = Color.FromHex("#c9db31");
+                    // make it way less bright
+                    return Color.FromHsba(PearGreen.Hue, PearGreen.Saturation, 0.025);
+                }
+            }
         }
     }
 }
