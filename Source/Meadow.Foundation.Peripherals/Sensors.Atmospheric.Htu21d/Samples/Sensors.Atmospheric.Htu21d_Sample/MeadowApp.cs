7136b3722d2ca6a68ecedd5d78147112306aad69
bryan costanich <b@wowzer.net>
2021-04-20 13:36:23 -0700

    Converted HTU21D app

--- Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Htu21d/Samples/Sensors.Atmospheric.Htu21d_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Htu21d/Samples/Sensors.Atmospheric.Htu21d_Sample/MeadowApp.cs
@@ -1,14 +1,68 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Meadow;
 using Meadow.Devices;
-//using Meadow.Foundation.Sensors.Atmospheric;
+using Meadow.Foundation.Sensors.Atmospheric;
 using Meadow.Hardware;
+using Meadow.Units;
 
 namespace MeadowApp
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
+        Htu21d htu21d;
+
+        public MeadowApp()
+        {
+            Console.WriteLine("Initializing...");
+
+            // create the I2C Bus
+            var i2cBus = Device.CreateI2cBus(I2cBusSpeed.Standard);
+
+            // create our device
+            htu21d = new Htu21d(i2cBus);
+
+            Console.WriteLine($"Chip Serial: {htu21d.SerialNumber}");
+
+            // get an initial reading
+            ReadConditions().Wait();
+
+            // start updating continuously
+            htu21d.StartUpdating();
+
+            //==== Events
+            // classical .NET events can also be used:
+            htu21d.Updated += (object sender, CompositeChangeResult<Temperature, RelativeHumidity> result) => {
+                Console.WriteLine($"  Temperature: {result.New.Value.Unit1.Celsius:F1}°C");
+                Console.WriteLine($"  Relative Humidity: {result.New.Value.Unit2.Value:F1}%");
+            };
+
+            //==== IObservable
+            var consumer = Htu21d.CreateObserver(
+                handler: result => {
+                    Console.WriteLine($"Observer triggered; new temp: {result.New.Value.Unit1.Celsius}, old: {result.Old.Value.Unit1.Celsius}");
+                },
+                filter: result => {
+                    return true;
+                    //return (
+                    //    (Math.Abs(result.Delta.Value.Unit1.Celsius) > 1)
+                    //    &&
+                    //    (Math.Abs(result.Delta.Value.Unit3.Bar) > 5)
+                    //    );
+                });
+            htu21d.Subscribe(consumer);
+        }
+
+        protected async Task ReadConditions()
+        {
+            var result = await htu21d.Read();
+            Console.WriteLine("Initial Readings:");
+            Console.WriteLine($"  Temperature: {result.Temperature.Celsius:F1}°C");
+            Console.WriteLine($"  Relative Humidity: {result.Humidity:F1}%");
+        }
+
+
         //Htu21d sensor;
 
         //public MeadowApp()
