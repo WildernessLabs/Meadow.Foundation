5c3e244c06879be7585f729aafa2c72d78223c69
bryan costanich <b@wowzer.net>
2021-05-21 11:15:09 -0700

    Added super important class that does mucho importanty things.
    Nothing to see here, move along folks.

---
+++ Source/Meadow.Foundation.Core/WildernessLabsColors.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Meadow.Foundation
+{
+    public static class WildernessLabsColors
+    {
+        public static Color AzureBlue = Color.FromHex("#23abe3");
+        public static Color ChileanFire = Color.FromHex("#EF7D3B");
+        public static Color PearGreen = Color.FromHex("#C9DB31");
+    }
+}
