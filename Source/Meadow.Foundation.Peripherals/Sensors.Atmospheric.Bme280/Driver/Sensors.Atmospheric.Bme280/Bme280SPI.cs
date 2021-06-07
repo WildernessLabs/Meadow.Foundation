1cbf4d85e769fef58664057a860a33190b109a11
bryan costanich <b@wowzer.net>
2021-06-07 12:06:52 -0700

    need to pass register address

--- Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Bme280/Driver/Sensors.Atmospheric.Bme280/Bme280SPI.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Bme280/Driver/Sensors.Atmospheric.Bme280/Bme280SPI.cs
@@ -17,14 +17,7 @@ namespace Meadow.Foundation.Sensors.Atmospheric
 
         public override void ReadRegisters(byte startRegister, Span<byte> readBuffer)
         {
-            // the buffer needs to be big enough for the output and response
-            //var buffer = new byte[readCount + 1];
-            //var bufferTx = new byte[readCount + 1];
-            //buffer[0] = startRegister;
-          
-            //var rx = _spi.ReceiveData(_chipSelect, readCount + 1);
-
-            spiPeripheral.Read(readBuffer);
+            spiPeripheral.ReadRegister(startRegister, readBuffer);
 
             // skip past the byte where we clocked out the register address
             for (int i = 1; i < readBuffer.Length; i++) {
