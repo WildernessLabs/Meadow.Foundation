699740507a6f3ff0a6da8956716d52a762ef0259
bryan costanich <b@wowzer.net>
2021-06-20 21:06:42 -0700

    update to new hotness

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Temperature.Tmp102/Driver/Sensors.Temperature.Tmp102/Tmp102.Resolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Meadow.Foundation.Sensors.Temperature
+{
+    public partial class Tmp102
+    {
+        /// <summary>
+        ///     Indicate the resolution of the sensor.
+        /// </summary>
+        public enum Resolution : byte
+        {
+            /// <summary>
+            ///     Operate in 12-bit mode.
+            /// </summary>
+            Resolution12Bits,
+
+            /// <summary>
+            ///     Operate in 13-bit mode.
+            /// </summary>
+            Resolution13Bits
+        }
+
+    }
+}
--- Source/Meadow.Foundation.Peripherals/Sensors.Temperature.Tmp102/Driver/Sensors.Temperature.Tmp102/Tmp102.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Temperature.Tmp102/Driver/Sensors.Temperature.Tmp102/Tmp102.cs
@@ -6,36 +6,19 @@ using Meadow.Peripherals.Sensors;
 
 namespace Meadow.Foundation.Sensors.Temperature
 {
+    // TODO: BC: this peripheral hasn't been tested since it got updated to the
+    // new hotness. I don't have one, but I will order one.
     /// <summary>
     /// TMP102 Temperature sensor object.
     /// </summary>    
-    public class Tmp102 :
-        SensorBase<Units.Temperature>,
-        ITemperatureSensor
+    public partial class Tmp102 : ByteCommsSensorBase<Units.Temperature>, ITemperatureSensor
     {
-        /// <summary>
-        ///     Indicate the resolution of the sensor.
-        /// </summary>
-        public enum Resolution : byte
-        {
-            /// <summary>
-            ///     Operate in 12-bit mode.
-            /// </summary>
-            Resolution12Bits,
+        //==== events
+        public event EventHandler<IChangeResult<Units.Temperature>> TemperatureUpdated = delegate { };
 
-            /// <summary>
-            ///     Operate in 13-bit mode.
-            /// </summary>
-            Resolution13Bits
-        }
-
+        //==== properties
         /// <summary>
-        ///     TMP102 sensor.
-        /// </summary>
-        private readonly II2cPeripheral tmp102;
-
-        /// <summary>
-        ///     Backing variable for the SensorResolution property.
+        /// Backing variable for the SensorResolution property.
         /// </summary>
         private Resolution _sensorResolution;
 
@@ -45,13 +28,16 @@ namespace Meadow.Foundation.Sensors.Temperature
         public Resolution SensorResolution {
             get { return _sensorResolution; }
             set {
-                var configuration = tmp102.ReadRegisters(0x01, 2);
+                Peripheral.ReadRegister(0x01, ReadBuffer.Span);
+                // TODO: Delete after testing
+                //var configuration = Peripheral.ReadRegisters(0x01, 2);
                 if (value == Resolution.Resolution12Bits) {
-                    configuration[1] &= 0xef;
+                    ReadBuffer.Span[1] &= 0xef;
                 } else {
-                    configuration[1] |= 0x10;
+                    ReadBuffer.Span[1] |= 0x10;
                 }
-                tmp102.WriteRegisters(0x01, configuration);
+                // @CTACKE: is there a better way here? do we need a WriteRegisters that takes a Span<byte>?
+                Peripheral.WriteRegisters(0x01, ReadBuffer.Span.ToArray());
                 _sensorResolution = value;
             }
         }
@@ -61,124 +47,45 @@ namespace Meadow.Foundation.Sensors.Temperature
         /// </summary>
         public Units.Temperature? Temperature { get; protected set; }
 
-        // internal thread lock
-        private object _lock = new object();
-        private CancellationTokenSource SamplingTokenSource;
-
-        /// <summary>
-        /// Gets a value indicating whether the analog input port is currently
-        /// sampling the ADC. Call StartSampling() to spin up the sampling process.
-        /// </summary>
-        /// <value><c>true</c> if sampling; otherwise, <c>false</c>.</value>
-        public bool IsSampling { get; protected set; } = false;
-
-        public event EventHandler<IChangeResult<Units.Temperature>> TemperatureUpdated = delegate { };
-
         /// <summary>
         ///     Create a new TMP102 object using the default configuration for the sensor.
         /// </summary>
         /// <param name="address">I2C address of the sensor.</param>
         public Tmp102(II2cBus i2cBus, byte address = 0x48)
+            : base(i2cBus, address, readBufferSize: 2, writeBufferSize: 2)
         {
-            tmp102 = new I2cPeripheral(i2cBus, address);
-
-            var configuration = tmp102.ReadRegisters(0x01, 2);
-
-            _sensorResolution = (configuration[1] & 0x10) > 0 ?
+            // TODO: Delete after testing
+            //var configuration = Peripheral.ReadRegisters(0x01, 2);
+            //_sensorResolution = (configuration[1] & 0x10) > 0 ?
+            //                     Resolution.Resolution13Bits : Resolution.Resolution12Bits;
+            Peripheral.ReadRegister(0x01, ReadBuffer.Span);
+            _sensorResolution = (ReadBuffer.Span[1] & 0x10) > 0 ?
                                  Resolution.Resolution13Bits : Resolution.Resolution12Bits;
-        }
-
-        /// <summary>
-        /// Convenience method to get the current sensor readings. For frequent reads, use
-        /// StartSampling() and StopSampling() in conjunction with the SampleBuffer.
-        /// </summary>
-        // TODO: Make this async?
-        public Units.Temperature Read()
-        {
-            Update();
-            return Temperature.Value;
-        }
-
-        /// <summary>
-		/// Begin reading temperature data
-		/// </summary>
-        public void StartUpdating(int standbyDuration = 1000)
-        {
-            // thread safety
-            lock (_lock) {
-                if (IsSampling) return;
-
-                // state muh-cheen
-                IsSampling = true;
-
-                SamplingTokenSource = new CancellationTokenSource();
-                CancellationToken ct = SamplingTokenSource.Token;
-
-                Units.Temperature? oldTemperature;
-                ChangeResult<Units.Temperature> result;
-                Task.Factory.StartNew(async () => 
-                {
-                    while (true) {
-                        if (ct.IsCancellationRequested) {
-                            // do task clean up here
-                            observers.ForEach(x => x.OnCompleted());
-                            break;
-                        }
-                        // capture history
-                        oldTemperature = Temperature;
-
-                        // read
-                        Update(); //syncrhnous for this driver 
-
-                        // build a new result with the old and new conditions
-                        result = new ChangeResult<Units.Temperature>(Temperature.Value, oldTemperature);
-
-                        // let everyone know
-                        RaiseChangedAndNotify(result);
-
-                        // sleep for the appropriate interval
-                        await Task.Delay(standbyDuration);
-                    }
-                }, SamplingTokenSource.Token);
-            }
-        }
-
-        /// <summary>
-        /// Stops sampling the temperature.
-        /// </summary>
-        public void StopUpdating()
-        {
-            lock (_lock) 
-            {
-                if (!IsSampling) { return; }
-
-                SamplingTokenSource?.Cancel();
-
-                IsSampling = false;
-            }
         }
 
         /// <summary>
         /// Update the Temperature property.
         /// </summary>
-        public void Update()
+        protected override Task<Units.Temperature> ReadSensor()
         {
-            var temperatureData = tmp102.ReadRegisters(0x00, 2);
+            Peripheral.ReadRegister(0x00, ReadBuffer.Span);
+            // TODO: Delete after testing
+            //var temperatureData = Peripheral.ReadRegisters(0x00, 2);
 
             var sensorReading = 0;
             if (SensorResolution == Resolution.Resolution12Bits) {
-                sensorReading = (temperatureData[0] << 4) | (temperatureData[1] >> 4);
+                sensorReading = (ReadBuffer.Span[0] << 4) | (ReadBuffer.Span[1] >> 4);
             } else {
-                sensorReading = (temperatureData[0] << 5) | (temperatureData[1] >> 3);
+                sensorReading = (ReadBuffer.Span[0] << 5) | (ReadBuffer.Span[1] >> 3);
             }
 
-            Temperature = new Units.Temperature((float)(sensorReading * 0.0625), Units.Temperature.UnitType.Celsius);            
+            return Task.FromResult(new Units.Temperature((float)(sensorReading * 0.0625), Units.Temperature.UnitType.Celsius));
         }
 
         protected void RaiseChangedAndNotify(IChangeResult<Units.Temperature> changeResult)
         {
             TemperatureUpdated?.Invoke(this, changeResult);
-            base.NotifyObservers(changeResult);
+            base.RaiseEventsAndNotify(changeResult);
         }
     }
 }
