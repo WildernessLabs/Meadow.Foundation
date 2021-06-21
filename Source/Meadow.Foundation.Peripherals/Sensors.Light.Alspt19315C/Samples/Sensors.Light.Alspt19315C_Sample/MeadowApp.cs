a1380dd03e52273ae7cfa839cd8290cc2d140e10
bryan costanich <b@wowzer.net>
2021-06-21 14:18:11 -0700

    updated to new hotness

--- Source/Meadow.Foundation.Peripherals/Sensors.Light.Alspt19315C/Samples/Sensors.Light.Alspt19315C_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Light.Alspt19315C/Samples/Sensors.Light.Alspt19315C_Sample/MeadowApp.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation.Sensors.Light;
+using Meadow.Units;
 
 namespace MeadowApp
 {
@@ -13,23 +14,48 @@ namespace MeadowApp
 
         public MeadowApp()
         {
-            Init();
+            Console.WriteLine("Initializing...");
 
-            Task.Run(async () =>
-            {
-                while(true)
-                {
-                    Console.WriteLine($"Voltage: {sensor.Read().Result.Volts}V");
-                    await Task.Delay(500);
+            // configure our sensor
+            sensor = new Alspt19315C(Device, Device.Pins.A03);
+
+            //==== IObservable Pattern with an optional notification filter.
+            // Example that uses an IObersvable subscription to only be notified
+            // when the voltage changes by at least 0.5V
+            var consumer = Alspt19315C.CreateObserver(
+                handler: result => {
+                    Console.WriteLine($"Observer filter satisfied: {result.New.Volts:N2}V, old: {result.Old?.Volts:N2}V");
+                },
+                // only notify if the change is greater than 0.5V
+                filter: result => {
+                    if (result.Old is { } old) { //c# 8 pattern match syntax. checks for !null and assigns var.
+                        return (result.New - old).Abs().Volts > 0.5; // returns true if > 0.5V change.
+                    }
+                    return false;
                 }
-            });
+                // if you want to always get notified, pass null for the filter:
+                //filter: null
+            );
+            sensor.Subscribe(consumer);
+
+            //==== Classic Events Pattern
+            // classical .NET events can also be used:
+            sensor.Updated += (object sender, IChangeResult<Voltage> result) => {
+                Console.WriteLine($"Voltage Changed, new: {result.New.Volts:N2}V, old: {result.Old?.Volts:N2}V");
+            };
+
+            //==== One-off reading use case/pattern
+            ReadTemp().Wait();
+
+            // Spin up the sampling thread so that events are raised and
+            // IObservable notifications are sent.
+            sensor.StartUpdating(TimeSpan.FromMilliseconds(1000));
         }
 
-        public void Init()
+        protected async Task ReadTemp()
         {
-            Console.WriteLine("Init...");
-
-            sensor = new Alspt19315C(Device, Device.Pins.A01);
+            var result = await sensor.Read();
+            Console.WriteLine($"Initial temp: {result.Volts:N2}V");
         }
     }
 }
