93905d6ac90606cf49a25bcc7ea6e4b59e53dac6
bryan costanich <b@wowzer.net>
2021-06-21 13:08:52 -0700

    updated to new hotness
    note: my sensor is giving me very low lux. sensor may be bad.  needs more testing.

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Light.Veml7700/Driver/Sensors.Light.Veml7700/Veml7700.SensorTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Meadow.Foundation.Sensors.Light
+{
+    public partial class Veml7700
+    {
+        public enum SensorTypes
+        {
+            Ambient,
+            White
+        }
+    }
+}
