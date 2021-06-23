30d9f6de3ab2128977f4ef7cc9e2fadd1d6add3b
bryan costanich <b@wowzer.net>
2021-06-23 11:16:13 -0700

    Mag3110 full cleanup pass. needs testing

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Motion.Mag3110/Driver/Sensors.Motion.Mag3110/Mag3110.Registers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Meadow.Foundation.Sensors.Motion
+{
+    public partial class Mag3110
+    {
+        /// <summary>
+        /// Register addresses in the sensor.
+        /// </summary>
+        private static class Registers
+        {
+            public const byte DR_STATUS = 0x00;
+            public const byte X_MSB = 0x01;
+            public const byte X_LSB = 0x02;
+            public const byte Y_MSB = 0x03;
+            public const byte Y_LSB = 0x04;
+            public const byte Z_MSB = 0x05;
+            public const byte Z_LSB = 0x06;
+            public const byte WHO_AM_I = 0x07;
+            public const byte SYSTEM_MODE = 0x08;
+            public const byte X_OFFSET_MSB = 0x09;
+            public const byte X_OFFSET_LSB = 0x0a;
+            public const byte Y_OFFSET_MSB = 0x0b;
+            public const byte Y_OFFSET_LSB = 0x0c;
+            public const byte Z_OFFSET_MSB = 0x0d;
+            public const byte Z_OFFSET_LSB = 0x0e;
+            public const byte TEMPERATURE = 0x0f;
+            public const byte CONTROL_1 = 0x10;
+            public const byte CONTROL_2 = 0x11;
+        }
+    }
+}
--- Source/Meadow.Foundation.Peripherals/Sensors.Motion.Mag3110/Samples/Sensors.Motion.Mag3110_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Motion.Mag3110/Samples/Sensors.Motion.Mag3110_Sample/MeadowApp.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Threading;
+using System.Threading.Tasks;
 using Meadow;
+using Meadow.Units;
+using MU = Meadow.Units.MagneticField.UnitType;
 using Meadow.Devices;
 using Meadow.Foundation.Sensors.Motion;
 
@@ -12,27 +14,61 @@ namespace MeadowApp
 
         public MeadowApp()
         {
-            InitHardware();
+            Console.WriteLine("Initializing");
 
-            while (true)
-            {
-                sensor.Read();
-
-                Console.WriteLine($"X: {sensor.MagneticField3d.X}, " +
-                    $"Y: {sensor.MagneticField3d.Y}, " +
-                    $"Z: {sensor.MagneticField3d.Z}");
-
-                Console.WriteLine($"Temperature: {sensor.Temperature.Celsius}");
-
-                Thread.Sleep(1000);
-            }
-        }
-
-        public void InitHardware()
-        {
-            Console.WriteLine("Init...");
-
+            // if using the other part, you can pass Mag3110.Addresses.Fxms3110 for the address
             sensor = new Mag3110(Device.CreateI2cBus());
+
+            //==== Events
+            // classical .NET events can also be used:
+            sensor.Updated += (sender, result) => {
+                Console.WriteLine($"Accel: [X:{result.New.MagneticField3D.X.MicroTesla:N2}," +
+                    $"Y:{result.New.MagneticField3D.Y.MicroTesla:N2}," +
+                    $"Z:{result.New.MagneticField3D.Z.MicroTesla:N2} (mps^2)]");
+
+                Console.WriteLine($"Temp: {result.New.Temperature.Celsius:N2}C");
+            };
+
+            //==== IObservable 
+            // Example that uses an IObersvable subscription to only be notified
+            // when the filter is satisfied
+            var consumer = Mag3110.CreateObserver(
+                handler: result => {
+                    Console.WriteLine($"Observer: [x] changed by threshold; new [x]: X:{result.New.MagneticField3D.X.MicroTesla:N2}, old: X:{result.Old?.MagneticField3D.X.MicroTesla:N2}");
+                },
+                // only notify if there's a greater than 1 micro tesla on the Y axis
+                filter: result => {
+                    if (result.Old is { } old) { //c# 8 pattern match syntax. checks for !null and assigns var.
+                        return ((result.New.MagneticField3D - old.MagneticField3D).Y > new MagneticField(1, MU.MicroTesla));
+                    }
+                    return false;
+                }
+                // if you want to always get notified, pass null for the filter:
+                //filter: null
+                );
+            sensor.Subscribe(consumer);
+
+            //==== one-off read
+            ReadConditions().Wait();
+
+            // start updating
+            sensor.StartUpdating(TimeSpan.FromMilliseconds(500));
         }
+
+        protected async Task ReadConditions()
+        {
+            var result = await sensor.Read();
+            Console.WriteLine("Initial Readings:");
+            Console.WriteLine($"Accel: [X:{result.MagneticField3D.X.MicroTesla:N2}," +
+                $"Y:{result.MagneticField3D.Y.MicroTesla:N2}," +
+                $"Z:{result.MagneticField3D.Z.MicroTesla:N2} (mps^2)]");
+
+            Console.WriteLine($"Angular Accel: [X:{result.MagneticField3D.X.MicroTesla:N2}," +
+                $"Y:{result.MagneticField3D.Y.MicroTesla:N2}," +
+                $"Z:{result.MagneticField3D.Z.MicroTesla:N2} (dps^2)]");
+
+            Console.WriteLine($"Temp: {result.Temperature.Celsius:N2}C");
+        }
+
     }
 }
