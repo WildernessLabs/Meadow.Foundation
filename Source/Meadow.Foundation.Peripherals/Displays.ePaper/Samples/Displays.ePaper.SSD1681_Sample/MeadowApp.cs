92781b35941122d596482651fed4e1d8c5b116fe
Adrian Stevens <adrian.stevens@gmail.com>
2021-08-23 20:32:00 -0700

    Add missing ePaper SSD1681 sample

---
+++ Source/Meadow.Foundation.Peripherals/Displays.ePaper/Samples/Displays.ePaper.SSD1681_Sample/MeadowApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Displays.ePaper;
+using Meadow.Foundation.Graphics;
+
+namespace Displays.ePaper.SSD1681_Sample
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        //<!—SNIP—>
+
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize ...");
+ 
+            var display = new Ssd1681(device: Device,
+                spiBus: Device.CreateSpiBus(),
+                chipSelectPin: Device.Pins.D02,
+                dcPin: Device.Pins.D01,
+                resetPin: Device.Pins.D00,
+                busyPin: Device.Pins.D03,
+                width: 200,
+                height: 200);
+
+            var graphics = new GraphicsLibrary(display);
+
+           graphics.DrawRectangle(1, 1, 126, 32, Meadow.Foundation.Color.Black);
+
+            graphics.CurrentFont = new Font8x12();
+            graphics.DrawText(2, 2, "SSD1681", Meadow.Foundation.Color.Black);
+            graphics.DrawText(2, 20, "Meadow F7", Meadow.Foundation.Color.Black);
+
+            graphics.Show();
+        }
+
+        //<!—SNOP—>
+    }
+}
