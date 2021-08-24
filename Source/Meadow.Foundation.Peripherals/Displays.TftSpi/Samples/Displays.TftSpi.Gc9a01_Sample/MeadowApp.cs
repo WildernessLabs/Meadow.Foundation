5dfb2f9ee4001810be98bbb77c4b2a8a78759b98
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-08-23 21:12:50 -0700

    Add missing samples for TftSpi package

---
+++ Source/Meadow.Foundation.Peripherals/Displays.TftSpi/Samples/Displays.TftSpi.Gc9a01_Sample/MeadowApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Displays.TftSpi;
+using Meadow.Foundation.Graphics;
+using Meadow.Hardware;
+
+namespace Displays.Tft.Gc9a01_Sample
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        //<!—SNIP—>
+
+        GraphicsLibrary graphics;
+
+        public MeadowApp()
+        {
+            Console.WriteLine("Initializing ...");
+
+            var config = new SpiClockConfiguration(12000, SpiClockConfiguration.Mode.Mode0);
+            var spiBus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, config);
+
+            Console.WriteLine("Create display driver instance");
+
+            var display = new Gc9a01
+            (
+                device: Device, 
+                spiBus: spiBus,
+                resetPin: Device.Pins.D00,
+				chipSelectPin: Device.Pins.D02,
+                dcPin: Device.Pins.D01
+            );
+
+            graphics = new GraphicsLibrary(display);
+
+            graphics.CurrentFont = new Font12x20();
+            graphics.Clear();
+            graphics.DrawTriangle(10, 10, 50, 50, 10, 50, Meadow.Foundation.Color.Red);
+            graphics.DrawRectangle(20, 15, 40, 20, Meadow.Foundation.Color.Yellow, false);
+            graphics.DrawCircle(50, 50, 40, Meadow.Foundation.Color.Blue, false);
+            graphics.DrawText(5, 5, "Meadow F7");
+            graphics.Show();
+        }
+
+        //<!—SNOP—>
+    }
+}
