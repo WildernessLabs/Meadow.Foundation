fc882ffb50a552dc4333660dbe0fd768f66e2c71
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-09-05 00:35:31 -0700

    Add missing\fix samples

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Dhtxx/Samples/Dht10_Sample/MeadowApp.cs
@@ -0,0 +1,21 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Atmospheric.Dhtxx;
+
+namespace Dht10_Sample
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        Dht10 dht10;
+
+        public MeadowApp()
+        {
+            Initialize();            
+        }
+
+        void Initialize()
+        {
+            dht10 = new Dht10(Device.CreateI2cBus());
+        }
+    }
+}
