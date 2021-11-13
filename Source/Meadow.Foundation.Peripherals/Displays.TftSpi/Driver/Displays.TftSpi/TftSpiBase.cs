c3f08a9beeccc1b15cab8decf86f17cd3d317e85
Adrian Stevens <adrian.stevens@gmail.com>
2021-11-12 17:06:24 -0800

    Update TftSpi to ignore out-of-bounds pixels #209

--- Source/Meadow.Foundation.Peripherals/Displays.TftSpi/Driver/Displays.TftSpi/TftSpiBase.cs
+++ Source/Meadow.Foundation.Peripherals/Displays.TftSpi/Driver/Displays.TftSpi/TftSpiBase.cs
@@ -230,8 +230,9 @@ namespace Meadow.Foundation.Displays.TftSpi
 
         private void SetPixel(int x, int y, ushort color)
         {
-            //  if (x < 0 || y < 0 || x >= width || y >= height)
-            //  { return; }
+            if (IgnoreOutOfBoundsPixels &&
+                x < 0 || y < 0 || x >= Width || y >= Height)
+            { return; }
 
             if (colorMode == ColorType.Format16bppRgb565)
             {
