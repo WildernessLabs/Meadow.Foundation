4d597bb0fdb620c8e4ddf46db14acf70a26379e5
ctacke <ctacke@opennetcf.com>
2021-12-29 12:35:59 -0600

    Starting work on Image

---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/MicroGraphics/Driver/Graphics.MicroGraphics/Image.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+
+namespace Meadow.Foundation.Graphics
+{
+    public partial class Image
+    {
+        public byte[] PixelData { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public ColorType ColorType { get; private set; } // TODO: determine this from color depth
+        public int BitsPerPixel { get; private set; }
+
+        public static Image LoadFromFile(string path)
+        {
+            var fi = new FileInfo(path);
+            if (!fi.Exists) throw new FileNotFoundException();
+
+            using (var reader = fi.OpenRead())
+            {
+                return new Image(reader);
+            }
+        }
+
+        public static Image LoadFromResource(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Image(Stream source)
+        {
+            // determine type
+            var buffer = new byte[2];
+
+            var header = source.Read(buffer, 0, 2);
+
+            switch (BitConverter.ToUInt16(buffer))
+            {
+                case 0x4d42:
+                    LoadBitmap(source);
+                    break;
+                case 0xffd8:
+                    LoadJpeg(source);
+                    break;
+                default:
+                    throw new NotSupportedException("Image type not supported");
+            }
+
+        }
+
+        private void LoadBitmap(Stream source)
+        {
+            // offset   size    description
+            // ------   ----    -----------
+            // 0        2       4D42
+            // 2        4       size (not reliable)
+            // 6        4       reserved - zeros
+            // 10       4       offset to image data
+            // ---- DIB ---
+            // 14       4       DIB size : 40
+            // 18       4       width in pixels
+            // 22       4       height in pixels
+            // 26       2       planes : 1
+            // 28       2       bits per pixel
+            // 30       4       compression type
+            // 34       4       size of image data
+            // 38       8       unreliable resolution stuff
+            // 46       4       colors in image
+            // 50       4       important colors in image
+            source.Seek(0, SeekOrigin.Begin);
+            var header = new byte[18];
+            source.Read(header, 0, header.Length);
+
+            var dataOffset = BitConverter.ToInt32(header, 10);
+            var dibSize = BitConverter.ToInt32(header, 14);
+
+            var dib = new byte[dibSize];
+            source.Seek(-4, SeekOrigin.Current);
+            source.Read(dib, 0, dib.Length);
+
+            Width = BitConverter.ToInt32(dib, 18 - 14 );
+            Height = BitConverter.ToInt32(dib, 22 - 14);
+            BitsPerPixel = BitConverter.ToInt16(dib, 28 - 14);
+            var compression = BitConverter.ToInt32(dib, 30 - 14);
+            var dataSize = BitConverter.ToInt32(dib, 34 - 14);
+
+            switch (compression)
+            {
+                case 0:
+                    // no compression, just pull the data
+                    break;
+                case 3:
+                    // not sure what these are used for
+                    var redMask = BitConverter.ToInt32(dib, 0x36 - 14);
+                    var greenMask = BitConverter.ToInt32(dib, 0x3a - 14);
+                    var blueMask = BitConverter.ToInt32(dib, 0x3e - 14);
+                    var alphaMask = BitConverter.ToInt32(dib, 0x42 - 14);
+                    break;
+                default:
+                    throw new NotSupportedException("Unsupported bitmap compression");
+            }
+
+            var offset = 14 + dibSize;
+            PixelData = new byte[source.Length - offset];
+            source.Seek(offset, SeekOrigin.Begin);
+            source.Read(PixelData, 0, PixelData.Length);
+        }
+
+        private void LoadJpeg(Stream stream)
+        {
+            throw new NotImplementedException();
+            // offset   size    description
+            // ------   ----    -----------
+            // 0        2       FFD8
+            // 2        2       width in pixels
+            // 4        2       height in pixels
+            // 6        1       components (1 == gray, 3 == color)
+            // 7        1       sampling factor 1
+            // 8        1       sampling factor 2
+            // 9        1       sampling factor 3
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/MicroGraphics/Samples/ImageLoadSample/MeadowApp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Displays.TftSpi;
+using Meadow.Foundation.Graphics;
+using Meadow.Hardware;
+using Meadow.Units;
+
+namespace Meadow.Foundation.Graphics
+{
+    /* 
+       AISU:
+        chipSelectPin: Device.Pins.D15,
+        dcPin: Device.Pins.D11,
+        resetPin: Device.Pins.D14, 
+       JUEGO:
+        chipSelectPin: Device.Pins.D14,
+        dcPin: Device.Pins.D03,
+        resetPin: Device.Pins.D04,
+    */
+
+    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    {
+        MicroGraphics _graphics;
+        St7789 _display;
+
+        public MeadowApp()
+        {
+            Console.WriteLine("Initializing ...");
+
+            var config = new SpiClockConfiguration(new Frequency(48000, Frequency.UnitType.Kilohertz), SpiClockConfiguration.Mode.Mode3);
+            var spiBus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, config);
+
+            var display = new St7789(
+                device: Device,
+                spiBus: spiBus,
+                chipSelectPin: Device.Pins.D15,
+                dcPin: Device.Pins.D11,
+                resetPin: Device.Pins.D14,
+                width: 240, height: 240, displayColorMode: ColorType.Format16bppRgb565)
+            {
+                IgnoreOutOfBoundsPixels = true
+            };
+
+            Meadow.Foundation.Graphics.
+//            _graphics.DrawBitmap
+
+            _graphics = new MicroGraphics(display);
+            _graphics.Rotation = RotationType._180Degrees;
+
+            _graphics.Clear(true);
+
+        }
+    }
+}
