b7e9e286693e4c18d18c4df642605297b0e4dceb
Adrian Stevens <adrian.stevens@gmail.com>
2022-01-12 11:22:27 -0800

    Samples cleanup for null safety

--- Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Hih6130/Samples/Hih6130_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Hih6130/Samples/Hih6130_Sample/MeadowApp.cs
@@ -22,17 +22,17 @@ namespace MeadowApp
                 handler: result => 
                 {
                     Console.WriteLine($"Observer: Temp changed by threshold; new temp: {result.New.Temperature?.Celsius:N2}C, old: {result.Old?.Temperature?.Celsius:N2}C");
-                },                
-                filter: result => 
+                },
+                filter: result =>
                 {
                     //c# 8 pattern match syntax. checks for !null and assigns var.
-                    if (result.Old is { } old)
-                    { 
-                        return (
-                        (result.New.Temperature.Value - old.Temperature.Value).Abs().Celsius > 0.5
-                        &&
-                        (result.New.Humidity.Value - old.Humidity.Value).Percent > 0.05
-                        );
+                    if (result.Old?.Temperature is { } oldTemp &&
+                        result.Old?.Humidity is { } oldHumidity &&
+                        result.New.Temperature is { } newTemp &&
+                        result.New.Humidity is { } newHumidity)
+                    {
+                        return ((newTemp - oldTemp).Abs().Celsius > 0.5 &&
+                                (newHumidity - oldHumidity).Percent > 0.05);
                     }
                     return false;
                 }
@@ -41,8 +41,8 @@ namespace MeadowApp
 
             sensor.Updated += (sender, result) => 
             {
-                Console.WriteLine($"  Temperature: {result.New.Temperature?.Celsius:F1}°C");
-                Console.WriteLine($"  Relative Humidity: {result.New.Humidity.Value:F1}%");
+                Console.WriteLine($"  Temperature: {result?.New.Temperature?.Celsius:F1}°C");
+                Console.WriteLine($"  Relative Humidity: {result?.New.Humidity?.Percent:F1}%");
             };
 
             ReadConditions().Wait();
