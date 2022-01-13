d3f3b834aae04c43474fae077d18fe449c5dc626
Adrian Stevens <adrian.stevens@gmail.com>
2022-01-13 10:15:31 -0800

    ePaperBase cleanup

--- Source/Meadow.Foundation.Peripherals/Displays.ePaper/Driver/Displays.ePaper/ePaperBase.cs
+++ Source/Meadow.Foundation.Peripherals/Displays.ePaper/Driver/Displays.ePaper/ePaperBase.cs
@@ -1,7 +1,6 @@
 ﻿using Meadow.Devices;
 using Meadow.Hardware;
 using Meadow.Foundation.Graphics.Buffers;
-using System;
 using Meadow.Foundation.Graphics;
 
 namespace Meadow.Foundation.Displays.ePaper
@@ -11,18 +10,45 @@ namespace Meadow.Foundation.Displays.ePaper
     /// </summary>
     public abstract class EpdBase : SpiDisplayBase, IGraphicsDisplay
     {
+        /// <summary>
+        /// Display color mode 
+        /// </summary>
         public ColorType ColorMode => ColorType.Format1bpp;
 
+        /// <summary>
+        /// Buffer to hold display data
+        /// </summary>
         protected readonly Buffer1bpp imageBuffer;
 
+        /// <summary>
+        /// Width of display in pixels
+        /// </summary>
         public int Width => imageBuffer.Width;
+
+        /// <summary>
+        /// Height of display in pixels
+        /// </summary>
         public int Height => imageBuffer.Height;
 
+        /// <summary>
+        /// Ignore out of bounds pixel writes 
+        /// </summary>
         public bool IgnoreOutOfBoundsPixels { get; set; }
 
         private EpdBase()
         { }
 
+        /// <summary>
+        /// Create a new ePaper display object
+        /// </summary>
+        /// <param name="device">Meadow device</param>
+        /// <param name="spiBus">SPI bus connected to display</param>
+        /// <param name="chipSelectPin">Chip select pin</param>
+        /// <param name="dcPin">Data command pin</param>
+        /// <param name="resetPin">Reset pin</param>
+        /// <param name="busyPin">Busy pin</param>
+        /// <param name="width">Width of display in pixels</param>
+        /// <param name="height">Height of display in pixels</param>
         public EpdBase(IMeadowDevice device, ISpiBus spiBus, IPin chipSelectPin, IPin dcPin, IPin resetPin, IPin busyPin,
             int width, int height)
         {
@@ -39,8 +65,15 @@ namespace Meadow.Foundation.Displays.ePaper
             Initialize();
         }
 
+        /// <summary>
+        /// Initialize display
+        /// </summary>
         protected abstract void Initialize();
 
+        /// <summary>
+        /// Clear display buffer
+        /// </summary>
+        /// <param name="updateDisplay">force display update</param>
         public void Clear(bool updateDisplay = false)
         {
             Clear(false, updateDisplay);
@@ -56,6 +89,14 @@ namespace Meadow.Foundation.Displays.ePaper
             Clear(color.Color1bpp, updateDisplay);
         }
 
+        /// <summary>
+        /// Fill the display buffer with a color
+        /// </summary>
+        /// <param name="x">x location in pixels to start fill</param>
+        /// <param name="y">y location in pixels to start fill</param>
+        /// <param name="width">width in pixels to fill</param>
+        /// <param name="height">height in pixels to fill</param>
+        /// <param name="color">color to fill</param>
         public void Fill(int x, int y, int width, int height, Color color)
         {
             if (IgnoreOutOfBoundsPixels)
@@ -122,6 +163,11 @@ namespace Meadow.Foundation.Displays.ePaper
             DrawPixel(x, y, color.Color1bpp);
         }
 
+        /// <summary>
+        /// Invert color of pixel
+        /// </summary>
+        /// <param name="x">x coordinate of pixel</param>
+        /// <param name="y">y coordinate of pixel</param>
         public void InvertPixel(int x, int y)
         {
             if (IgnoreOutOfBoundsPixels)
@@ -136,11 +182,11 @@ namespace Meadow.Foundation.Displays.ePaper
         /// <summary>
         ///     Draw a single pixel 
         /// </summary>
-        /// <param name="x">x location </param>
+        /// <param name="x">x location</param>
         /// <param name="y">y location</param>
-        /// <param name="r">y location</param>
-        /// <param name="g">y location</param>
-        /// <param name="b">y location</param>
+        /// <param name="r">red component of color in bytes (0-255)</param>
+        /// <param name="g">green component of color in bytes (0-255)</param>
+        /// <param name="b">blue component of color in bytes (0-255)</param>
         public void DrawPixel(int x, int y, byte r, byte g, byte b)
         {
             DrawPixel(x, y, r > 0 || g > 0 || b > 0);
@@ -155,6 +201,12 @@ namespace Meadow.Foundation.Displays.ePaper
             DisplayFrame();
         }
 
+        /// <summary>
+        /// Draw a buffer at a specific location
+        /// </summary>
+        /// <param name="x">x location in pixels</param>
+        /// <param name="y">y location in pixels</param>
+        /// <param name="displayBuffer"></param>
         public void DrawBuffer(int x, int y, IDisplayBuffer displayBuffer)
         {
             imageBuffer.WriteBuffer(x, y, displayBuffer);
@@ -169,6 +221,14 @@ namespace Meadow.Foundation.Displays.ePaper
             DisplayFrame();
         }
 
+        /// <summary>
+        /// Set frame buffer memory of display
+        /// </summary>
+        /// <param name="image_buffer">buffer</param>
+        /// <param name="x">x location</param>
+        /// <param name="y">y location</param>
+        /// <param name="image_width">width in pixels</param>
+        /// <param name="image_height">height in pixels</param>
         public virtual void SetFrameMemory(byte[] image_buffer,
                                 int x,
                                 int y,
@@ -190,7 +250,7 @@ namespace Meadow.Foundation.Displays.ePaper
             image_width &= 0xF8;
             if (x + image_width >= Width)
             {
-                x_end = (int)Width - 1;
+                x_end = Width - 1;
             }
             else
             {
@@ -199,7 +259,7 @@ namespace Meadow.Foundation.Displays.ePaper
 
             if (y + image_height >= Height)
             {
-                y_end = (int)Height - 1;
+                y_end = Height - 1;
             }
             else
             {
@@ -219,9 +279,14 @@ namespace Meadow.Foundation.Displays.ePaper
             }
         }
 
+        /// <summary>
+        /// Set frame buffer memory of display (full screen)
+        /// </summary>
+        /// <param name="image_buffer">buffer</param>
+
         public virtual void SetFrameMemory(byte[] image_buffer)
         {
-            SetMemoryArea(0, 0, (int)Width - 1, (int)Height - 1);
+            SetMemoryArea(0, 0, Width - 1, Height - 1);
             SetMemoryPointer(0, 0);
             SendCommand(Command.WRITE_RAM);
             /* send the image data */
@@ -233,9 +298,13 @@ namespace Meadow.Foundation.Displays.ePaper
             }
         }
 
+        /// <summary>
+        /// Clear frame buffer memory
+        /// </summary>
+        /// <param name="color">color to clear to</param>
         public virtual void ClearFrameMemory(byte color)
         {
-            SetMemoryArea(0, 0, (int)Width - 1, (int)Height - 1);
+            SetMemoryArea(0, 0, Width - 1, Height - 1);
             SetMemoryPointer(0, 0);
             SendCommand(Command.WRITE_RAM);
             /* send the color data */
@@ -245,6 +314,9 @@ namespace Meadow.Foundation.Displays.ePaper
             }
         }
 
+        /// <summary>
+        /// Display frame buffer 
+        /// </summary>
         public virtual void DisplayFrame()
         {
             SendCommand(Command.DISPLAY_UPDATE_CONTROL_2);
@@ -278,18 +350,28 @@ namespace Meadow.Foundation.Displays.ePaper
             WaitUntilIdle();
         }
 
+        /// <summary>
+        /// Set display to sleep mode
+        /// </summary>
         protected void Sleep()
         {
             SendCommand(Command.DEEP_SLEEP_MODE);
             WaitUntilIdle();
         }
 
+        /// <summary>
+        /// Set command to display
+        /// </summary>
+        /// <param name="command">command</param>
         protected void SendCommand(Command command)
         {
             SendCommand((byte)command);
         }
 
-        public enum Command : byte
+        /// <summary>
+        /// Display commands
+        /// </summary>
+        protected enum Command : byte
         {
             DRIVER_OUTPUT_CONTROL = 0x01,
             BOOSTER_SOFT_START_CONTROL = 0x0C,
@@ -314,5 +396,4 @@ namespace Meadow.Foundation.Displays.ePaper
             TERMINATE_FRAME_READ_WRITE = 0xFF,
         }
     }
-
 }
