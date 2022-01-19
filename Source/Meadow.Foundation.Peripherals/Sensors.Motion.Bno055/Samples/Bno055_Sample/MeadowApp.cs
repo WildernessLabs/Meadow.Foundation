324d31152ab43243184e937854c713f770b1efe6
ctacke <ctacke@opennetcf.com>
2022-01-19 14:08:17 -0600

    unitized euler angles and updated BNO055

--- Source/Meadow.Foundation.Peripherals/Sensors.Motion.Bno055/Samples/Bno055_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Motion.Bno055/Samples/Bno055_Sample/MeadowApp.cs
@@ -8,7 +8,7 @@ using Meadow.Foundation.Sensors.Motion;
 
 namespace MeadowApp
 {
-    public class MeadowApp : App<F7MicroV2, MeadowApp>
+    public class MeadowApp : App<F7Micro, MeadowApp>
     {
         //<!—SNIP—>
 
