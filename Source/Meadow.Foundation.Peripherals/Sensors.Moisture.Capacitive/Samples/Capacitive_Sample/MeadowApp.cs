e217365ffcadc6badfbd4bb25679e3127765a84c
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-01-27 21:38:19 -0800

    Fix build errors in M.F. Solution

--- Source/Meadow.Foundation.Peripherals/Sensors.Moisture.Capacitive/Samples/Capacitive_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Moisture.Capacitive/Samples/Capacitive_Sample/MeadowApp.cs
@@ -18,7 +18,7 @@ namespace Sensors.Moisture.Capacitive_Sample
             Console.WriteLine("Initializing...");
 
             capacitive = new Capacitive(
-                analogPort: Device.CreateAnalogInputPort(Device.Pins.A00),
+                analogPort: Device.CreateAnalogInputPort(Device.Pins.A00, 5, TimeSpan.FromMilliseconds(40), new Voltage(3.3, Voltage.UnitType.Volts)),
                 minimumVoltageCalibration: new Voltage(2.84f),
                 maximumVoltageCalibration: new Voltage(1.63f)
             );
