b24968f7ff0ee895b8dbfe6df4fa4d3e1534b039
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-03-21 23:49:22 -0700

    GH-266 - Remove redundant driver folder and update all project references

---
+++ Source/Meadow.Foundation.Peripherals/FeatherWings.GPSWing/Driver/GPSWing.cs
@@ -0,0 +1,13 @@
+﻿using Meadow.Foundation.Sensors.Gnss;
+using Meadow.Hardware;
+
+namespace Meadow.Foundation.FeatherWings
+{
+    public class GPSWing : Mt3339
+    {
+        public GPSWing(ISerialMessagePort serialMessagePort)
+            : base(serialMessagePort)
+        {
+        }
+    }
+}
