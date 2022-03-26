6a4c5c713ebb0898f8cfe936b3a987228a8c9ce7
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-03-25 18:21:55 -0700

    [Meadow.Foundation.Core] Add AnalogObservableBase class for uncalibrated analog sensors

---
+++ Source/Meadow.Foundation.Core/Sensors/Base/AnalogObservableBase.cs
@@ -0,0 +1,104 @@
+﻿using Meadow.Hardware;
+using Meadow.Units;
+using System;
+using System.Threading.Tasks;
+
+namespace Meadow.Foundation.Sensors.Base
+{
+    /// <summary>
+    /// Represents an AnalogObservableBase sensor
+    /// </summary>
+    public abstract class AnalogObservableBase : SensorBase<Voltage>
+    {
+        /// <summary>
+        /// Analog port connected to the sensor.
+        /// </summary>
+        private readonly IAnalogInputPort AnalogInputPort;
+
+        public Voltage Voltage { get; protected set; }
+
+        /// <summary>
+        /// Creates a new AnalogObservableBase driver
+        /// </summary>
+        /// <param name="pin">AnalogChannel connected to the sensor.</param>
+        public AnalogObservableBase(IAnalogInputController device, IPin pin, int sampleCount = 5, TimeSpan? sampleInterval = null, Voltage? voltage = null)
+            : this(device.CreateAnalogInputPort(pin, sampleCount, sampleInterval ?? TimeSpan.FromMilliseconds(40), voltage ?? new Voltage(3.3)))
+        {
+        }
+
+        public AnalogObservableBase(IAnalogInputPort port)
+        {
+            AnalogInputPort = port;
+
+            // wire up our observable
+            // have to convert from voltage to temp units for our consumers
+            // this is where the magic is: this allows us to extend the IObservable
+            // pattern through the sensor driver
+            AnalogInputPort.Subscribe
+            (
+                IAnalogInputPort.CreateObserver(
+                    result =>
+                    {
+                        // create a new change result from the new value
+                        ChangeResult<Voltage> changeResult = new ChangeResult<Voltage>()
+                        {
+                            New = result.New,
+                            Old = Voltage
+                        };
+                        // save state
+                        Voltage = changeResult.New;
+                        // notify
+                        RaiseEventsAndNotify(changeResult);
+                    }
+                )
+           );
+        }
+
+        /// <summary>
+        /// Starts continuously sampling the sensor.
+        ///
+        /// This method also starts raising `Changed` events and IObservable
+        /// subscribers getting notified. Use the `readIntervalDuration` parameter
+        /// to specify how often events and notifications are raised/sent.
+        /// </summary>
+        /// <param name="updateInterval">A `TimeSpan` that specifies how long to
+        /// wait between readings. This value influences how often `*Updated`
+        /// events are raised and `IObservable` consumers are notified.
+        /// The default is 5 seconds.</param>
+        public void StartUpdating(TimeSpan? updateInterval)
+        {
+            // thread safety
+            lock (samplingLock)
+            {
+                if (IsSampling) return;
+                IsSampling = true;
+                AnalogInputPort.StartUpdating(updateInterval);
+            }
+        }
+
+        /// <summary>
+        /// Stops sampling the temperature.
+        /// </summary>
+        public void StopUpdating()
+        {
+            lock (samplingLock)
+            {
+                if (!IsSampling) return;
+                base.IsSampling = false;
+                AnalogInputPort.StopUpdating();
+            }
+        }
+
+        /// <summary>
+        /// Convenience method to get the current temperature. For frequent reads, use
+        /// StartSampling() and StopSampling() in conjunction with the SampleBuffer.
+        /// </summary>
+        /// <returns>A float value that's ann average value of all the samples taken.</returns>
+        protected override async Task<Voltage> ReadSensor()
+        {
+            // read the voltage
+            Voltage = await AnalogInputPort.Read();
+            return Voltage;
+        }
+    }
+}
--- Source/Meadow.Foundation.Peripherals/Sensors.Light.Temt6000/Driver/Sensors.Light.Temt6000/Temt6000.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Light.Temt6000/Driver/Sensors.Light.Temt6000/Temt6000.cs
@@ -1,108 +1,28 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using Meadow.Foundation.Sensors.Base;
 using Meadow.Hardware;
 using Meadow.Units;
+using System;
 
 namespace Meadow.Foundation.Sensors.Light
 {
-    public class Temt6000 : SensorBase<Voltage>
+    /// <summary>
+    /// Represents a Temt6000 light sensor
+    /// </summary>
+    public class Temt6000 : AnalogObservableBase
     {
-        //==== internals
-        /// <summary>
-        /// Analog port connected to the sensor.
-        /// </summary>
-        private readonly IAnalogInputPort AnalogInputPort;
-
-        //==== Properties
-        public Voltage Voltage { get; protected set; }
-
-        //==== constructors
-
         /// <summary>
         /// Creates a new Temt6000 driver
         /// </summary>
         /// <param name="pin">AnalogChannel connected to the sensor.</param>
         public Temt6000(IAnalogInputController device, IPin pin, int sampleCount = 5, TimeSpan? sampleInterval = null, Voltage? voltage = null)
             : this(device.CreateAnalogInputPort(pin, sampleCount, sampleInterval ?? TimeSpan.FromMilliseconds(40), voltage ?? new Voltage(3.3)))
-        {
-        }
-
-        public Temt6000(IAnalogInputPort port)
-        {
-            AnalogInputPort = port;
-
-            // wire up our observable
-            // have to convert from voltage to temp units for our consumers
-            // this is where the magic is: this allows us to extend the IObservable
-            // pattern through the sensor driver
-            AnalogInputPort.Subscribe
-            (
-                IAnalogInputPort.CreateObserver(
-                    result =>
-                    {
-                        // create a new change result from the new value
-                        ChangeResult<Voltage> changeResult = new ChangeResult<Voltage>()
-                        {
-                            New = result.New,
-                            Old = Voltage
-                        };
-                        // save state
-                        Voltage = changeResult.New;
-                        // notify
-                        RaiseEventsAndNotify(changeResult);
-                    }
-                )
-           );
-        }
-
-        //==== methods
+        { }
 
         /// <summary>
-        /// Starts continuously sampling the sensor.
-        ///
-        /// This method also starts raising `Changed` events and IObservable
-        /// subscribers getting notified. Use the `readIntervalDuration` parameter
-        /// to specify how often events and notifications are raised/sent.
+        /// Creates a new Temt6000 driver
         /// </summary>
-        /// <param name="updateInterval">A `TimeSpan` that specifies how long to
-        /// wait between readings. This value influences how often `*Updated`
-        /// events are raised and `IObservable` consumers are notified.
-        /// The default is 5 seconds.</param>
-        public void StartUpdating(TimeSpan? updateInterval)
-        {
-            // thread safety
-            lock (samplingLock)
-            {
-                if (IsSampling) return;
-                IsSampling = true;
-                AnalogInputPort.StartUpdating(updateInterval);
-            }
-        }
-
-        /// <summary>
-        /// Stops sampling the temperature.
-        /// </summary>
-        public void StopUpdating()
-        {
-            lock (samplingLock)
-            {
-                if (!IsSampling) return;
-                base.IsSampling = false;
-                AnalogInputPort.StopUpdating();
-            }
-        }
-
-        /// <summary>
-        /// Convenience method to get the current temperature. For frequent reads, use
-        /// StartSampling() and StopSampling() in conjunction with the SampleBuffer.
-        /// </summary>
-        /// <returns>A float value that's ann average value of all the samples taken.</returns>
-        protected override async Task<Voltage> ReadSensor()
-        {
-            // read the voltage
-            Voltage = await AnalogInputPort.Read();
-            return Voltage;
-        }
+        /// <param name="port"></param>
+        public Temt6000(IAnalogInputPort port) : base(port) 
+        { }
     }
 }
