3eceac61d035f6805cafd14d9799ccf552861073
ctacke <ctacke@gmail.com>
2022-05-02 11:00:41 -0500

    Basic [FromBody] implementation

---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Web.Maple/Samples/ServerConsole_Sample/RequestHandlers/SensorHandler.cs
@@ -0,0 +1,28 @@
+﻿using Meadow.Foundation.Web.Maple.Server;
+using Meadow.Foundation.Web.Maple.Server.Routing;
+
+namespace Maple.ServerBasic_Sample.RequestHandlers
+{
+    public class SensorData
+    {
+        public string Location { get; set; }
+        public double Temperature { get; set; }
+    }
+
+    public class SensorHandler : RequestHandlerBase
+    {
+        public override bool IsReusable => true;
+
+        [HttpPut]
+        public OkObjectResult PutContent([FromBody(EmptyBodyBehavior = EmptyBodyBehavior.Disallow)] SensorData data)
+        {
+            return new OkObjectResult($"Received PUT data for {data.Location}");
+        }
+
+        [HttpPost]
+        public OkObjectResult PostContent([FromBody] SensorData data)
+        {
+            return new OkObjectResult($"Received POST data for {data.Location}");
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Web.Maple/Tests/Maple.Unit.Tests/ParameterTests.cs
@@ -0,0 +1,23 @@
+﻿using Meadow.Foundation.Web.Maple.Server;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Maple.Unit.Tests
+{
+    [TestClass]
+    public class ParameterTests
+    {
+        [TestMethod]
+        public void TestFromBody()
+        {
+            var cache = new RequestMethodCache(null);
+
+            cache.AddType(typeof(SensorHandler));
+
+            var postInfo = cache.Match("POST", $"/sensor", out object postParam);
+            var putInfo = cache.Match("PUT", $"/sensor", out object putParam);
+
+            Assert.IsNotNull(postInfo);
+            Assert.IsNotNull(putInfo);
+        }
+    }
+}
