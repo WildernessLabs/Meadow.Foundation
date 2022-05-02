a0fe165646e831dde9f80922e11dc1f80463d691
Adrian Stevens <adrian.stevens@gmail.com>
2022-05-01 21:34:09 -0700

    Add Sht4x driver, sample, datasheet

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Sht4x/Driver/Sht4x.Enums.cs
@@ -0,0 +1,78 @@
+﻿namespace Meadow.Foundation.Sensors.Atmospheric
+{
+    public partial class Sht4x
+    {
+        /// <summary>
+        /// Valid addresses for the sensor.
+        /// </summary>
+        public enum Addresses : byte
+        {
+            /// <summary>
+            /// Bus address 0x44
+            /// </summary>
+            Address_0x44 = 0x44,
+            /// <summary>
+            /// Bus address 0x45
+            /// </summary>
+            Address_0x45 = 0x45,
+            /// <summary>
+            /// Default bus address
+            /// </summary>
+            Default = Address_0x44
+        }
+
+        /// <summary>
+        /// Sht4x precision and heater control
+        /// </summary>
+        public enum Precision : byte
+        {
+            /// <summary>
+            /// High precision no heat
+            /// </summary>
+            HighPrecisionNoHeat = 0xFD,
+            /// <summary>
+            /// Medium precision no heat
+            /// </summary>
+            MediumPrecisionNoHeat = 0xF6,
+            /// <summary>
+            /// Low precision no heat
+            /// </summary>
+            LowPrecisionNoHeat = 0xE0,
+
+            /// <summary>
+            /// High precision high heat for 1s
+            /// </summary>
+            HighPrecisionHighHeat1s = 0x39,
+            /// <summary>
+            /// High precision high heat for 100ms
+            /// </summary>
+            HighPrecisionHighHeat100ms = 0x32,
+
+            /// <summary>
+            /// High precision medium heat for 1s
+            /// </summary>
+            HighPrecisionMediumHeat1s = 0x2F,
+            /// <summary>
+            /// High precision medium heat for 100ms
+            /// </summary>
+            HighPrecisionMediumHeat100ms = 0x24,
+
+            /// <summary>
+            /// High precision low heat for 1s
+            /// </summary>
+            HighPrecisionLowHeat1s = 0x1E,
+            /// <summary>
+            /// High precision low heat for 100ms
+            /// </summary>
+            HighPrecisionLowHeat100ms = 0x15,
+        }
+
+        enum Commands : int
+        {
+            MEASURE_HPM = 0xFD,
+            MEASURE_LPM = 0xE0,
+            READ_SERIAL = 0x89,
+            DURATION_USEC = 1000
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Sht4x/Driver/Sht4x.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Meadow.Hardware;
+using Meadow.Peripherals.Sensors;
+using Meadow.Units;
+
+namespace Meadow.Foundation.Sensors.Atmospheric
+{
+    /// <summary>
+    /// Provide a mechanism for reading the temperature and humidity from
+    /// a SHT4x temperature / humidity sensor (SHT40, SHT41, SHT45, etc.)
+    /// </summary>
+    /// <remarks>
+    /// Readings from the sensor are made in Single-shot mode.
+    /// </remarks>
+    public partial class Sht4x :
+        ByteCommsSensorBase<(Units.Temperature? Temperature, RelativeHumidity? Humidity)>,
+        ITemperatureSensor, IHumiditySensor
+    {
+        /// <summary>
+        /// Precension of sensor reading
+        /// </summary>
+        public Precision ReadPrecision { get; protected set; } = Sht4x.Precision.HighPrecisionNoHeat;
+
+        /// <summary>
+        /// Temperature changed event handler
+        /// </summary>
+        public event EventHandler<IChangeResult<Units.Temperature>> TemperatureUpdated = delegate { };
+        /// <summary>
+        /// Humidity changed event handler
+        /// </summary>
+        public event EventHandler<IChangeResult<RelativeHumidity>> HumidityUpdated = delegate { };
+
+        /// <summary>
+        /// The curren temperature -from the last reading.
+        /// </summary>
+        public Units.Temperature? Temperature => Conditions.Temperature;
+
+        /// <summary>
+        /// The humidity, in percent relative humidity, from the last reading
+        /// </summary>
+        public RelativeHumidity? Humidity => Conditions.Humidity;
+
+        /// <summary>
+        /// Create a new SHT4x object
+        /// </summary>
+        /// <param name="address">Sensor address (0x44 or 0x45)</param>
+        /// <param name="i2cBus">I2cBus</param>
+        public Sht4x(II2cBus i2cBus, byte address = (byte)Addresses.Default)
+            : base(i2cBus, address, readBufferSize: 6, writeBufferSize: 2)
+        {
+        }
+
+        /// <summary>
+        /// Raise events for subscribers
+        /// </summary>
+        /// <param name="changeResult"></param>
+        protected override void RaiseEventsAndNotify(IChangeResult<(Units.Temperature? Temperature, RelativeHumidity? Humidity)> changeResult)
+        {
+            if (changeResult.New.Temperature is { } temp)
+            {
+                TemperatureUpdated?.Invoke(this, new ChangeResult<Units.Temperature>(temp, changeResult.Old?.Temperature));
+            }
+            if (changeResult.New.Humidity is { } humidity)
+            {
+                HumidityUpdated?.Invoke(this, new ChangeResult<Units.RelativeHumidity>(humidity, changeResult.Old?.Humidity));
+            }
+            base.RaiseEventsAndNotify(changeResult);
+        }
+
+        int GetDelayForPrecision(Precision precision)
+        {
+            int delay = 10;
+
+            switch (precision)
+            {
+                case Precision.HighPrecisionNoHeat:
+                    delay = 10;
+                    break;
+                case Precision.MediumPrecisionNoHeat:
+                    delay = 5;
+                    break;
+                case Precision.LowPrecisionNoHeat:
+                    delay = 2;
+                    break;
+                case Precision.HighPrecisionHighHeat1s:
+                case Precision.HighPrecisionMediumHeat1s:
+                case Precision.HighPrecisionLowHeat1s:
+                    delay = 1100;
+                    break;
+                case Precision.HighPrecisionHighHeat100ms:
+                case Precision.HighPrecisionMediumHeat100ms:
+                case Precision.HighPrecisionLowHeat100ms:
+                    delay = 110;
+                    break;
+            }
+
+            return delay;
+        }
+
+        /// <summary>
+        /// Get a reading from the sensor and set the Temperature and Humidity properties.
+        /// </summary>
+        protected async override Task<(Units.Temperature? Temperature, RelativeHumidity? Humidity)> ReadSensor()
+        {
+            (Units.Temperature Temperature, RelativeHumidity Humidity) conditions;
+
+            return await Task.Run(() =>
+            {
+                Peripheral?.Write((byte)ReadPrecision);
+                Thread.Sleep(GetDelayForPrecision(ReadPrecision)); // Maximum conversion time is 20ms 
+                Peripheral?.Read(ReadBuffer.Span[0..5]);
+
+                var temperature = (175 * (float)((ReadBuffer.Span[0] << 8) + ReadBuffer.Span[1]) / 65535) - 45;
+                var humidity = (125 * (float)((ReadBuffer.Span[3] << 8) + ReadBuffer.Span[4]) / 65535) - 6;
+
+                conditions.Humidity = new RelativeHumidity(humidity);
+                conditions.Temperature = new Units.Temperature(temperature, Units.Temperature.UnitType.Celsius);
+
+                return conditions;
+            });
+        }
+    }
+}
