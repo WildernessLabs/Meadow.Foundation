e6161be4c115381a8b1ffdc569686a7a097284b2
Adrian Stevens <adrian.stevens@gmail.com>
2022-05-05 18:24:43 -0700

    Add DS3502 driver, sample, datasheet

---
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Ds3502/Driver/Ds3502.Enums.cs
@@ -0,0 +1,39 @@
+﻿namespace Meadow.Foundation.ICs.IOExpanders
+{
+    public partial class Ds3502
+    {
+        /// <summary>
+        /// Valid addresses for the sensor
+        /// Controlled by pulling A0 & A1 high or low
+        /// </summary>
+        public enum Addresses : byte
+        {
+            /// <summary>
+            /// Bus address 0x28 (A0 low, A1 low)
+            /// </summary>
+            Address_0x28 = 0x28,
+            /// <summary>
+            /// Bus address 0x29 (A0 high, A1 low)
+            /// </summary>
+            Address_0x29 = 0x29,
+            /// <summary>
+            /// Bus address 0x2A (A0 low, A1 high)
+            /// </summary>
+            Address_0x2A = 0x2A,
+            /// <summary>
+            /// Bus address 0x2B (A0 high, A1 high)
+            /// </summary>
+            Address_0x2B = 0x2B,
+            /// <summary>
+            /// Default bus address
+            /// </summary>
+            Default = Address_0x28
+        }
+
+        enum Register : byte
+        {
+            DS3502_WIPER = 0, // Wiper value register
+            DS3502_MODE = 2, // Mode selection register
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Ds3502/Samples/Ds3502_Sample/MeadowApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.ICs.IOExpanders;
+
+namespace ICs.IOExpanders.Ds3502_Sample
+{
+    public class MeadowApp : App<F7FeatherV1, MeadowApp>
+    {
+        //<!=SNIP=>
+
+        protected Ds3502 ds3502;
+
+        public MeadowApp()
+        {
+            Console.WriteLine("Initialize...");
+
+            ds3502 = new Ds3502(Device.CreateI2cBus(Ds3502.DefaultBusSpeed));
+
+            for (byte i = 0; i < 127; i++)
+            {
+                ds3502.SetWiper(i);
+                Console.WriteLine($"wiper {ds3502.GetWiper()}");
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        //<!=SNOP=>
+    }
+}
