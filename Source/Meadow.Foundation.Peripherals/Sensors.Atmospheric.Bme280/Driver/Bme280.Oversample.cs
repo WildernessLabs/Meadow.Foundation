5942beaa4a0149d772a22ba7eb691908dfad8aea
Adrian Stevens <adrian.stevens@gmail.com>
2022-05-13 21:11:09 -0700

    Bme280 xml comments

--- Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Bme280/Driver/Bme280.Oversample.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Atmospheric.Bme280/Driver/Bme280.Oversample.cs
@@ -17,11 +17,29 @@ namespace Meadow.Foundation.Sensors.Atmospheric
         /// </remarks>
         public enum Oversample : byte
         {
+            /// <summary>
+            /// No sampling
+            /// </summary>
             Skip = 0,
+            /// <summary>
+            /// 1x oversampling
+            /// </summary>
             OversampleX1,
+            /// <summary>
+            /// 2x oversampling
+            /// </summary>
             OversampleX2,
+            /// <summary>
+            /// 4x oversampling
+            /// </summary>
             OversampleX4,
+            /// <summary>
+            /// 8x oversampling
+            /// </summary>
             OversampleX8,
+            /// <summary>
+            /// 16x oversampling
+            /// </summary>
             OversampleX16
         }
     }
