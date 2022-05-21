8fd55c6f0eeb55aa8b1c6165b0f1e244c5ec6fd0
Justin Johnson <justin.d.johnson@gmail.com>
2022-05-20 21:30:28 -0600

    Added new interface and base class that should start replacing existing classes

---
+++ Source/Meadow.Foundation.Core/Displays/IPixelBuffer.cs
@@ -0,0 +1,111 @@
+﻿using Meadow.Foundation.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meadow.Foundation.Displays
+{
+    /// <summary>
+    /// An interface representing the concept of a
+    /// buffer that holds a collection of pixels
+    /// that can be rendered to a wide variety of devices
+    /// at a variety of bit depths.
+    /// 
+    /// This interface has default implementations for many
+    /// of its properties and methods.
+    /// </summary>
+    public interface IPixelBuffer
+    {
+        /// <summary>
+        /// The width of the pixel buffer
+        /// </summary>
+        int Width { get; }
+
+        /// <summary>
+        /// The height of the pixel buffer
+        /// </summary>
+        int Height { get; }
+
+        /// <summary>
+        /// The ColorMode of the pixel buffer
+        /// </summary>
+        ColorType ColorMode { get; }
+
+        /// <summary>
+        /// The BitDepth of the chosen ColorMode.
+        /// </summary>
+        int BitDepth { get; }
+
+        /// <summary>
+        /// The number of bytes in this pixel buffer
+        /// </summary>
+        int ByteCount { get; }
+
+        /// <summary>
+        /// Determines whether this buffer should throw errors if
+        /// you try to write pixels outside of the bounds
+        /// </summary>
+        bool IgnoreOutOfBounds { get; }
+
+        /// <summary>
+        /// The byte array that holds all pixel data
+        /// </summary>
+        byte[] Buffer { get; }
+
+
+
+
+        /// <summary>
+        /// Set the color of the pixel at the provided coordinates
+        /// </summary>
+        /// <param name="x">X coordinate of the pixel: 0,0 at top left</param>
+        /// <param name="y">Y coordinate of the pixel: 0,0 at top left</param>
+        /// <param name="color">The pixel color</param>
+        void SetPixel(int x, int y, Color color);
+
+        /// <summary>
+        /// Get the color of a pixel - may be scaled based on buffer color depth
+        /// </summary>
+        /// <param name="x">X coordinate of the pixel: 0,0 at top left</param>
+        /// <param name="y">Y coordinate of the pixel: 0,0 at top left</param>
+        /// <param name="color">The pixel color</param>
+        Color GetPixel(int x, int y);
+
+        /// <summary>
+        /// Invert the color of a pixel at the provided location
+        /// </summary>
+        /// <param name="x">The X coord to invert</param>
+        /// <param name="y">The Y coord to invert</param>
+        public void InvertPixel(int x, int y);
+
+        /// <summary>
+        /// Writes another pixel buffer into this buffer.
+        /// </summary>
+        /// <param name="originX">The X origin to start writing</param>
+        /// <param name="originY">The Y origin to start writing</param>
+        /// <param name="buffer">The buffer to write into this buffer</param>
+        /// <returns></returns>
+        bool WriteBuffer(int originX, int originY, IPixelBuffer buffer);
+
+        /// <summary>
+        /// Fills the buffer with the provided color
+        /// </summary>
+        /// <param name="color">The color to fill</param>
+        void Fill(Color color);
+
+        /// <summary>
+        /// Fills part of the buffer with the provided color
+        /// </summary>
+        /// <param name="color">The color to fill</param>
+        /// <param name="originX">The X coord to start filling</param>
+        /// <param name="originY">The Y coord to start filling</param>
+        /// <param name="width">The width to fill</param>
+        /// <param name="height">The height to fill</param>
+        void Fill(Color color, int originX, int originY, int width, int height);
+
+        /// <summary>
+        /// Clears the buffer (writes 0s to the byte array)
+        /// </summary>
+        void Clear();
+    }
+}
---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/MicroGraphics/Driver/Graphics.MicroGraphics/Buffers/PixelBufferBase.cs
@@ -0,0 +1,130 @@
+﻿using Meadow.Foundation;
+using Meadow.Foundation.Displays;
+using Meadow.Foundation.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroGraphics.Buffers
+{
+    public abstract class PixelBufferBase : IPixelBuffer
+    {
+        public int Width { get; protected set; }
+
+        public int Height { get; protected set; }
+
+        public ColorType ColorMode { get; protected set; }
+
+        public int BitDepth
+        {
+            get
+            {
+                switch (ColorMode)
+                {
+                    case ColorType.Format1bpp: return 1;
+                    case ColorType.Format2bpp: return 2;
+                    case ColorType.Format4bppGray: return 4;
+                    case ColorType.Format8bppGray: return 8;
+                    case ColorType.Format8bppRgb332: return 8;
+                    case ColorType.Format12bppRgb444: return 12;
+                    case ColorType.Format16bppRgb555: return 15;
+                    case ColorType.Format16bppRgb565: return 16;
+                    case ColorType.Format24bppRgb888: return 24;
+                    case ColorType.Format32bppRgba8888: return 32;
+                    default:
+                        throw new NotImplementedException($"Unknown or unsupported ColorMode: {ColorMode}");
+                        break;
+                }
+            }
+        }
+
+        public int ByteCount
+        {
+            get
+            {
+                return (Width * Height * BitDepth) / 8;
+            }
+        }
+
+        public bool IgnoreOutOfBounds { get; protected set; } = true;
+
+        public byte[] Buffer { get; protected set; }
+
+
+        public PixelBufferBase() { }
+
+        public PixelBufferBase(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Buffer = new byte[this.ByteCount];
+        }
+
+        public PixelBufferBase(int width, int height, byte[] buffer)
+        {
+            Width = width;
+            Height = height;
+            if(buffer.Length != ByteCount)
+            {
+                throw new ArgumentException($"Provided buffer length ({buffer.Length}) does not match this buffer's ByteCount ({ByteCount}).");
+            }
+            Buffer = buffer;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(Buffer, 0, Buffer.Length);
+        }
+
+        public abstract void Fill(Color color);
+        
+        public abstract void Fill(Color color, int originX, int originY, int width, int height);
+
+        public abstract Color GetPixel(int x, int y);
+
+        public abstract void SetPixel(int x, int y, Color color);
+
+        /// <summary>
+        /// Default way to write a buffer into this buffer.
+        /// 
+        /// This is very slow and should be avoided if possible.
+        /// It loops through every pixel. If the buffer bit depths match
+        /// there should be faster ways to write using Array.Copy.
+        /// </summary>
+        /// <param name="originX">The X coord to start writing</param>
+        /// <param name="originY">The Y coord to start writing</param>
+        /// <param name="buffer">The buffer to copy into this buffer</param>
+        public void WriteBuffer(int originX, int originY, IPixelBuffer buffer)
+        {
+            WriteBufferSlow(originX, originY, buffer);
+        }
+
+        /// <summary>
+        /// A slow buffer write operation that writes pixel-by-pixel using
+        /// the Color struct to scale color based on bit depth.
+        /// 
+        /// This method can handle buffers with mismatched bit depths.
+        /// </summary>
+        /// <param name="originX">The X coord to start writing the buffer</param>
+        /// <param name="originY">The Y coord to start writing the buffer</param>
+        /// <param name="buffer">The buffer to write</param>
+        protected void WriteBufferSlow(int originX, int originY, IPixelBuffer buffer)
+        {
+            Color color;
+
+            // ensure that we don't write beyond the bounds and have a range exception
+            // if IgnoreOutOfBounds is true
+            var xRange = IgnoreOutOfBounds ? Math.Min(Width, originX + buffer.Width) : buffer.Width;
+            var yRange = IgnoreOutOfBounds ? Math.Min(Height, originY + buffer.Height) : buffer.Width;
+
+            for (var x = 0; x < xRange; x++)
+            {
+                for(var y = 0; y < yRange; y++)
+                {
+                    color = buffer.GetPixel(x, y);
+                    SetPixel(originX + x, originY + y, color);
+                }
+            }
+        }
+    }
+}
