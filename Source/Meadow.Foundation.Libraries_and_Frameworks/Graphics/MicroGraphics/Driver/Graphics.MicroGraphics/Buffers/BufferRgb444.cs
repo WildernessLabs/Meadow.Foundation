7dbda94bb185a9b4d87151078404dea37b710972
Justin Johnson <justin.d.johnson@gmail.com>
2022-05-20 22:20:36 -0600

    Update BufferRgb444.cs

--- Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/MicroGraphics/Driver/Graphics.MicroGraphics/Buffers/BufferRgb444.cs
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics/MicroGraphics/Driver/Graphics.MicroGraphics/Buffers/BufferRgb444.cs
@@ -2,37 +2,52 @@
 
 namespace Meadow.Foundation.Graphics.Buffers
 {
-    public class BufferRgb444 : BufferBase
+    public class BufferRgb444 : PixelBufferBase
     {
-        public override int ByteCount => Width * Height * 3 / 2;
+        public override ColorType ColorMode => ColorType.Format12bppRgb444;
 
-        public override ColorType displayColorMode => ColorType.Format12bppRgb444;
 
         public BufferRgb444(int width, int height, byte[] buffer) : base(width, height, buffer) { }
 
         public BufferRgb444(int width, int height) : base(width, height) { }
 
-        public ushort GetPixel12bpp(int x, int y)
+
+        public override void Fill(Color color)
         {
-            byte r, g, b;
-            int index;
-            if (x % 2 == 0)
-            {
-                index = (x + y * Width) * 3 / 2;
+            // could do a minor optimization by caching the ushort 444 value 
+            Buffer[0] = (byte)(color.Color12bppRgb444 >> 4);
+            Buffer[1] = (byte)((color.Color12bppRgb444 << 4) | (color.Color12bppRgb444 >> 8));
+            Buffer[2] = (byte)color.Color12bppRgb444;
 
-                r = (byte)(Buffer[index] >> 4);
-                g = (byte)(Buffer[index] & 0x0F);
-                b = (byte)(Buffer[index + 1] >> 4);
-            }
-            else
+            int arrayMidPoint = Buffer.Length / 2;
+            int copyLength;
+
+            for (copyLength = 3; copyLength < arrayMidPoint; copyLength <<= 1)
             {
-                index = ((x - 1 + y * Width) * 3 / 2) + 1;
-                r = (byte)(Buffer[index] & 0x0F);
-                g = (byte)(Buffer[index + 1] >> 4);
-                b = (byte)(Buffer[index + 1] & 0x0F);
+                Array.Copy(Buffer, 0, Buffer, copyLength, copyLength);
             }
 
-            return (ushort)(r << 8 | g << 4 | b);
+            Array.Copy(Buffer, 0, Buffer, copyLength, Buffer.Length - copyLength);
+        }
+
+        public override void Fill(Color color, int x, int y, int width, int height)
+        {
+            if (x < 0 || x + width > Width ||
+                y < 0 || y + height > Height)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            //TODO optimize
+            var uColor = color.Color12bppRgb444;
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    SetPixel(x + i, y + j, uColor);
+                }
+            }
         }
 
         public override Color GetPixel(int x, int y)
@@ -66,7 +81,6 @@ namespace Meadow.Foundation.Graphics.Buffers
         {
             SetPixel(x, y, color.Color12bppRgb444);
         }
-
         public void SetPixel(int x, int y, ushort color)
         {
             int index;
@@ -90,53 +104,46 @@ namespace Meadow.Foundation.Graphics.Buffers
             }
         }
 
-        public override void Fill(Color color)
+        public override void InvertPixel(int x, int y)
         {
-            // could do a minor optimization by caching the ushort 444 value 
-            Buffer[0] = (byte)  (color.Color12bppRgb444 >> 4);
-            Buffer[1] = (byte) ((color.Color12bppRgb444 << 4) | (color.Color12bppRgb444 >> 8));
-            Buffer[2] = (byte)   color.Color12bppRgb444;
-
-            int arrayMidPoint = Buffer.Length / 2;
-            int copyLength;
-
-            for (copyLength = 3; copyLength < arrayMidPoint; copyLength <<= 1)
-            {
-                Array.Copy(Buffer, 0, Buffer, copyLength, copyLength);
-            }
-
-            Array.Copy(Buffer, 0, Buffer, copyLength, Buffer.Length - copyLength);
+            throw new NotImplementedException();
         }
 
-        public override void Fill(Color color, int x, int y, int width, int height)
+        public override void WriteBuffer(int x, int y, IPixelBuffer buffer)
         {
-            if (x < 0 || x + width > Width ||
-                y < 0 || y + height > Height)
+            if(buffer.ColorMode == ColorMode)
             {
-                throw new ArgumentOutOfRangeException();
+                // TODO: optimize, this is doing a slow write!
+                WriteBufferSlow(x, y, buffer);
             }
-
-            //TODO optimize
-            var uColor = color.Color12bppRgb444;
-
-            for (int i = 0; i < width; i++)
+            else
             {
-                for (int j = 0; j < height; j++)
-                {
-                    SetPixel(x + i, y + j, uColor);
-                }
+                base.WriteBuffer(x, y, buffer);
             }
         }
 
-        public new void WriteBuffer(int x, int y, IDisplayBuffer buffer)
+
+        public ushort GetPixel12bpp(int x, int y)
         {
-            if (base.WriteBuffer(x, y, buffer))
-            {   //call the base for validation
-                //and to handle the slow path when buffers don't match
-                return;
+            byte r, g, b;
+            int index;
+            if (x % 2 == 0)
+            {
+                index = (x + y * Width) * 3 / 2;
+
+                r = (byte)(Buffer[index] >> 4);
+                g = (byte)(Buffer[index] & 0x0F);
+                b = (byte)(Buffer[index + 1] >> 4);
+            }
+            else
+            {
+                index = ((x - 1 + y * Width) * 3 / 2) + 1;
+                r = (byte)(Buffer[index] & 0x0F);
+                g = (byte)(Buffer[index + 1] >> 4);
+                b = (byte)(Buffer[index + 1] & 0x0F);
             }
 
-            WriteBufferSlow(x, y, buffer); //ToDo - optimize 
+            return (ushort)(r << 8 | g << 4 | b);
         }
     }
 }
