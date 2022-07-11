69b3be9b30e0fdcc800a95c28cdb120d6cf90efd
Adrian Stevens <adrian.stevens@gmail.com>
2022-07-11 14:12:21 -0700

    Fix graphics/display rendering for Apa102

--- Source/Meadow.Foundation.Peripherals/Leds.Apa102/Driver/Apa102PixelBuffer.cs
+++ Source/Meadow.Foundation.Peripherals/Leds.Apa102/Driver/Apa102PixelBuffer.cs
@@ -4,19 +4,48 @@ namespace Meadow.Foundation.Leds
 {
     public partial class Apa102 : IPixelBuffer
     {
+        /// <summary>
+        /// The color bit depth of the display
+        /// </summary>
         public int BitDepth => 24;
 
+        /// <summary>
+        /// The size of the display buffer in bytes
+        /// </summary>
         public int ByteCount => NumberOfLeds * 3;
 
+        /// <summary>
+        /// The display buffer - not implemented for the Apa102 driver
+        /// </summary>
         public byte[] Buffer => throw new System.NotImplementedException();
 
+        /// <summary>
+        /// Fill the display buffer with a color
+        /// </summary>
+        /// <param name="color">The fill color</param>
         public void Fill(Color color) => Fill(color, false);
 
+        /// <summary>
+        /// Get the color of pixel at at given coordinate
+        /// </summary>
+        /// <param name="x">X position in pixels</param>
+        /// <param name="y">Y position in pixels</param>
+        /// <returns></returns>
         public Color GetPixel(int x, int y)
         {
-            throw new System.NotImplementedException();
+            var index = 3 * GetIndexForCoordinate(x, y);
+
+            return new Color(red: buffer[index + pixelOrder[0]],
+                             green: buffer[index + pixelOrder[1]], 
+                             blue: buffer[index + pixelOrder[2]]);
         }
 
-        public void SetPixel(int x, int y, Color color) => SetLed(x, color);
+        /// <summary>
+        /// Set the pixel for a given coordindate
+        /// </summary>
+        /// <param name="x">X position in pixels</param>
+        /// <param name="y">Y position in pixels</param>
+        /// <param name="color">Color of pixel</param>
+        public void SetPixel(int x, int y, Color color) => SetLed(GetIndexForCoordinate(x, y), color);
     }
 }
