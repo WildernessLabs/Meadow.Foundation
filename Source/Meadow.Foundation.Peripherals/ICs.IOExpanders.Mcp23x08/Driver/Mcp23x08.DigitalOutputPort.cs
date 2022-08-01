edabac71e3b490d98f13ee8db1b594437616c84a
ctacke <ctacke@gmail.com>
2022-08-01 18:35:54 -0500

    Dispose moved to base

--- Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Mcp23x08/Driver/Mcp23x08.DigitalOutputPort.cs
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Mcp23x08/Driver/Mcp23x08.DigitalOutputPort.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Meadow.Hardware;
+﻿using Meadow.Hardware;
 
 namespace Meadow.Foundation.ICs.IOExpanders
 {
@@ -9,12 +8,15 @@ namespace Meadow.Foundation.ICs.IOExpanders
         {
             Mcp23x08 _mcp;
 
-            public override bool State {
+            public override bool State
+            {
                 get => this.state;
-                set {
+                set
+                {
                     _mcp.WriteToPort(this.Pin, value);
                 }
-            } protected bool state;
+            }
+            protected bool state;
 
             public DigitalOutputPort(
                 Mcp23x08 mcpController,
@@ -26,21 +28,17 @@ namespace Meadow.Foundation.ICs.IOExpanders
                 _mcp = mcpController;
             }
 
-            public override void Dispose()
-            {
-                Dispose(true);
-                GC.SuppressFinalize(this);
-            }
-
-            protected virtual void Dispose(bool disposing)
+            protected override void Dispose(bool disposing)
             {
                 // TODO: we should consider moving this logic to the finalizer
                 // but the problem with that is that we don't know when it'll be called
                 // but if we do it in here, we may need to check the _disposed field
                 // elsewhere
 
-                if (!disposed) {
-                    if (disposing) {
+                if (!disposed)
+                {
+                    if (disposing)
+                    {
                         this.state = false;
                         _mcp.ResetPin(this.Pin);
                     }
