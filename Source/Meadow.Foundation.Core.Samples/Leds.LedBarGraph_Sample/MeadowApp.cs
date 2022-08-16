bc0b6109c67afb1f166730ec6ff78f15e88f13e8
Jorge Ramirez <ramirez.jorgea@outlook.com>
2022-08-16 00:26:58 -0700

    F7FeatherV2

--- Source/Meadow.Foundation.Core.Samples/Leds.LedBarGraph_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Core.Samples/Leds.LedBarGraph_Sample/MeadowApp.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Leds.LedBarGraph_Sample
 {
-    public class MeadowApp : App<F7FeatherV1>
+    public class MeadowApp : App<F7FeatherV2>
     {
         //<!=SNIP=>
 
