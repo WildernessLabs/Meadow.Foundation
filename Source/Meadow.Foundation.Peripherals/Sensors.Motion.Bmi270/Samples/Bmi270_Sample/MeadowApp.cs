006661ca5efe157435aac43af2d3276c67650b4f
Adrian Stevens <adrian.stevens@gmail.com>
2022-08-19 21:38:13 -0700

    Add configurable accel and angular velocity for BMI270

--- Source/Meadow.Foundation.Peripherals/Sensors.Motion.Bmi270/Samples/Bmi270_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Motion.Bmi270/Samples/Bmi270_Sample/MeadowApp.cs
@@ -2,7 +2,6 @@
 using Meadow.Devices;
 using Meadow.Foundation.Sensors.Accelerometers;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace MeadowApp
@@ -16,20 +15,25 @@ namespace MeadowApp
         {
             Console.WriteLine("Initialize hardware...");
             bmi270 = new Bmi270(Device.CreateI2cBus());
+         //   bmi270.SetAccelerationRange(Bmi270.AccelerationRange._8g);
+
+            bmi270.Updated += Bmi270_Updated;
 
             return base.Initialize();
         }
 
+        private void Bmi270_Updated(object sender, IChangeResult<(Meadow.Units.Acceleration3D? Acceleration3D, Meadow.Units.AngularVelocity3D? AngularVelocity3D)> e)
+        {
+            var accel = e.New.Acceleration3D.Value;
+            var gyro = e.New.AngularVelocity3D.Value;
+
+            Console.WriteLine($"X={accel.X.Gravity:0.##}g, Y={accel.Y.Gravity:0.##}g, Z={accel.Z.Gravity:0.##}g");
+            Console.WriteLine($"X={gyro.X.RadiansPerMinute:0.##}rpm, Y={gyro.Y.RadiansPerMinute:0.##}rpm, Z={gyro.Z.RadiansPerMinute:0.##}rpm");
+        }
+
         public override Task Run()
         {
-            while(true)
-            {
-                var data = bmi270?.ReadAccelerationData();
-
-                Console.WriteLine(BitConverter.ToString(data));
-
-                Thread.Sleep(2000);
-            }
+            bmi270.StartUpdating(TimeSpan.FromSeconds(1));
 
             return base.Run();
         }
