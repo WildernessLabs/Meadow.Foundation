cf672f151501edaa86bc916105718d4cc51fa6fc
Adrian Stevens <adrian.stevens@gmail.com>
2022-08-22 19:54:04 -0700

    Add Mcp23018 and Mcp23s18 drivers

---
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Mcp23xxx/Driver/Drivers/Mcp23018.cs
@@ -0,0 +1,20 @@
+﻿using Meadow.Hardware;
+
+namespace Meadow.Foundation.ICs.IOExpanders
+{
+    /// <summary>
+    /// Represent an MCP23018 I2C port expander with open-drain outputs
+    /// </summary>
+    public class Mcp23018 : Mcp23x1x
+    {
+        /// <summary>
+        /// Creates an Mcp23018 object
+        /// </summary>
+        /// <param name="i2cBus">The I2C bus</param>
+        /// <param name="address">The I2C address</param>
+        /// <param name="interruptPort">The interrupt port</param>
+        public Mcp23018(II2cBus i2cBus, byte address = 32, IDigitalInputPort interruptPort = null) :
+            base(i2cBus, address, interruptPort)
+        { }
+    }
+}
