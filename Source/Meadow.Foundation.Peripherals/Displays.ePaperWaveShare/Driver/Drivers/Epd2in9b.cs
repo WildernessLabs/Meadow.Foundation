2f12d78c78b3d9690762b34cf99ba198e0192c7d
Adrian Stevens <adrian.stevens@gmail.com>
2022-08-26 12:55:52 -0700

    Add WaveShare ePaper nuget

---
+++ Source/Meadow.Foundation.Peripherals/Displays.ePaperWaveShare/Driver/Drivers/Epd2in9b.cs
@@ -0,0 +1,45 @@
+﻿using Meadow.Hardware;
+
+namespace Meadow.Foundation.Displays.ePaper
+{
+    /// <summary>
+    /// Represents a WaveShare Epd2in9b ePaper display
+    /// 128x296, 2.9 inch e-Ink display, SPI interface 
+    /// </summary>
+    public class Epd2in9b : Il0373
+    {
+        /// <summary>
+        /// Create a new WaveShare Epd2in9b 128x296 pixel display object ePaper display object
+        /// </summary>
+        /// <param name="device">Meadow device</param>
+        /// <param name="spiBus">SPI bus connected to display</param>
+        /// <param name="chipSelectPin">Chip select pin</param>
+        /// <param name="dcPin">Data command pin</param>
+        /// <param name="resetPin">Reset pin</param>
+        /// <param name="busyPin">Busy pin</param>
+        public Epd2in9b(IMeadowDevice device,
+                                    ISpiBus spiBus,
+                                    IPin chipSelectPin,
+                                    IPin dcPin,
+                                    IPin resetPin,
+                                    IPin busyPin) :
+            base(device, spiBus, chipSelectPin, dcPin, resetPin, busyPin, 128, 296)
+        { }
+
+        /// <summary>
+        /// Create a new WaveShare Epd2in9b 128x296 pixel display object ePaper display object
+        /// </summary>
+        /// <param name="spiBus">SPI bus connected to display</param>
+        /// <param name="chipSelectPort">Chip select output port</param>
+        /// <param name="dataCommandPort">Data command output port</param>
+        /// <param name="resetPort">Reset output port</param>
+        /// <param name="busyPort">Busy input port</param>
+        public Epd2in9b(ISpiBus spiBus,
+                        IDigitalOutputPort chipSelectPort,
+                        IDigitalOutputPort dataCommandPort,
+                        IDigitalOutputPort resetPort,
+                        IDigitalInputPort busyPort) :
+            base(spiBus, chipSelectPort, dataCommandPort, resetPort, busyPort, 128, 296)
+        { }
+    }
+}
