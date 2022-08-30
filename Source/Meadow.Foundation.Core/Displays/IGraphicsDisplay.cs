a921476dd68bc3e8a41d8dc8fbc7f3642fe0ef2b
Adrian Stevens <adrian.stevens@gmail.com>
2022-08-29 20:06:44 -0700

    Add xml comments to IGraphicsDisplay

--- Source/Meadow.Foundation.Core/Displays/IGraphicsDisplay.cs
+++ Source/Meadow.Foundation.Core/Displays/IGraphicsDisplay.cs
@@ -23,7 +23,7 @@ namespace Meadow.Foundation.Graphics
         public int Height { get; }
 
         /// <summary>
-        /// Provide a buffer that matches this display's color depth, height, and width.
+        /// Provide a buffer that matches this display's color depth, height, and width
         /// This should be the buffer that is sent to the device when Show is called
         /// </summary>
         public IPixelBuffer PixelBuffer { get; }
@@ -39,7 +39,7 @@ namespace Meadow.Foundation.Graphics
         public Color DisabledColor => Color.Black;
 
         /// <summary>
-        /// Transfer the contents of the buffer to the display.
+        /// Transfer the contents of the buffer to the display
         /// </summary>
         public void Show();
 
@@ -50,49 +50,49 @@ namespace Meadow.Foundation.Graphics
         public void Show(int left, int top, int right, int bottom);
 
         /// <summary>
-        /// Clear the display.
+        /// Clear the display
         /// </summary>
-        /// <param name="updateDisplay">Update the dipslay once the buffer has been cleared when true.</param>
+        /// <param name="updateDisplay">Update the dipslay once the buffer has been cleared when true</param>
         public void Clear(bool updateDisplay = false);
 
         /// <summary>
-        /// Clear the display.
+        /// Clear the display
         /// </summary>
-        /// <param name="fillColor">The color used to fill the display buffer.</param>
-        /// <param name="updateDisplay">Update the dipslay once the buffer has been cleared when true.</param>
+        /// <param name="fillColor">The color used to fill the display buffer</param>
+        /// <param name="updateDisplay">Update the dipslay once the buffer has been cleared when true</param>
         public void Fill(Color fillColor, bool updateDisplay = false);
 
         /// <summary>
         /// Clear the display.
         /// </summary>
-        /// <param name="x">x position</param>
-        /// <param name="y">y postion</param>
-        /// <param name="width">width to fill</param>
-        /// <param name="height">height to fill</param>
-        /// <param name="fillColor">The color used to fill the display buffer.</param>
+        /// <param name="x">x position in pixels</param>
+        /// <param name="y">y position in pixels</param>
+        /// <param name="width">width to fill in pixels</param>
+        /// <param name="height">height to fill in pixels</param>
+        /// <param name="fillColor">The color used to fill the display buffer</param>
         public abstract void Fill(int x, int y, int width, int height, Color fillColor);
 
         /// <summary>
         /// Draw a single pixel at the specified color
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="color">The Meadow.Foundation color of the pixel</param>
+        /// <param name="x">x position in pixels</param>
+        /// <param name="y">y position in pixels</param>
+        /// <param name="color">The Meadow Foundation color of the pixel</param>
         public abstract void DrawPixel(int x, int y, Color color);
 
         /// <summary>
         /// Enable or disable a single pixel (used for 1bpp displays)
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="enabled"></param>
+        /// <param name="x">x position in pixels</param>
+        /// <param name="y">y position in pixels</param>
+        /// <param name="enabled">On if true, off if false</param>
         public abstract void DrawPixel(int x, int y, bool enabled);
 
         /// <summary>
         /// Invert the color of a single pixel
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
+        /// <param name="x">x position in pixels</param>
+        /// <param name="y">y position in pixels</param>
         public abstract void InvertPixel(int x, int y);
 
         /// <summary>
