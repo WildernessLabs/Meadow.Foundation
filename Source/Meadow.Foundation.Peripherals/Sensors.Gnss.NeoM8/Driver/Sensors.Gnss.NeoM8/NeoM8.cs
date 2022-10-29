b5e95e1fca8f5beaf26e8b0aea78529fcc4d71f5
bryan costanich <b@wowzer.net>
2022-10-29 12:23:55 -0700

    Initial check in of NeoM8 driver work

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Gnss.NeoM8/Driver/Sensors.Gnss.NeoM8/NeoM8.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Text;
+using Meadow.Foundation.Sensors.Location.Gnss;
+using Meadow.Hardware;
+using Meadow.Peripherals.Sensors.Location.Gnss;
+
+namespace Meadow.Foundation.Sensors.Gnss
+{
+    /// <summary>
+    /// NMEA Event args - holds an NMEA sentance as a string
+    /// </summary>
+    public class NmeaEventArgs
+    {
+        /// <summary>
+        /// The NMEA sentance
+        /// </summary>
+        public string NmeaSentence { get; set; } = string.Empty;
+    }
+
+    public class NeoM8
+    {
+        //public int BaudRate {
+        //    get => serialPort.BaudRate;
+        //    set => serialPort.BaudRate = value;
+        //}
+
+        readonly ISerialMessagePort serialPort;
+        NmeaSentenceProcessor? nmeaProcessor;
+
+        //public event EventHandler<NmeaEventArgs> NmeaSentenceArrived = delegate { };
+
+        public event EventHandler<GnssPositionInfo> GgaReceived = delegate { };
+        public event EventHandler<GnssPositionInfo> GllReceived = delegate { };
+        public event EventHandler<ActiveSatellites> GsaReceived = delegate { };
+        public event EventHandler<GnssPositionInfo> RmcReceived = delegate { };
+        public event EventHandler<CourseOverGround> VtgReceived = delegate { };
+        public event EventHandler<SatellitesInView> GsvReceived = delegate { };
+
+        // TODO: if we want to make this public then we're going to have to add
+        // a bunch of checks around baud rate, 8n1, etc.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="serialPort"></param>
+        protected NeoM8(ISerialMessagePort serialPort)
+        {
+            this.serialPort = serialPort;
+
+            serialPort.MessageReceived += SerialPort_MessageReceived;
+
+            Init();
+        }
+
+        /// <summary>
+        /// Create a new Mt3339 object
+        /// </summary>
+        /// <param name="device">IMeadowDevice instance</param>
+        /// <param name="serialPortName">The serial port name to create</param>
+        public NeoM8(ISerialMessageController device, SerialPortName serialPortName)
+            : this(device.CreateSerialMessagePort(
+                serialPortName, suffixDelimiter: Encoding.ASCII.GetBytes("\r\n"),
+                preserveDelimiter: true, readBufferSize: 512))
+        { }
+
+        protected void Init()
+        {
+            serialPort.MessageReceived += SerialPort_MessageReceived;
+            InitDecoders();
+            Console.WriteLine("Finish NeoM8 initialization.");
+        }
+
+        public void StartUpdating()
+        {
+            // open the serial connection
+            serialPort.Open();
+            Console.WriteLine("serial port opened.");
+
+            //==== setup commands
+
+            //// get release and version
+            //Console.WriteLine("Asking for release and version.");
+            //this.serialPort.Write(Encoding.ASCII.GetBytes(Commands.PMTK_Q_RELEASE));
+
+            //// get atntenna info
+            //Console.WriteLine("Start output antenna info");
+            //this.serialPort.Write(Encoding.ASCII.GetBytes(Commands.PGCMD_ANTENNA));
+
+            //// turn on all data
+            //Console.WriteLine("Turning on all data");
+            //this.serialPort.Write(Encoding.ASCII.GetBytes(Commands.PMTK_SET_NMEA_OUTPUT_ALLDATA));
+        }
+
+        protected void InitDecoders()
+        {
+            Console.WriteLine("Create NMEA");
+            nmeaProcessor = new NmeaSentenceProcessor();
+
+            Console.WriteLine("Add decoders");
+
+            // GGA
+            var ggaDecoder = new GgaDecoder();
+            Console.WriteLine("Created GGA");
+            nmeaProcessor.RegisterDecoder(ggaDecoder);
+            ggaDecoder.PositionReceived += (object sender, GnssPositionInfo location) => {
+                GgaReceived(this, location);
+            };
+
+            // GLL
+            var gllDecoder = new GllDecoder();
+            nmeaProcessor.RegisterDecoder(gllDecoder);
+            gllDecoder.GeographicLatitudeLongitudeReceived += (object sender, GnssPositionInfo location) => {
+                GllReceived(this, location);
+            };
+
+            // GSA
+            var gsaDecoder = new GsaDecoder();
+            nmeaProcessor.RegisterDecoder(gsaDecoder);
+            gsaDecoder.ActiveSatellitesReceived += (object sender, ActiveSatellites activeSatellites) => {
+                GsaReceived(this, activeSatellites);
+            };
+
+            // RMC (recommended minimum)
+            var rmcDecoder = new RmcDecoder();
+            nmeaProcessor.RegisterDecoder(rmcDecoder);
+            rmcDecoder.PositionCourseAndTimeReceived += (object sender, GnssPositionInfo positionCourseAndTime) => {
+                RmcReceived(this, positionCourseAndTime);
+            };
+
+            // VTG (course made good)
+            var vtgDecoder = new VtgDecoder();
+            nmeaProcessor.RegisterDecoder(vtgDecoder);
+            vtgDecoder.CourseAndVelocityReceived += (object sender, CourseOverGround courseAndVelocity) => {
+                VtgReceived(this, courseAndVelocity);
+            };
+
+            // GSV (satellites in view)
+            var gsvDecoder = new GsvDecoder();
+            nmeaProcessor.RegisterDecoder(gsvDecoder);
+            gsvDecoder.SatellitesInViewReceived += (object sender, SatellitesInView satellites) => {
+                GsvReceived(this, satellites);
+            };
+        }
+
+        private void SerialPort_MessageReceived(object sender, SerialMessageData e)
+        {
+            string msg = (e.GetMessageString(Encoding.ASCII));
+
+            Console.WriteLine($"Message arrived:{msg}");
+
+            nmeaProcessor?.ProcessNmeaMessage(msg);
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Gnss.NeoM8/Samples/NeoM8_Sample/MeadowApp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Gnss;
+using Meadow.Peripherals.Sensors.Location.Gnss;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7CoreComputeV2>
+    {
+        //<!=SNIP=>
+
+        NeoM8 gps;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initializing ...");
+
+            gps = new NeoM8(Device, Device.SerialPortNames.Com4);
+
+            gps.GgaReceived += (object sender, GnssPositionInfo location) =>
+            {
+                Console.WriteLine("*********************************************");
+                Console.WriteLine(location);
+                Console.WriteLine("*********************************************");
+            };
+            // GLL
+            gps.GllReceived += (object sender, GnssPositionInfo location) =>
+            {
+                Console.WriteLine("*********************************************");
+                Console.WriteLine(location);
+                Console.WriteLine("*********************************************");
+            };
+            // GSA
+            gps.GsaReceived += (object sender, ActiveSatellites activeSatellites) =>
+            {
+                Console.WriteLine("*********************************************");
+                Console.WriteLine(activeSatellites);
+                Console.WriteLine("*********************************************");
+            };
+            // RMC (recommended minimum)
+            gps.RmcReceived += (object sender, GnssPositionInfo positionCourseAndTime) =>
+            {
+                Console.WriteLine("*********************************************");
+                Console.WriteLine(positionCourseAndTime);
+                Console.WriteLine("*********************************************");
+
+            };
+            // VTG (course made good)
+            gps.VtgReceived += (object sender, CourseOverGround courseAndVelocity) =>
+            {
+                Console.WriteLine("*********************************************");
+                Console.WriteLine($"{courseAndVelocity}");
+                Console.WriteLine("*********************************************");
+            };
+            // GSV (satellites in view)
+            gps.GsvReceived += (object sender, SatellitesInView satellites) =>
+            {
+                Console.WriteLine("*********************************************");
+                Console.WriteLine($"{satellites}");
+                Console.WriteLine("*********************************************");
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+            gps.StartUpdating();
+
+            return Task.CompletedTask;
+        }
+
+        //<!=SNOP=>
+    }
+}
