e5765f1d780eee7c4cfbbe9244d3855d4a09cdda
Adrian Stevens <adrian.stevens@gmail.com>
2022-11-18 10:26:56 -0800

    XML sentence typo fix

--- Source/Meadow.Foundation.Libraries_and_Frameworks/Sensors.Location.Gnss.NmeaProcessor/Driver/NmeaEventArgs.cs
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Sensors.Location.Gnss.NmeaProcessor/Driver/NmeaEventArgs.cs
@@ -1,12 +1,12 @@
 ﻿namespace Meadow.Foundation.Sensors.Gnss
 {
     /// <summary>
-    /// NMEA Event args - holds an NMEA sentance as a string
+    /// NMEA Event args - holds an NMEA sentence as a string
     /// </summary>
     public class NmeaEventArgs
     {
         /// <summary>
-        /// The NMEA sentance
+        /// The NMEA sentence
         /// </summary>
         public string NmeaSentence { get; set; } = string.Empty;
     }
