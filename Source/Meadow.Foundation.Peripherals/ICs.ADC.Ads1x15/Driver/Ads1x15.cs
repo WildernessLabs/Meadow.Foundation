054c9af2ee08316a55c601ba8d1256605b68f8db
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-01-08 21:31:37 -0800

    Delete unused usings

--- Source/Meadow.Foundation.Peripherals/ICs.ADC.Ads1x15/Driver/Ads1x15.cs
+++ Source/Meadow.Foundation.Peripherals/ICs.ADC.Ads1x15/Driver/Ads1x15.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Meadow.Hardware;
 using Meadow.Units;
+using System.Threading.Tasks;
 
 namespace Meadow.Foundation.ICs.ADC
 {
