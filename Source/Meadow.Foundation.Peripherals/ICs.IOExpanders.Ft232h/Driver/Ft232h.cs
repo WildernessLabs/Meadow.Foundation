2d76e06be266cfbaa5b6c9eab1abaf7268b5e6ba
Chris Tacke <ctacke@opennetcf.com>
2023-01-17 22:12:02 -0600

    starting the FT232H driver

---
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Ft232h/Driver/Ft232h.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+namespace Meadow.Foundation.ICs.IOExpanders
+{
+    /// <summary>
+    /// Represents a DS3502 digital potentiometer
+    /// </summary>
+    public partial class Ft232h : IDisposable
+    {
+        private bool _isDisposed;
+        private static int _instanceCount = 0;
+
+        static Ft232h()
+        {
+            if (Interlocked.Increment(ref _instanceCount) == 1)
+            {
+                // only do this one time (no matter how many instances are created instances)
+                Native.Functions.Init_libMPSSE();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (Interlocked.Decrement(ref _instanceCount) == 0)
+                {
+                    // last instance was disposed, clean house
+                    Native.Functions.Cleanup_libMPSSE();
+                }
+                _isDisposed = true;
+            }
+        }
+
+        public void EnumI2CBus()
+        {
+            if (CheckStatus(Native.Functions.I2C_GetNumChannels(out int channels)))
+            {
+                Console.WriteLine($"{channels} I2C Channels");
+
+                for (var c = 0; c < channels; c++)
+                {
+                    if (CheckStatus(Native.Functions.I2C_GetChannelInfo(c, out Native.FT_DEVICE_LIST_INFO_NODE info)))
+                    {
+                        Console.WriteLine($"Serial #:       {info.SerialNumber}");
+                        Console.WriteLine($"Description #:  {info.Description}");
+                    }
+                }
+            }
+        }
+
+        private bool CheckStatus(Native.FT_STATUS status)
+        {
+            if (status == Native.FT_STATUS.FT_OK)
+            {
+                return true;
+            }
+
+            throw new Exception($"Native error: {status}");
+        }
+
+
+        ~Ft232h()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Ft232h/Driver/Native.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Meadow.Foundation.ICs.IOExpanders
+{
+    internal static class Native
+    {
+        public enum FT_STATUS
+        {
+            FT_OK,
+            FT_INVALID_HANDLE,
+            FT_DEVICE_NOT_FOUND,
+            FT_DEVICE_NOT_OPENED,
+            FT_IO_ERROR,
+            FT_INSUFFICIENT_RESOURCES,
+            FT_INVALID_PARAMETER,
+            FT_INVALID_BAUD_RATE,
+
+            FT_DEVICE_NOT_OPENED_FOR_ERASE,
+            FT_DEVICE_NOT_OPENED_FOR_WRITE,
+            FT_FAILED_TO_WRITE_DEVICE,
+            FT_EEPROM_READ_FAILED,
+            FT_EEPROM_WRITE_FAILED,
+            FT_EEPROM_ERASE_FAILED,
+            FT_EEPROM_NOT_PRESENT,
+            FT_EEPROM_NOT_PROGRAMMED,
+            FT_INVALID_ARGS,
+            FT_NOT_SUPPORTED,
+            FT_OTHER_ERROR,
+            FT_DEVICE_LIST_NOT_READY,
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+        public struct FT_DEVICE_LIST_INFO_NODE
+        {
+            /*
+            typedef struct _ft_device_list_info_node {
+		        ULONG Flags;
+		        ULONG Type;
+		        ULONG ID;
+		        DWORD LocId;
+		        char SerialNumber[16];
+		        char Description[64];
+		        FT_HANDLE ftHandle;
+	        } FT_DEVICE_LIST_INFO_NODE;
+            */
+            public uint Flags;
+            public uint Type;
+            public uint ID;
+            public uint LocId;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+            public string SerialNumber;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string Description;
+            public IntPtr ftHandle;
+        }
+
+        public class Functions
+        {
+            private const string MPSSE_LIB = "libmpsse";
+
+            // FTDIMPSSE_API void Init_libMPSSE(void);
+            [DllImport(MPSSE_LIB, CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+            public static extern void Init_libMPSSE();
+
+            // FTDIMPSSE_API void Cleanup_libMPSSE(void);
+            [DllImport(MPSSE_LIB, CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+            public static extern void Cleanup_libMPSSE();
+
+            // === I2C ===
+
+            // FTDIMPSSE_API FT_STATUS I2C_GetNumChannels(DWORD *numChannels);
+            [DllImport(MPSSE_LIB, CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+            public static extern FT_STATUS I2C_GetNumChannels(out int numChannels);
+
+            // FTDIMPSSE_API FT_STATUS I2C_GetChannelInfo(DWORD index, FT_DEVICE_LIST_INFO_NODE* chanInfo);
+            [DllImport(MPSSE_LIB, CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+            public static extern FT_STATUS I2C_GetChannelInfo(int index, out FT_DEVICE_LIST_INFO_NODE chanInfo);
+
+            // === SPI ===
+
+            // FTDIMPSSE_API FT_STATUS SPI_GetNumChannels(DWORD *numChannels);
+            [DllImport(MPSSE_LIB, CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+            public static extern FT_STATUS SPI_GetNumChannels(out int numChannels);
+        }
+    }
+}
