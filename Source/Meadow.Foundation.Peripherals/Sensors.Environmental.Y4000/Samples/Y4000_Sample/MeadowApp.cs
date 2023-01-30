9b9ba43cb7bffe98a7019a9048c33aea60c46c15
Adrian Stevens <adrian.stevens@gmail.com>
2023-01-30 12:05:27 -0800

    Y4000 sample cleanup

--- Source/Meadow.Foundation.Peripherals/Sensors.Environmental.Y4000/Samples/Y4000_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Environmental.Y4000/Samples/Y4000_Sample/MeadowApp.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Meadow;
-using Meadow.Devices;
 using Meadow.Foundation.Sensors.Environmental;
-using Meadow.Gateways.Bluetooth;
-using Meadow.Hardware;
 
 namespace Sensors.Environmental.Y4000_Sample
 {
-    public class MeadowApp : App<F7FeatherV1>
+    public class MeadowApp : App<F7FeatherV2>
     {
         //<!=SNIP=>
 
