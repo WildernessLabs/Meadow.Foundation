aa03581ebbc213ad579c2fc6ee6c210adaf65a77
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-01-30 10:06:12 -0800

    Sample target F7MeadowV2

--- Source/Meadow.Foundation.Peripherals/Sensors.Moisture.Capacitive/Samples/Capacitive_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Moisture.Capacitive/Samples/Capacitive_Sample/MeadowApp.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Sensors.Moisture.Capacitive_Sample
 {
-    public class MeadowApp : App<F7FeatherV1>
+    public class MeadowApp : App<F7FeatherV2>
     {
         //<!=SNIP=>
 
