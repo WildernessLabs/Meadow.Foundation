407ddd25735af5c5e9751c7d6c98484f40591fb4
Adrian Stevens <adrian.stevens@gmail.com>
2023-04-03 19:26:42 -0700

    Add 14-segment support to HT16K33 w sample

---
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Ht16k33/Driver/Ht16k33.14Character.cs
@@ -0,0 +1,35 @@
+﻿using Meadow.Foundation.Displays.Led;
+using System;
+
+namespace Meadow.Foundation.ICs.IOExpanders
+{
+    public partial class Ht16k33
+    {
+        /// <summary>
+        /// Set a 14-segment display to a specific character
+        /// </summary>
+        /// <param name="character">The ascii chracter</param>
+        /// <param name="displayIndex">The display index (0-3)</param>
+        public void Set14SegmentDisplay(char character, int displayIndex)
+        {
+            if (displayIndex < 0 || displayIndex > 3)
+            {
+                throw new IndexOutOfRangeException("Set14SegmentDisplay index must be 0, 1, 2 or 3");
+            }
+
+            int startIndex = 16 * displayIndex;
+
+            for (int i = 0; i < 16; i++)
+            {
+                if (FourteenSegment.IsEnabled((FourteenSegment.Segment)i, character))
+                {
+                    SetLed((byte)(startIndex + i), true);
+                }
+                else
+                {
+                    SetLed((byte)(startIndex + i), false);
+                }
+            }
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Ht16k33/Samples/Ht16k33_Character_Sample/MeadowApp.cs
@@ -0,0 +1,53 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.ICs.IOExpanders;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ICs.IOExpanders.HT16K33_Sample
+{
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        //<!=SNIP=>
+
+        Ht16k33 ht16k33;
+
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
+            ht16k33 = new Ht16k33(Device.CreateI2cBus());
+
+            return base.Initialize();
+        }
+
+        public override async Task Run()
+        {
+            ht16k33.Set14SegmentDisplay('F', 0);
+            ht16k33.Set14SegmentDisplay('7', 1);
+            ht16k33.Set14SegmentDisplay('v', 2);
+            ht16k33.Set14SegmentDisplay('2', 3);
+
+            ht16k33.UpdateDisplay();
+        }
+
+        //<!=SNOP=>
+
+        void LoopCharacters()
+        {
+            while (true)
+            {
+                for (int i = 32; i < 127; i++)
+                {
+                    ht16k33.Set14SegmentDisplay((char)i, 0);
+                    ht16k33.Set14SegmentDisplay((char)i, 1);
+                    ht16k33.Set14SegmentDisplay((char)i, 2);
+                    ht16k33.Set14SegmentDisplay((char)i, 3);
+
+                    ht16k33.UpdateDisplay();
+
+                    Thread.Sleep(250);
+                }
+            }
+        }
+    }
+}
