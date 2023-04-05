fd1816f8bbdd0cfaa17a55c204dea495e940d583
Adrian Stevens <adrian.stevens@gmail.com>
2023-04-05 13:21:29 -0700

    Switch console writelines to logger

--- Source/Meadow.Foundation.Peripherals/Sensors.Environmental.Pmsa003I/Samples/Pma003I_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Environmental.Pmsa003I/Samples/Pma003I_Sample/MeadowApp.cs
@@ -17,7 +17,7 @@ namespace Pmsa003i_Sample
 
         public override Task Run()
         {
-            Console.WriteLine("Run...");
+            Resolver.Log.Info("Run...");
 
             pmsa003i.StartUpdating(TimeSpan.FromSeconds(2));
 
@@ -48,19 +48,19 @@ namespace Pmsa003i_Sample
             int? particles_50microns,
             int? particles_100microns)> e)
         {
-            Console.WriteLine($"Standard Particulate Matter 1 micron: {e.New.StandardParticulateMatter_1micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
-            Console.WriteLine($"Standard Particulate Matter 2_5micron: {e.New.StandardParticulateMatter_2_5micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
-            Console.WriteLine($"Standard Particulate Matter 10 micron: {e.New.StandardParticulateMatter_10micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
-            Console.WriteLine($"Environmental Particulate Matter 1 micron: {e.New.EnvironmentalParticulateMatter_1micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
-            Console.WriteLine($"Environmental Particulate Matter 2.5 micron: {e.New.EnvironmentalParticulateMatter_2_5micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
-            Console.WriteLine($"Environmental Particulate Matter 10 micron: {e.New.EnvironmentalParticulateMatter_10micron.Value.MicroGramsPerMetersCubed} micrograms per m^3"); ;
+            Resolver.Log.Info($"Standard Particulate Matter 1 micron: {e.New.StandardParticulateMatter_1micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
+            Resolver.Log.Info($"Standard Particulate Matter 2_5micron: {e.New.StandardParticulateMatter_2_5micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
+            Resolver.Log.Info($"Standard Particulate Matter 10 micron: {e.New.StandardParticulateMatter_10micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
+            Resolver.Log.Info($"Environmental Particulate Matter 1 micron: {e.New.EnvironmentalParticulateMatter_1micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
+            Resolver.Log.Info($"Environmental Particulate Matter 2.5 micron: {e.New.EnvironmentalParticulateMatter_2_5micron.Value.MicroGramsPerMetersCubed} micrograms per m^3");
+            Resolver.Log.Info($"Environmental Particulate Matter 10 micron: {e.New.EnvironmentalParticulateMatter_10micron.Value.MicroGramsPerMetersCubed} micrograms per m^3"); ;
 
-            Console.WriteLine($"Count of particles - 0.3 microns: {e.New.particles_0_3microns.Value} in 0.1 liters of air");
-            Console.WriteLine($"Count of particles - 0.5 microns: {e.New.particles_0_5microns.Value} in 0.1 liters of air");
-            Console.WriteLine($"Count of particles - 10 microns: {e.New.particles_10microns.Value} in 0.1 liters of air");
-            Console.WriteLine($"Count of particles - 25 microns: {e.New.particles_25microns.Value} in 0.1 liters of air");
-            Console.WriteLine($"Count of particles - 50 microns: {e.New.particles_50microns.Value} in 0.1 liters of air");
-            Console.WriteLine($"Count of particles - 100 microns: {e.New.particles_100microns.Value} in 0.1 liters of air");
+            Resolver.Log.Info($"Count of particles - 0.3 microns: {e.New.particles_0_3microns.Value} in 0.1 liters of air");
+            Resolver.Log.Info($"Count of particles - 0.5 microns: {e.New.particles_0_5microns.Value} in 0.1 liters of air");
+            Resolver.Log.Info($"Count of particles - 10 microns: {e.New.particles_10microns.Value} in 0.1 liters of air");
+            Resolver.Log.Info($"Count of particles - 25 microns: {e.New.particles_25microns.Value} in 0.1 liters of air");
+            Resolver.Log.Info($"Count of particles - 50 microns: {e.New.particles_50microns.Value} in 0.1 liters of air");
+            Resolver.Log.Info($"Count of particles - 100 microns: {e.New.particles_100microns.Value} in 0.1 liters of air");
         }
 
         //<!=SNOP=>
