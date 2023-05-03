0a336f2151afc551a6a6b2964d8f1aeed45b4509
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-05-03 11:37:29 -0700

    LedBarGraph samples switched to Feather v2

--- Source/Meadow.Foundation.Core.Samples/Leds.PwmLedBarGraph_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Core.Samples/Leds.PwmLedBarGraph_Sample/MeadowApp.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Leds.PwmLedBarGraph_Sample
 {
-    public class MeadowApp : App<F7FeatherV1>
+    public class MeadowApp : App<F7FeatherV2>
     {
         //<!=SNIP=>
 
