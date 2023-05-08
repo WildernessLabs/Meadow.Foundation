31611112cdb8dab70fc40721b2d42cb8397c534a
Chris Tacke <ctacke@opennetcf.com>
2023-05-08 15:33:16 -0500

    Start of CT work

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Power.CurrentTransducer/Driver/CurrentTransducer.cs
@@ -0,0 +1,60 @@
+﻿using Meadow.Hardware;
+using Meadow.Peripherals.Sensors;
+using Meadow.Units;
+using System;
+using System.Threading.Tasks;
+
+namespace Meadow.Foundation.Sensors.Power
+{
+    public partial class CurrentTransducer : SamplingSensorBase<Current>
+    {
+        private IAnalogInputPort? _inputPort = null;
+        private ISensor<Voltage>? _inputSensor = null;
+
+        private Current _ctMaxCurrent;
+
+        public Voltage ReferenceVoltage { get; private set; }
+
+        public CurrentTransducer(IAnalogInputPort input, Voltage referenceVoltage, Current sensorMaximum)
+        {
+            _inputPort = input;
+            ReferenceVoltage = referenceVoltage;
+            _ctMaxCurrent = sensorMaximum;
+        }
+
+        public CurrentTransducer(ISensor<Voltage> input, Voltage referenceVoltage, Current sensorMaximum)
+        {
+            _inputSensor = input;
+            ReferenceVoltage = referenceVoltage;
+            _ctMaxCurrent = sensorMaximum;
+        }
+
+        protected override async Task<Current> ReadSensor()
+        {
+            Voltage rawReading;
+
+            if (_inputPort != null)
+            {
+                rawReading = await _inputPort.Read();
+            }
+            else
+            {
+                rawReading = await _inputSensor.Read();
+            }
+
+            var ratio = rawReading.Volts / ReferenceVoltage.Volts;
+            var reading = new Current(_ctMaxCurrent.Amps * ratio, Current.UnitType.Amps);
+            return reading;
+        }
+
+        public override void StartUpdating(TimeSpan? updateInterval = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StopUpdating()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Power.CurrentTransducer/Samples/Ina260_Sample/MeadowApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Power;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        //<!=SNIP=>
+
+        Ina260 ina260;
+
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
+
+            var bus = Device.CreateI2cBus();
+            ina260 = new Ina260(bus);
+
+            Resolver.Log.Info($"-- INA260 Sample App ---");
+            Resolver.Log.Info($"Manufacturer: {ina260.ManufacturerID}");
+            Resolver.Log.Info($"Die: {ina260.DieID}");
+            ina260.Updated += (s, v) =>
+            {
+                Resolver.Log.Info($"{v.New.Item2}V @ {v.New.Item3}A");
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        { 
+            ina260.StartUpdating(TimeSpan.FromSeconds(2));
+            return Task.CompletedTask;
+        }
+
+        //<!=SNOP=>
+    }
+}
