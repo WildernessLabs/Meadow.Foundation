e1ebcdfd68662c814e0ef89dc774d8796d728f97
Chris Tacke <ctacke@opennetcf.com>
2023-06-04 09:06:14 -0600

    added new logger implementations

---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/LogProviders/Driver/Meadow.LogProviders/FileLogger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Meadow.Logging
+{
+    public class FileLogger : ILogProvider
+    {
+        private string LogFilePath { get; }
+
+        public FileLogger()
+        {
+            LogFilePath = Path.Combine(Resolver.Device.PlatformOS.FileSystem.DocumentsDirectory, "meadow.log");
+            if (!File.Exists(LogFilePath))
+            {
+                File.Create(LogFilePath).Close();
+            }
+        }
+
+        public void Log(LogLevel level, string message)
+        {
+            switch (level)
+            {
+                case LogLevel.Warning:
+                case LogLevel.Error:
+                    LogToFile(message);
+                    break;
+            }
+        }
+
+        private void LogToFile(string message)
+        {
+            if (message.EndsWith(Environment.NewLine))
+            {
+                File.AppendAllText(LogFilePath, message);
+            }
+            else
+            {
+                File.AppendAllText(LogFilePath, message + Environment.NewLine);
+            }
+        }
+
+        public string[] GetLogContents()
+        {
+            if (!File.Exists(LogFilePath))
+            {
+                return new string[0];
+            }
+            return File.ReadLines(LogFilePath).ToArray();
+        }
+
+        public void TruncateLog()
+        {
+            File.Create(LogFilePath).Close();
+        }
+    }
+}
