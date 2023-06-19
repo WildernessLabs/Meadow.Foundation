2759856f0b3a3ff5ad46e618c1f20cb35725eaca
Adrian Stevens <adrian.stevens@gmail.com>
2023-06-18 21:22:06 -0700

    Update samples

--- Source/Meadow.Foundation.Peripherals/ICs.ADCs.Mcp3xxx/Samples/Mcp3008_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/ICs.ADCs.Mcp3xxx/Samples/Mcp3008_Sample/MeadowApp.cs
@@ -18,17 +18,42 @@ namespace ICs.IOExpanders.Mcp3008_Sample
 
         public override Task Initialize()
         {
+            Resolver.Log.Info("Initialize");
+
             IDigitalOutputPort chipSelectPort = Device.CreateDigitalOutputPort(Device.Pins.D01);
 
             mcp = new Mcp3008(Device.CreateSpiBus(), chipSelectPort);
 
             port = mcp.CreateAnalogInputPort(mcp.Pins.CH0, 32, TimeSpan.FromSeconds(1), new Voltage(3.3, Voltage.UnitType.Volts), Mcp3xxx.InputType.SingleEnded);
 
+            port.Updated += (s, result) =>
+            {
+                Console.WriteLine($"Analog event, new voltage: {result.New.Volts:N2}V, old: {result.Old?.Volts:N2}V");
+            };
+
+            var observer = IAnalogInputPort.CreateObserver(
+                handler: result =>
+                {
+                    Resolver.Log.Info($"Analog observer triggered; new: {result.New.Volts:n2}V, old: {result.Old?.Volts:n2}V");
+                },
+                filter: result =>
+                {
+                    if (result.Old is { } oldValue)
+                    {
+                        return (result.New - oldValue).Abs().Volts > 0.1;
+                    }
+                    else { return false; }
+                }
+            );
+            port.Subscribe(observer);
+
             return base.Initialize();
         }
 
         public override Task Run()
         {
+            Resolver.Log.Info("Run");
+
             port.StartUpdating();
 
             return Task.CompletedTask;
