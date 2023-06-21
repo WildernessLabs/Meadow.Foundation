774b5bd9f1fc52b91bf5641122bab574db3ba204
Chris Tacke <ctacke@opennetcf.com>
2023-06-21 11:55:49 -0500

    updates to CurrentTransducer

--- Source/Meadow.Foundation.Peripherals/Sensors.Power.CurrentTransducer/Driver/CurrentTransducer.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Power.CurrentTransducer/Driver/CurrentTransducer.cs
@@ -12,25 +12,35 @@ namespace Meadow.Foundation.Sensors.Power
     public partial class CurrentTransducer : SamplingSensorBase<Current>
     {
         /// <summary>
+        /// Raised when the value of the reading changes
+        /// </summary>
+        public event EventHandler<IChangeResult<Current>> CurrentUpdated = delegate { };
+
+        /// <summary>
         /// The analog input port connected to the transducer
         /// </summary>
-        protected IAnalogInputPort AnalogPort { get; }
+        protected IAnalogInputPort AnalogPort { get; private set; } = default!;
         /// <summary>
         /// The maximum voltage the CT outputs
         /// </summary>
-        protected Voltage MaxVoltage { get; }
+        protected Voltage MaxVoltage { get; private set; } = default!;
         /// <summary>
         /// The sensed current at the maximum output voltage
         /// </summary>
-        protected Current MaxCurrent { get; }
+        protected Current MaxCurrent { get; private set; } = default!;
         /// <summary>
         /// The minimum voltage the CT outputs
         /// </summary>
-        protected Voltage MinVoltage { get; }
+        protected Voltage MinVoltage { get; private set; } = default!;
         /// <summary>
         /// The sensed current at the minimum output voltage
         /// </summary>
-        protected Current MinCurrent { get; }
+        protected Current MinCurrent { get; private set; } = default!;
+
+        /// <summary>
+        /// The last sensed Current
+        /// </summary>
+        public Current? Current { get; protected set; }
 
         /// <summary>
         /// Creates a new CurrentTransducer instance
@@ -40,13 +50,58 @@ namespace Meadow.Foundation.Sensors.Power
         /// <param name="maxCurrent">The sensed current at the maximum output voltage</param>
         /// <param name="minVoltage">The minimum voltage the CT outputs</param>
         /// <param name="minCurrent">The sensed current at the minimum output voltage</param>
-        public CurrentTransducer(IAnalogInputPort analogPort, Voltage maxVoltage, Current maxCurrent, Voltage? minVoltage, Current? minCurrent)
+        public CurrentTransducer(IAnalogInputPort analogPort, Voltage maxVoltage, Current maxCurrent, Voltage? minVoltage = null, Current? minCurrent = null)
+        {
+            Initialize(analogPort, maxVoltage, maxCurrent, minVoltage, minCurrent);
+        }
+
+        /// <summary>
+        /// Creates a new CurrentTransducer instance
+        /// </summary>
+        protected CurrentTransducer()
+        {
+        }
+
+        /// <summary>
+        /// Initializes the CurrentTransducer instance.  Use this method when a derived class must do pre-initialization work
+        /// </summary>
+        /// <param name="analogPort">The analog input port connected to the transducer</param>
+        /// <param name="maxVoltage">The maximum voltage the CT outputs</param>
+        /// <param name="maxCurrent">The sensed current at the maximum output voltage</param>
+        /// <param name="minVoltage">The minimum voltage the CT outputs</param>
+        /// <param name="minCurrent">The sensed current at the minimum output voltage</param>
+        protected virtual void Initialize(IAnalogInputPort analogPort, Voltage maxVoltage, Current maxCurrent, Voltage? minVoltage = null, Current? minCurrent = null)
         {
             AnalogPort = analogPort;
             MaxVoltage = maxVoltage;
             MaxCurrent = maxCurrent;
-            MinVoltage = minVoltage.HasValue ? minVoltage.Value : new Voltage(0, Voltage.UnitType.Volts);
-            MinCurrent = minCurrent.HasValue ? minCurrent.Value : new Current(0, Current.UnitType.Amps);
+            MinVoltage = minVoltage ?? new Voltage(0, Voltage.UnitType.Volts);
+            MinCurrent = minCurrent ?? new Current(0, Units.Current.UnitType.Amps);
+
+            AnalogPort.Subscribe
+            (
+                IAnalogInputPort.CreateObserver(
+                    result =>
+                    {
+                        var newCurrent = ConvertVoltageToCurrent(result.New);
+                        var old = Current ?? new Current(0);
+
+                        var delta = Math.Abs(old.Amps - newCurrent.Amps);
+
+                        // TODO: make the filter below (0.01) user-settable
+                        if (!Current.HasValue || delta > 0.01)
+                        {
+                            ChangeResult<Current> changeResult = new ChangeResult<Current>()
+                            {
+                                New = ConvertVoltageToCurrent(result.New),
+                                Old = Current
+                            };
+                            Current = changeResult.New;
+                            RaiseEventsAndNotify(changeResult);
+                        }
+                    }
+                )
+            );
         }
 
         /// <summary>
@@ -54,7 +109,7 @@ namespace Meadow.Foundation.Sensors.Power
         /// </summary>
         /// <param name="voltage">The ADC voltage read by the AnalogPort</param>
         /// <returns>The current being sensed by the CT</returns>
-        public virtual Current ConvertVoltageToCurrent(Voltage voltage)
+        protected virtual Current ConvertVoltageToCurrent(Voltage voltage)
         {
             // the default implementation just does a simple linear conversion
             return new Current(
@@ -67,7 +122,9 @@ namespace Meadow.Foundation.Sensors.Power
         protected override async Task<Current> ReadSensor()
         {
             var adc = await AnalogPort.Read();
-            return ConvertVoltageToCurrent(adc);
+            var newCurrent = ConvertVoltageToCurrent(adc);
+            Current = newCurrent;
+            return newCurrent;
         }
 
         ///<inheritdoc/>
@@ -99,6 +156,16 @@ namespace Meadow.Foundation.Sensors.Power
                 // state machine
                 IsSampling = false;
             }
+        }
+
+        /// <summary>
+        /// Method to notify subscribers to CurrentUpdated event handler
+        /// </summary>
+        /// <param name="changeResult"></param>
+        protected override void RaiseEventsAndNotify(IChangeResult<Current> changeResult)
+        {
+            CurrentUpdated?.Invoke(this, changeResult);
+            base.RaiseEventsAndNotify(changeResult);
         }
     }
 }
