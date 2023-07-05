7219e18d8177bd1da5447aa1d25cd2a5cda6b30f
Chris Tacke <ctacke@opennetcf.com>
2023-07-05 09:52:40 -0500

    current transducer project clean-up

--- Source/Meadow.Foundation.Peripherals/Sensors.Power.CurrentTransducer/Driver/CurrentTransducer.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Power.CurrentTransducer/Driver/CurrentTransducer.cs
@@ -4,57 +4,56 @@ using Meadow.Units;
 using System;
 using System.Threading.Tasks;
 
-namespace Meadow.Foundation.Sensors.Power
+namespace Meadow.Foundation.Sensors.Power;
+
+public partial class CurrentTransducer : SamplingSensorBase<Current>
 {
-    public partial class CurrentTransducer : SamplingSensorBase<Current>
+    private IAnalogInputPort? _inputPort = null;
+    private ISensor<Voltage>? _inputSensor = null;
+
+    private Current _ctMaxCurrent;
+
+    public Voltage ReferenceVoltage { get; private set; }
+
+    public CurrentTransducer(IAnalogInputPort input, Voltage referenceVoltage, Current sensorMaximum)
     {
-        private IAnalogInputPort? _inputPort = null;
-        private ISensor<Voltage>? _inputSensor = null;
+        _inputPort = input;
+        ReferenceVoltage = referenceVoltage;
+        _ctMaxCurrent = sensorMaximum;
+    }
 
-        private Current _ctMaxCurrent;
+    public CurrentTransducer(ISensor<Voltage> input, Voltage referenceVoltage, Current sensorMaximum)
+    {
+        _inputSensor = input;
+        ReferenceVoltage = referenceVoltage;
+        _ctMaxCurrent = sensorMaximum;
+    }
 
-        public Voltage ReferenceVoltage { get; private set; }
+    protected override async Task<Current> ReadSensor()
+    {
+        Voltage rawReading;
 
-        public CurrentTransducer(IAnalogInputPort input, Voltage referenceVoltage, Current sensorMaximum)
+        if (_inputPort != null)
         {
-            _inputPort = input;
-            ReferenceVoltage = referenceVoltage;
-            _ctMaxCurrent = sensorMaximum;
+            rawReading = await _inputPort.Read();
+        }
+        else
+        {
+            rawReading = await _inputSensor.Read();
         }
 
-        public CurrentTransducer(ISensor<Voltage> input, Voltage referenceVoltage, Current sensorMaximum)
-        {
-            _inputSensor = input;
-            ReferenceVoltage = referenceVoltage;
-            _ctMaxCurrent = sensorMaximum;
-        }
+        var ratio = rawReading.Volts / ReferenceVoltage.Volts;
+        var reading = new Current(_ctMaxCurrent.Amps * ratio, Current.UnitType.Amps);
+        return reading;
+    }
 
-        protected override async Task<Current> ReadSensor()
-        {
-            Voltage rawReading;
+    public override void StartUpdating(TimeSpan? updateInterval = null)
+    {
+        throw new NotImplementedException();
+    }
 
-            if (_inputPort != null)
-            {
-                rawReading = await _inputPort.Read();
-            }
-            else
-            {
-                rawReading = await _inputSensor.Read();
-            }
-
-            var ratio = rawReading.Volts / ReferenceVoltage.Volts;
-            var reading = new Current(_ctMaxCurrent.Amps * ratio, Current.UnitType.Amps);
-            return reading;
-        }
-
-        public override void StartUpdating(TimeSpan? updateInterval = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void StopUpdating()
-        {
-            throw new NotImplementedException();
-        }
+    public override void StopUpdating()
+    {
+        throw new NotImplementedException();
     }
 }
