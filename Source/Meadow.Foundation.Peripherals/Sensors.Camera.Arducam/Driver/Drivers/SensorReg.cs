2ff29ab571cd80f500d6e9fd2cec5347333f43e5
Adrian Stevens <adrian.stevens@gmail.com>
2023-09-07 13:57:54 -0700

    Add OV2640 sensor reg

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Camera.Arducam/Driver/Drivers/SensorReg.cs
@@ -0,0 +1,14 @@
+﻿namespace Meadow.Foundation.Sensors.Camera
+{
+    public struct SensorReg
+    {
+        public byte Register;
+        public byte Value;
+
+        public SensorReg(byte register, byte value)
+        {
+            Register = register;
+            Value = value;
+        }
+    }
+}
