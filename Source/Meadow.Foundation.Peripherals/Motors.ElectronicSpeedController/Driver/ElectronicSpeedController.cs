e0e097ebae032fce0c9fd1c6021228cd3faeaa61
Adrian Stevens <adrian.stevens@gmail.com>
2023-11-07 12:23:51 -0800

    IDisposable added to motor drivers

--- Source/Meadow.Foundation.Peripherals/Motors.ElectronicSpeedController/Driver/ElectronicSpeedController.cs
+++ Source/Meadow.Foundation.Peripherals/Motors.ElectronicSpeedController/Driver/ElectronicSpeedController.cs
@@ -1,42 +1,33 @@
 ﻿using Meadow.Hardware;
+using System;
 
 namespace Meadow.Foundation.Motors
 {
     /// <summary>
-    /// Driver for Electronic Speed Controllers used, typically, to drive
-    /// motors via a PWM signal. To use, you generally have to first _calibrate_
-    /// the ESC via the following steps:
-    /// 1. De-power the ESC, set power to intended max point (e.g. `1.0` power)
-    /// 2. Power the ESC, wait for "happy tones" to indicate good power supply
-    /// then (possibly) two beeps to indicate max power limit set.
-    /// 3. Set the ESC power to intended minimum power point (e.g. `0.0` power)
-    /// and the ESC should provide one beep per every LiPo cell (`3.7V`) of
-    /// power supplied, and then a long beep.
-    /// 4. Optionally, per some ESCs, arm, by calling the `Arm()` method, which
-    /// will drop the power below 0.0;
+    /// Driver for Electronic Speed Controllers (ESCs) typically used to control motors via PWM signals.
+    /// To use, you generally have to calibrate the ESC through the following steps:
+    /// 1. Depower the ESC, set the power to the intended maximum point (e.g., 1.0).
+    /// 2. Power the ESC and wait for "happy tones" to indicate a good power supply, followed by possibly two beeps to indicate the max power limit set.
+    /// 3. Set the ESC power to the intended minimum power point (e.g., 0.0), and the ESC should provide one beep for every LiPo cell (3.7V) of power supplied, followed by a long beep.
+    /// 4. Optionally, for some ESCs, arm it by calling the `Arm()` method, which will drop the power below 0.0.
     /// </summary>
     public class ElectronicSpeedController
     {
         readonly IPwmPort pwmPort;
 
         /// <summary>
-        /// The pulse duration, in milliseconds, necessary to "arm" the ESC.
-        /// Default value is 0.5ms.
+        /// The pulse duration, in milliseconds, necessary to "arm" the ESC. Default value is 0.5ms.
         /// </summary>
         public float ArmingPulseDuration { get; set; } = 0.5f;
 
         /// <summary>
-        /// `0.0` -> `1.0`
+        /// Gets or sets the power of the ESC, in the range of 0.0 to 1.0.
         /// </summary>
         public float Power
         {
             get => power;
             set
             {
-                //if (!armed) {
-                //    Resolver.Log.Info("not armed.");
-                //    return;
-                //}
                 if (value < 0) { value = 0; }
                 if (value > 1) { value = 1; }
                 power = value;
@@ -46,29 +37,35 @@ namespace Meadow.Foundation.Motors
         float power = 0f;
 
         /// <summary>
-        /// Frequency (in Hz) of the PWM signal. Default is 50Hz. Set during
-        /// construction, and increase for higher quality ESC's that allow a
-        /// higher frequency PWM control signal.
+        /// Frequency (in Hz) of the PWM signal. The default is 50Hz. Increase for higher quality ESCs that allow higher frequency PWM control signals.
         /// </summary>
         public Units.Frequency Frequency => pwmPort.Frequency;
 
         /// <summary>
-        /// Initializes an electronic speed controller on the specified device 
-        /// pin, at the specified frequency.
+        /// Gets a value indicating whether the object has been disposed.
         /// </summary>
-        /// <param name="pwmPin">PWM capable pin.</param>
-        /// <param name="frequency">Frequency of the PWM signal. All ESCs should
-        /// support 50Hz, but some support much higher. Increase for finer grained
-        /// control of speed in time.</param>
-        public ElectronicSpeedController(IPin pwmPin, Units.Frequency frequency) :
-            this(pwmPin.CreatePwmPort(frequency))
-        { }
+        public bool IsDisposed { get; private set; }
 
         /// <summary>
-        /// Initializes an electronic speed controller on the specified device 
-        /// pin, at the specified frequency.
+        /// Gets a value indicating whether the port(s) used by the peripheral were created.
         /// </summary>
-        /// <param name="port">The `IPwmPort` that will drive the ESC.</param>
+        readonly bool createdPorts = false;
+
+        /// <summary>
+        /// Creates a new ElectronicSpeedController object.
+        /// </summary>
+        /// <param name="pwmPin">A PWM capable pin.</param>
+        /// <param name="frequency">The frequency of the PWM signal. All ESCs should support 50Hz, but some support much higher frequencies for finer-grained speed control.</param>
+        public ElectronicSpeedController(IPin pwmPin, Units.Frequency frequency) :
+            this(pwmPin.CreatePwmPort(frequency))
+        {
+            createdPorts = true;
+        }
+
+        /// <summary>
+        /// Initializes an electronic speed controller on the specified device pin, at the specified frequency.
+        /// </summary>
+        /// <param name="port">The IPwmPort that will drive the ESC.</param>
         public ElectronicSpeedController(IPwmPort port)
         {
             pwmPort = port;
@@ -84,31 +81,51 @@ namespace Meadow.Foundation.Motors
         }
 
         /// <summary>
-        /// Calculates the appropriate duty cycle of a PWM signal for the given
-        /// pulse duration and frequency.
+        /// Calculates the appropriate duty cycle of a PWM signal for the given pulse duration and frequency.
         /// </summary>
-        /// <param name="pulseDuration">The duration of the target pulse, in milliseconds.</param>
-        /// <param name="frequency">The frequency of the PWM.</param>
+        /// <param name="pulseDuration">The duration of the target pulse in milliseconds.</param>
+        /// <param name="frequency">The frequency of the PWM signal.</param>
         /// <returns>A duty cycle value expressed as a percentage between 0.0 and 1.0.</returns>
         protected float CalculateDutyCycle(float pulseDuration, Units.Frequency frequency)
         {
-            // the pulse duration is dependent on the frequency we're driving the servo at
             return pulseDuration / ((1.0f / (float)frequency.Hertz) * 1000f);
         }
 
         /// <summary>
-        /// Returns a pulse duration, in milliseconds, for the given power,
-        /// assuming that the allowed power band is between 1ms and 2ms.
+        /// Returns a pulse duration in milliseconds for the given power, assuming that the allowed power band is between 1ms and 2ms.
         /// </summary>
-        /// <param name="power">A value between 0.0 and 1.0 representing the percentage
-        /// of power, between 0% and 100%, with 0.0 = 0%, and 1.0 = 100%.</param>
+        /// <param name="power">A value between 0.0 and 1.0 representing the percentage of power, with 0.0 = 0% and 1.0 = 100%.</param>
         /// <returns>A pulse duration in milliseconds for the given power.</returns>
         protected float CalculatePulseDuration(float power)
         {
-            //Resolver.Log.Info($"CalculatePulseDuration(power:{power:n1}) = {power * 1000} + 1000f");
-            // power band is between 1ms -> 2ms pulse durations.
-            // so 10% power = 1.1ms, 100% power = 2ms.
+            // Power band is between 1ms (10% power) and 2ms (100% power) pulse durations.
             return (power) + 1f;
+        }
+
+        /// <summary>
+        /// Dispose of the object.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Dispose of the object.
+        /// </summary>
+        /// <param name="disposing">A flag indicating whether disposing is in progress.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!IsDisposed)
+            {
+                if (disposing && createdPorts)
+                {
+                    pwmPort?.Dispose();
+                }
+
+                IsDisposed = true;
+            }
         }
     }
 }
