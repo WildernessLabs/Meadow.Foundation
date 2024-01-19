4871433aeee6caba34873991fffd203f5ee0de28
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-01-18 23:47:09 -0800

    Minor update

--- Source/Meadow.Foundation.Peripherals/Displays.WinForms/Samples/WinForms_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Displays.WinForms/Samples/WinForms_Sample/MeadowApp.cs
@@ -3,58 +3,57 @@ using Meadow.Foundation.Displays;
 using Meadow.Foundation.Graphics;
 using System.Windows.Forms;
 
-namespace WinForms_Sample
+namespace WinForms_Sample;
+
+//<!=SNIP=>
+
+public class MeadowApp : App<Meadow.Windows>
 {
-    public class MeadowApp : App<Meadow.Windows>
+    WinFormsDisplay _display;
+    MicroGraphics _graphics = default!;
+
+    public override Task Initialize()
     {
-        //<!=SNIP=>
+        _display = new WinFormsDisplay(240, 320);
 
-        WinFormsDisplay _display;
-        MicroGraphics _graphics = default!;
-
-        public override Task Initialize()
+        _graphics = new MicroGraphics(_display)
         {
-            _display = new WinFormsDisplay(240, 320);
+            CurrentFont = new Font12x20(),
+            Stroke = 1
+        };
 
-            _graphics = new MicroGraphics(_display)
-            {
-                CurrentFont = new Font12x20(),
-                Stroke = 1
-            };
-
-            _ = Task.Run(() =>
-            {
-                Thread.Sleep(2000);
-
-                _graphics.Clear();
-
-                _graphics.DrawTriangle(10, 30, 50, 50, 10, 50, Color.Red);
-                _graphics.DrawRectangle(20, 45, 40, 20, Color.Yellow, false);
-                _graphics.DrawCircle(50, 50, 40, Color.Blue, false);
-                _graphics.DrawText(5, 5, "Meadow F7", Color.White);
-
-                _graphics.Show();
-            });
-
-            return Task.CompletedTask;
-        }
-
-        public override Task Run()
+        _ = Task.Run(() =>
         {
-            Application.Run(_display);
+            Thread.Sleep(2000);
 
-            return Task.CompletedTask;
-        }
+            _graphics.Clear();
 
-        public static async Task Main(string[] args)
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            ApplicationConfiguration.Initialize();
+            _graphics.DrawTriangle(10, 30, 50, 50, 10, 50, Color.Red);
+            _graphics.DrawRectangle(20, 45, 40, 20, Color.Yellow, false);
+            _graphics.DrawCircle(50, 50, 40, Color.Blue, false);
+            _graphics.DrawText(5, 5, "Meadow F7", Color.White);
 
-            await MeadowOS.Start(args);
-        }
+            _graphics.Show();
+        });
 
-        //<!=SNOP=>
+        return Task.CompletedTask;
+    }
+
+    public override Task Run()
+    {
+        Application.Run(_display);
+
+        return Task.CompletedTask;
+    }
+
+    public static async Task Main(string[] args)
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        ApplicationConfiguration.Initialize();
+
+        await MeadowOS.Start(args);
     }
 }
+
+//<!=SNOP=>
