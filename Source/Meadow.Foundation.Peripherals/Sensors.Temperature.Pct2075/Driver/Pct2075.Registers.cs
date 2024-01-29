b10e7c806ebe164bf441a9de669ff6b676a18c5b
Chris Tacke <ctacke@opennetcf.com>
2024-01-29 12:42:08 -0600

    PCT2075 temp sensor

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Temperature.Pct2075/Driver/Pct2075.Registers.cs
@@ -0,0 +1,13 @@
+﻿namespace Meadow.Foundation.Sensors.Temperature;
+
+public partial class Pct2075
+{
+    internal enum Registers
+    {
+        Temp = 0x00,
+        Conf = 0x01,
+        Thyst = 0x02,
+        Tos = 0x03,
+        Tidle = 0x04
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Temperature.Pct2075/Samples/Pct2075_Sample/MeadowApp.cs
@@ -0,0 +1,38 @@
+﻿using Meadow;
+using Meadow.Foundation.ICs.IOExpanders;
+using Meadow.Foundation.Sensors.Temperature;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Sensors.Temperature.Pct2075_Sample;
+
+public class MeadowApp : App<Windows>
+{
+    //<!=SNIP=>
+    private Ft232h _expander;
+    private Pct2075 _sensor;
+
+    public override Task Initialize()
+    {
+        _expander = new Ft232h(false);
+        _sensor = new Pct2075(_expander.CreateI2cBus());
+
+        _sensor.Updated += OnUpdated;
+        _sensor.StartUpdating(TimeSpan.FromSeconds(1));
+
+        return base.Initialize();
+    }
+
+    private void OnUpdated(object sender, IChangeResult<Meadow.Units.Temperature> e)
+    {
+        Debug.WriteLine($"Temp: {e.New.Celsius:n1}C  {e.New.Fahrenheit:n1}F");
+    }
+
+    //<!=SNOP=>
+
+    public static async Task Main(string[] args)
+    {
+        await MeadowOS.Start(args);
+    }
+}
