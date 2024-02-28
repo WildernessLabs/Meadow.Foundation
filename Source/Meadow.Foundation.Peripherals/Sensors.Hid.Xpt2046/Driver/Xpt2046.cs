4c0b6ea321861a50589d7a783a14d45effd15854
Chris Tacke <ctacke@opennetcf.com>
2024-02-28 14:43:52 -0600

    Starting XPT2046 touch driver

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Hid.Xpt2046/Driver/Xpt2046.cs
@@ -0,0 +1,101 @@
+﻿using Meadow.Hardware;
+using System;
+using System.Threading;
+
+namespace Meadow.Foundation.Sensors.Hid
+{
+    /// <summary>
+    /// Represents an XPT2046 4-wire touch screen controller
+    /// </summary>
+    public partial class Xpt2046 : ITouchScreen
+    {
+        public event TouchEventHandler? TouchDown = null;
+        public event TouchEventHandler? TouchUp = null;
+        public event TouchEventHandler? TouchClick = null;
+
+        private readonly ISpiBus spiBus;
+        private readonly IDigitalInterruptPort touchInterrupt;
+        private readonly IDigitalOutputPort? chipSelect = null;
+
+        private bool isCollecting = false;
+
+        public Xpt2046(ISpiBus spiBus, IDigitalInterruptPort touchInterrupt, IDigitalOutputPort? chipSelect)
+        {
+            this.spiBus = spiBus;
+            this.touchInterrupt = touchInterrupt;
+            this.chipSelect = chipSelect;
+
+            if (touchInterrupt.InterruptMode != InterruptMode.EdgeBoth)
+            {
+                throw new ArgumentException("Interrupt port must be InterruptMode.EdgeBoth");
+            }
+
+            touchInterrupt.Changed += OnTouchInterrupt;
+        }
+
+        private void OnTouchInterrupt(object sender, DigitalPortResult e)
+        {
+            if (e.New.State)
+            {
+                new Thread(CollectTouchInfo).Start();
+            }
+            else
+            {
+                TouchUp?.Invoke(0, 0);
+                Resolver.Log.Info($"touch up");
+                isCollecting = false;
+            }
+        }
+
+        private const int SamplePeriodMilliseconds = 1000;
+
+        private void CollectTouchInfo()
+        {
+            if (isCollecting) return;
+            isCollecting = true;
+
+            TouchDown?.Invoke(0, 0);
+            Resolver.Log.Info($"touch down");
+
+            while (isCollecting)
+            {
+                var z = ReadZ();
+
+                var x = ReadX();
+                var y = ReadY();
+
+                Resolver.Log.Info($"Z:{z}  ({x},{y})");
+
+                Thread.Sleep(SamplePeriodMilliseconds);
+            }
+        }
+
+        private ushort ReadX()
+        {
+            return ReadChannel(0x91);
+        }
+
+        private ushort ReadY()
+        {
+            return ReadChannel(0xD1);
+        }
+
+        private ushort ReadZ()
+        {
+            return ReadChannel(0xC1);
+        }
+
+        private ushort ReadChannel(byte channelIdentifier)
+        {
+            Span<byte> txBuffer = stackalloc byte[1];
+            Span<byte> rxBuffer = stackalloc byte[2];
+
+            txBuffer[0] = channelIdentifier;
+
+            spiBus.Exchange(chipSelect, txBuffer, rxBuffer);
+
+            return (ushort)((rxBuffer[0] >> 3) << 8 | (rxBuffer[1] >> 3));
+        }
+
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Hid.Xpt2046/Samples/Tsc2004_Sample/MeadowApp.cs
@@ -0,0 +1,59 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Sensors.Hid;
+using Meadow.Hardware;
+using Meadow.Peripherals.Displays;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bbq10Keyboard_Sample
+{
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        //<!=SNIP=>
+
+        Tsc2004 touchScreen;
+
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initialize...");
+
+            var i2cBus = Device.CreateI2cBus(I2cBusSpeed.Fast);
+
+            touchScreen = new Tsc2004(i2cBus)
+            {
+                DisplayWidth = 240,
+                DisplayHeight = 320,
+                XMin = 260,
+                XMax = 3803,
+                YMin = 195,
+                YMax = 3852,
+                Rotation = RotationType._90Degrees
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+            return Task.Run(() =>
+            {
+                Point3d pt;
+
+                while (true)
+                {
+                    if (touchScreen.IsTouched())
+                    {
+                        pt = touchScreen.GetPoint();
+                        Resolver.Log.Info($"Location: X:{pt.X}, Y:{pt.Y}, Z:{pt.Z}");
+                    }
+
+                    Thread.Sleep(0);
+                }
+            });
+        }
+
+        //<!=SNOP=>
+    }
+}
