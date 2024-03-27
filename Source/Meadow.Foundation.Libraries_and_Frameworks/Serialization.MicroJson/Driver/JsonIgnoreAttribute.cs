77fa77c1d0ef71a369d52a610ec23ea034ea8311
Adrian Stevens <adrian.stevens@gmail.com>
2024-03-27 16:51:05 -0700

    Add xml comment for JsonIgnoreAttribute

--- Source/Meadow.Foundation.Libraries_and_Frameworks/Serialization.MicroJson/Driver/JsonIgnoreAttribute.cs
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Serialization.MicroJson/Driver/JsonIgnoreAttribute.cs
@@ -2,6 +2,15 @@
 
 namespace Meadow.Foundation.Serialization;
 
+/// <summary>
+/// Specifies that a property should be ignored when serializing an object to JSON.
+/// </summary>
+/// <remarks>
+/// This attribute can be applied to properties in a class to indicate that the property
+/// should not be included in the JSON representation of the object. This can be useful
+/// for properties that do not contain information relevant to the purpose of the serialization,
+/// or for properties that might contain sensitive information.
+/// </remarks>
 [AttributeUsage(AttributeTargets.Property, Inherited = true)]
 public class JsonIgnoreAttribute : Attribute
 {
