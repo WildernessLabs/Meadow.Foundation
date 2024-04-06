c547dc966b73c9d6650f9256693292b457663eb6
Chris Tacke <ctacke@opennetcf.com>
2024-04-05 21:24:18 -0500

    Adding vartical bar chart

---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics.MicroLayout/Driver/Charts/ChartControl.cs
@@ -0,0 +1,94 @@
+﻿namespace Meadow.Foundation.Graphics.MicroLayout;
+
+/// <summary>
+/// An abstract base class for Chart controls
+/// </summary>
+public abstract class ChartControl : ThemedControl
+{
+    private IFont _axisFont = default!;
+
+    /// <summary>
+    /// The default color for axis lines
+    /// </summary>
+    public static Color DefaultAxisColor = Color.Gray;
+
+    /// <summary>
+    /// The default color for axis labels
+    /// </summary>
+    public static Color DefaultAxisLabelColor = Color.White;
+
+    /// <summary>
+    /// The default chart background color
+    /// </summary>
+    public static Color DefaultBackgroundColor = Color.Black;
+
+    /// <summary>
+    /// The default width of the control margin
+    /// </summary>
+    protected const int DefaultMargin = 5;
+
+    /// <summary>
+    /// The default width of the control's axis lines
+    /// </summary>
+    protected const int DefaultAxisStroke = 4;
+
+    /// <summary>
+    /// The IFont used to for displaying axis labels
+    /// </summary>
+    public IFont? AxisFont { get; set; }
+    /// <summary>
+    /// The chart's background color
+    /// </summary>
+    public Color BackgroundColor { get; set; } = DefaultBackgroundColor;
+    /// <summary>
+    /// The color used to draw the chart axes lines
+    /// </summary>
+    public Color AxisColor { get; set; } = DefaultAxisColor;
+    /// <summary>
+    /// The color used to draw the chart axes labels
+    /// </summary>
+    public Color AxisLabelColor { get; set; } = DefaultAxisLabelColor;
+    /// <summary>
+    /// The width of the axes lines
+    /// </summary>
+    public int AxisStroke { get; set; } = DefaultAxisStroke;
+
+    protected int ChartAreaHeight { get; set; }
+    protected int ChartAreaWidth { get; set; }
+    protected int ChartAreaLeft { get; set; }
+    protected int ChartAreaTop { get; set; }
+    protected int ChartAreaBottom { get; set; }
+    protected int ParentOffsetX => (Parent?.Left ?? 0);
+    protected int ParentOffsetY => (Parent?.Top ?? 0);
+
+    /// <summary>
+    /// Creates a DisplayLineChart instance
+    /// </summary>
+    /// <param name="left">The control's left position</param>
+    /// <param name="top">The control's top position</param>
+    /// <param name="width">The control's width</param>
+    /// <param name="height">The control's height</param>
+    public ChartControl(int left, int top, int width, int height)
+        : base(left, top, width, height)
+    {
+    }
+
+    protected IFont GetAxisFont()
+    {
+        if (AxisFont == null)
+        {
+            _axisFont = new Font6x8();
+        }
+        else
+        {
+            _axisFont = AxisFont;
+        }
+
+        return _axisFont;
+    }
+
+    /// <inheritdoc/>
+    public override void ApplyTheme(DisplayTheme theme)
+    {
+    }
+}
---
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Graphics.MicroLayout/Driver/Charts/VerticalBarChart.cs
@@ -0,0 +1,94 @@
+﻿using System.Linq;
+
+namespace Meadow.Foundation.Graphics.MicroLayout;
+
+/// <summary>
+/// A vertical bar chart
+/// </summary>
+public class VerticalBarChart : ChartControl
+{
+    public float[]? Series { get; set; }
+    public int BarSpacing { get; set; } = 1;
+    public string XAxisLabelFormat { get; set; } = "N1";
+    public Color SeriesColor { get; set; } = Color.White;
+
+    public VerticalBarChart(int left, int top, int width, int height)
+        : base(left, top, width, height)
+    {
+    }
+
+    public VerticalBarChart(int left, int top, int width, int height, float[] series)
+        : base(left, top, width, height)
+    {
+        Series = series;
+    }
+
+    /// <inheritdoc/>
+    protected override void OnDraw(MicroGraphics graphics)
+    {
+        graphics.DrawRectangle(Left, Top, Width, Height, BackgroundColor, true);
+
+        var font = GetAxisFont();
+
+        ChartAreaTop = Top + DefaultMargin * 2 - AxisStroke;
+        ChartAreaBottom = Bottom - DefaultMargin - font.Height - AxisStroke;
+        ChartAreaHeight = Height - DefaultMargin * 3;
+
+        int yLabelWidth = 0;
+        if (Series != null)
+        {
+            // if we add Y labels (future feature), turn this on
+            //yLabelWidth = graphics.MeasureText(Series.Max().ToString(XAxisLabelFormat), font).Width;
+        }
+        ChartAreaLeft = Left + DefaultMargin + yLabelWidth;
+
+        ChartAreaWidth = Width - DefaultMargin;
+
+        if (Series != null)
+        {
+            DrawSeries(graphics, Series, font);
+        }
+
+        graphics.DrawRectangle(
+            ChartAreaLeft,
+            ChartAreaBottom,
+            ChartAreaWidth - DefaultMargin,
+            AxisStroke,
+            color: AxisColor,
+            filled: true);
+
+    }
+
+    private void DrawSeries(MicroGraphics graphics, float[] series, IFont font)
+    {
+        var barWidth = (ChartAreaWidth / series.Length) - 2 * BarSpacing;
+        var halfWidth = barWidth / 2;
+
+        var x = ChartAreaLeft + BarSpacing;
+
+        var heightScale = ChartAreaHeight * 0.9f / series.Max();
+
+        foreach (var item in series)
+        {
+            var barHeight = (int)(heightScale * item);
+
+            graphics.DrawRectangle(
+                x,
+                ChartAreaBottom - barHeight,
+                barWidth,
+                barHeight,
+                color: SeriesColor,
+                filled: true);
+
+            graphics.DrawText(
+                x + halfWidth,
+                ChartAreaBottom + DefaultMargin + AxisStroke,
+                alignmentH: HorizontalAlignment.Center,
+                color: AxisLabelColor,
+                text: item.ToString(XAxisLabelFormat),
+                font: font);
+
+            x += barWidth + 2 * BarSpacing;
+        }
+    }
+}
