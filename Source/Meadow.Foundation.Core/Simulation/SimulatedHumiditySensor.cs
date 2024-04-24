c4c9a7ef4db6911672ef2fd0fd5739a38eb749b0
Chris Tacke <ctacke@opennetcf.com>
2024-04-24 14:33:50 -0500

    added simulated moisture and humidity sensors

---
+++ Source/Meadow.Foundation.Core/Simulation/SimulatedHumiditySensor.cs
@@ -0,0 +1,35 @@
+﻿using Meadow.Peripherals.Sensors;
+using Meadow.Peripherals.Sensors.Atmospheric;
+using Meadow.Units;
+using System;
+
+namespace Meadow.Foundation.Sensors;
+
+public class SimulatedHumiditySensor : SimulatedSamplingSensorBase<RelativeHumidity>, IHumiditySensor
+{
+    private Random random = new Random();
+
+    public RelativeHumidity? Humidity { get; private set; }
+    public override Type ValueType => typeof(RelativeHumidity);
+
+    public SimulatedHumiditySensor()
+    {
+        Humidity = new RelativeHumidity(25, RelativeHumidity.UnitType.Percent);
+    }
+
+    public override void SetSensorValue(object value)
+    {
+        Humidity = (RelativeHumidity)value;
+    }
+
+    protected override RelativeHumidity GenerateSimulatedValue(SimulationBehavior behavior)
+    {
+        switch (behavior)
+        {
+            case SimulationBehavior.RandomWalk:
+                var delta = random.NextDouble() * 10d - 5d;
+                return new RelativeHumidity(Humidity.Value.Percent + delta);
+        }
+        return Humidity!.Value;
+    }
+}
---
+++ Source/Meadow.Foundation.Core/Simulation/SimulatedMoistureSensor.cs
@@ -0,0 +1,38 @@
+﻿using Meadow.Peripherals.Sensors;
+using Meadow.Peripherals.Sensors.Moisture;
+using System;
+
+namespace Meadow.Foundation.Sensors;
+
+public class SimulatedMoistureSensor : SimulatedSamplingSensorBase<double>, IMoistureSensor
+{
+    private Random random = new Random();
+
+    public double? Moisture { get; private set; }
+    public override Type ValueType => typeof(double);
+
+    public SimulatedMoistureSensor()
+    {
+        Moisture = 0.5d;
+    }
+
+    public override void SetSensorValue(object value)
+    {
+        var val = Convert.ToDouble(value);
+        if (val < 0) val = 0d;
+        if (val > 1) val = 1.0d;
+
+        Moisture = val;
+    }
+
+    protected override double GenerateSimulatedValue(SimulationBehavior behavior)
+    {
+        switch (behavior)
+        {
+            case SimulationBehavior.RandomWalk:
+                var delta = random.NextDouble() / 100d - 0.5;
+                return Moisture!.Value + delta;
+        }
+        return Moisture ?? 0;
+    }
+}
