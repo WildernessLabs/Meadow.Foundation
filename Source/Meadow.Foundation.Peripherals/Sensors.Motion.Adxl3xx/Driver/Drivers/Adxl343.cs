f0ced5337c07083f330848a98d2ce31312130405
Adrian Stevens <adrian.stevens@gmail.com>
2024-05-01 19:05:57 -0700

    Add ADXL343 driver and sample

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Motion.Adxl3xx/Driver/Drivers/Adxl343.cs
@@ -0,0 +1,231 @@
+﻿using Meadow.Foundation.Helpers;
+using Meadow.Hardware;
+using Meadow.Peripherals.Sensors.Motion;
+using Meadow.Units;
+using System;
+using System.Threading.Tasks;
+
+namespace Meadow.Foundation.Sensors.Motion
+{
+    /// <summary>
+    /// Driver for the ADXL343 triple axis accelerometer
+    /// +/- 16g
+    /// </summary>
+    public partial class Adxl343 : ByteCommsSensorBase<Acceleration3D>, IAccelerometer, II2cPeripheral
+    {
+        /// <summary>
+        /// The default I2C address for the peripheral
+        /// </summary>
+        public byte DefaultI2cAddress => (byte)Addresses.Default;
+
+        readonly double ADXL343_MG2G_MULTIPLIER = 0.004;
+
+        /// <summary>
+        /// Minimum value that can be used for the update interval when the
+        /// sensor is being configured to generate interrupts
+        /// </summary>
+        public const ushort MinimumPollingPeriod = 100;
+
+        /// <summary>
+        /// Current acceleration
+        /// </summary>
+        public Acceleration3D? Acceleration3D => Conditions;
+
+        /// <summary>
+        /// Values stored in this register are automatically added to the X reading
+        /// </summary>
+        /// <remarks>
+        /// Scale factor is 15.6 mg/LSB so 0x7f represents an offset of 2g
+        /// </remarks>
+        public sbyte OffsetX
+        {
+            get => (sbyte)ReadRegister(Register.OFFSET_X);
+            set => WriteRegister(Register.OFFSET_X, (byte)value);
+        }
+
+        /// <summary>
+        /// Values stored in this register are automatically added to the Y reading
+        /// </summary>
+        /// <remarks>
+        /// Scale factor is 15.6 mg/LSB so 0x7f represents an offset of 2g
+        /// </remarks>
+        public sbyte OffsetY
+        {
+            get => (sbyte)ReadRegister(Register.OFFSET_Y);
+            set => WriteRegister(Register.OFFSET_Y, (byte)value);
+        }
+
+        /// <summary>
+        /// Values stored in this register are automatically added to the Z reading
+        /// </summary>
+        /// <remarks>
+        /// Scale factor is 15.6 mg/LSB so 0x7f represents an offset of 2g
+        /// </remarks>
+        public sbyte OffsetZ
+        {
+            get => (sbyte)ReadRegister(Register.OFFSET_Z);
+            set => WriteRegister(Register.OFFSET_Z, (byte)value);
+        }
+
+        /// <summary>
+        /// Create a new instance of the ADXL343 communicating over the I2C interface
+        /// </summary>
+        /// <param name="address">Address of the I2C sensor</param>
+        /// <param name="i2cBus">I2C bus</param>
+        public Adxl343(II2cBus i2cBus, Addresses address = Addresses.Default)
+            : this(i2cBus, (byte)address)
+        {
+        }
+
+        /// <summary>
+        /// Create a new instance of the ADXL343 communicating over the I2C interface
+        /// </summary>
+        /// <param name="address">Address of the I2C sensor</param>
+        /// <param name="i2cBus">I2C bus</param>
+        public Adxl343(II2cBus i2cBus, byte address)
+            : base(i2cBus, address)
+        {
+            var deviceID = ReadRegister(Register.DEVICE_ID);
+
+            if (deviceID != 0xe5)
+            {
+                throw new Exception("Invalid device ID.");
+            }
+        }
+
+        /// <summary>
+        /// Read sensor
+        /// </summary>
+        /// <returns>Current acceleration</returns>
+        protected override Task<Acceleration3D> ReadSensor()
+        {
+            BusComms.ReadRegister((byte)Register.DATAX0, ReadBuffer.Span[0..6]);
+
+            var conditions = new Acceleration3D(
+                new Acceleration(ADXL343_MG2G_MULTIPLIER * (short)(ReadBuffer.Span[0] + (ReadBuffer.Span[1] << 8)), Acceleration.UnitType.Gravity),
+                new Acceleration(ADXL343_MG2G_MULTIPLIER * (short)(ReadBuffer.Span[2] + (ReadBuffer.Span[3] << 8)), Acceleration.UnitType.Gravity),
+                new Acceleration(ADXL343_MG2G_MULTIPLIER * (short)(ReadBuffer.Span[4] + (ReadBuffer.Span[5] << 8)), Acceleration.UnitType.Gravity));
+
+            return Task.FromResult(conditions);
+        }
+
+        /// <summary>
+        /// Set the PowerControl register (see pages 25 and 26 of the data sheet)
+        /// </summary>
+        /// <param name="linkActivityAndInactivity">Link the activity and inactivity events</param>
+        /// <param name="autoSleep">Enable / disable auto sleep when the activity and inactivity are linked</param>
+        /// <param name="measuring">Enable or disable measurements (turn on or off)</param>
+        /// <param name="sleep">Put the part to sleep (true) or run in normal more (false)</param>
+        /// <param name="frequency">Frequency of measurements when the part is in sleep mode</param>
+        public void SetPowerState(bool linkActivityAndInactivity, bool autoSleep, bool measuring, bool sleep, Frequencies frequency)
+        {
+            byte data = 0;
+            if (linkActivityAndInactivity)
+            {
+                data |= 0x20;
+            }
+            if (autoSleep)
+            {
+                data |= 0x10;
+            }
+            if (measuring)
+            {
+                data |= 0x08;
+            }
+            if (sleep)
+            {
+                data |= 0x40;
+            }
+            data |= (byte)frequency;
+
+            WriteRegister(Register.POWER_CONTROL, data);
+        }
+
+        /// <summary>
+        /// Configure the data format (see pages 26 and 27 of the data sheet).
+        /// </summary>
+        /// <param name="selfTest">Put the device into self test mode when true</param>
+        /// <param name="spiMode">Use 3-wire SPI (true) or 4-wire SPI (false)</param>
+        /// <param name="fullResolution">
+        /// Set to full resolution (true) or 10-bit mode using the range determined by the range
+        /// parameter (false).
+        /// </param>
+        /// <param name="justification">Left-justified when true, right justified with sign extension when false</param>
+        /// <param name="range">Set the range of the sensor to 2g, 4g, 8g or 16g</param>
+        /// <remarks>
+        /// The range of the sensor is determined by the following table:
+        /// 0:  +/- 2g
+        /// 1:  +/- 4g
+        /// 2:  +/- 8g
+        /// 3:  +/ 16g
+        /// </remarks>
+        public void SetDataFormat(bool selfTest, bool spiMode, bool fullResolution, bool justification, GForceRanges range)
+        {
+            byte data = 0;
+            if (selfTest)
+            {
+                data |= 0x80;
+            }
+            if (spiMode)
+            {
+                data |= 0x40;
+            }
+            if (fullResolution)
+            {
+                data |= 0x04;
+            }
+            if (justification)
+            {
+                data |= 0x02;
+            }
+            data |= (byte)range;
+
+            WriteRegister(Register.DATA_FORMAT, data);
+        }
+
+        /// <summary>
+        /// Set the data rate and low power mode for the sensor.
+        /// </summary>
+        /// <param name="dataRate">Data rate for the sensor</param>
+        /// <param name="lowPower">
+        /// Setting this to true will enter low power mode (note measurement will encounter more noise in
+        /// this mode).
+        /// </param>
+        public void SetDataRate(byte dataRate, bool lowPower)
+        {
+            if (dataRate > 0xff)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dataRate), "Data rate should be in the range 0-15 inclusive");
+            }
+
+            var data = dataRate;
+
+            if (lowPower)
+            {
+                data |= 0x10;
+            }
+
+            WriteRegister(Register.DATA_RATE, data);
+        }
+
+        private void WriteRegister(Register register, byte value)
+        {
+            BusComms.WriteRegister((byte)register, value);
+        }
+
+        private byte ReadRegister(Register register)
+        {
+            return BusComms.ReadRegister((byte)register);
+        }
+
+        /// <summary>
+        /// Dump the registers to the debug output stream.
+        /// </summary>
+        public void DisplayRegisters()
+        {
+            byte[] registerData = new byte[29];
+            BusComms.ReadRegister((byte)Register.TAP_THRESHOLD, registerData);
+            DebugInformation.DisplayRegisters((byte)Register.TAP_THRESHOLD, registerData);
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Motion.Adxl3xx/Driver/Drivers/Adxl343_Extras/Adxl343.Registers.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Meadow.Foundation.Sensors.Motion
+{
+    public partial class Adxl343
+    {
+        /// <summary>
+        /// Control registers for the ADXL345 chip.
+        /// </summary>
+        /// <remarks>
+        /// Taken from table 19 on page 23 of the data sheet.
+        /// </remarks>
+        private enum Register : byte
+        {
+            DEVICE_ID = 0x00,
+            TAP_THRESHOLD = 0x1D,
+            OFFSET_X = 0x1E,
+            OFFSET_Y = 0x1F,
+            OFFSET_Z = 0x20,
+            TAP_DURATION = 0x21,
+            TAP_LATENCY = 0x22,
+            TAP_WINDOW = 0x23,
+            ACTIVITY_THRESHOLD = 0x24,
+            INACTIVITY_THRESHOLD = 0x25,
+            INACTIVITY_TIME = 0x26,
+            ACTIVITY_INACTIVITY_CONTROL = 0x27,
+            FREEFALL_THRESHOLD = 0x28,
+            FREEFALL_TIME = 0x29,
+            TAP_AXES = 0x2A,
+            TAP_ACTIVITY_STATUS = 0x2B,
+            DATA_RATE = 0x2C,
+            POWER_CONTROL = 0x2D,
+            INTERRUPT_ENABLE = 0x2E,
+            INTERRUPT_MAP = 0x2F,
+            INTERRUPT_SOURCE = 0x30,
+            DATA_FORMAT = 0x31,
+            DATAX0 = 0x32,
+            DATAX1 = 0x33,
+            DATAY0 = 0x33,
+            DATAY1 = 0x34,
+            DATAZ0 = 0x36,
+            DATAZ1 = 0x37,
+            FIFO_CONTROL = 0x38,
+            FIFO_STATUS = 0x39,
+        }
+    }
+}
