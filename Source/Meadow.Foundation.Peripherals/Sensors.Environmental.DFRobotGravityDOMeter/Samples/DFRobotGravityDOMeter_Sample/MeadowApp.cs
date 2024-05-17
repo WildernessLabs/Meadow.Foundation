0097b9a0000540f7c382ab8ce99e2c9589a48806
Jamie Boyd <jadobo@gmail.com>
2024-05-16 22:17:59 -0700

    DO meter

--- Source/Meadow.Foundation.Peripherals/Sensors.Environmental.DFRobotGravityDOMeter/Samples/DFRobotGravityDOMeter_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Environmental.DFRobotGravityDOMeter/Samples/DFRobotGravityDOMeter_Sample/MeadowApp.cs
@@ -1,46 +1,74 @@
 ﻿using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation.Sensors.Environmental;
+using Meadow.Foundation.Sensors.Temperature;
+using Meadow.Hardware;
 using Meadow.Units;
 using System;
 using System.Threading.Tasks;
 
+
+
 namespace Sensors.Environmental.DFRobotGravityDOMeter_Sample
 {
-    public class MeadowApp : App<F7FeatherV2>
+    public class MeadowApp : App<F7CoreComputeV2>
     {
         //<!=SNIP=>
 
-        DFRobotGravityDOMeter sensor;
-
+        IProjectLabHardware projectLab;
+        DFRobotGravityDOMeter DOsensor;
+        SteinhartHartCalculatedThermistor tempSensor;
+        protected FilterableChangeObserver<Meadow.Units.Temperature> tempConsumer;
+        protected FilterableChangeObserver<ConcentrationInWater> DOconsumer;
         public override Task Initialize()
         {
             Resolver.Log.Info("Initialize...");
+            projectLab = ProjectLab.Create();
+            tempSensor = new SteinhartHartCalculatedThermistor(
+                projectLab.GroveAnalog.Pins.D0.CreateAnalogInputPort(10),
+                new Resistance(10, Resistance.UnitType.Kiloohms));
+            IAnalogInputPort DOport = projectLab.IOTerminal.Pins.A1.CreateAnalogInputPort(10, TimeSpan.FromMilliseconds(40), new Voltage(3.3));
+            DOsensor = new DFRobotGravityDOMeter(DOport, tempSensor);
 
-            sensor = new DFRobotGravityDOMeter(Device.Pins.A01) {
-                WaterTemperature = new Meadow.Units.Temperature(17, Meadow.Units.Temperature.UnitType.Celsius)
-            };
-
-            sensor.WaterTemperature = new Meadow.Units.Temperature(12, Meadow.Units.Temperature.UnitType.Celsius);
-            // Example that uses an IObservable subscription to only be notified when the saturation changes
-            var consumer = DFRobotGravityDOMeter.CreateObserver(
+            tempConsumer = SteinhartHartCalculatedThermistor.CreateObserver(
                 handler: result =>
                 {
-                    string oldValue = (result.Old is { } old) ? $"{old.MilligramsPerLiter:n0}" : "n/a";
-                    string newValue = $"{result.New.MilligramsPerLiter:n3}";
-                    Resolver.Log.Info($"New: {newValue}mg/l, Old: {oldValue}mg/l");
+                    Resolver.Log.Info($"Water Temp: {result.New.Celsius:N2}°C");
                 },
-                filter: null
+                filter: result =>
+                {
+                    if (result.Old is { } old)
+                    {
+                        return (result.New - old).Abs().Celsius > 0.1; // returns true if > 0.5°C change.
+                    }
+                    return false;
+                }
             );
-            sensor.Subscribe(consumer);
+            tempSensor.Subscribe(tempConsumer);
 
-            // optional classical .NET events can also be used:
-            sensor.Updated += (sender, result) =>
+             DOconsumer = DFRobotGravityDOMeter.CreateObserver(
+                 handler: result =>
+                {
+                    Resolver.Log.Info($"0xygen Conc:{result.New.MilligramsPerLiter:N0}mg/l");
+                    //string oldValue = (result.Old is { } old) ? $"{old.MilligramsPerLiter:n0}" : "n/a";
+                    //string newValue = $"{result.New.MilligramsPerLiter:n0}";
+                    //Resolver.Log.Info($"New: {newValue}mg/l, Old: {oldValue}mg/l");
+                },
+                //filter: null
+            filter: result =>
             {
-                string oldValue = (result.Old is { } old) ? $"{old.MilligramsPerLiter}mg/l" : "n/a";
-                Resolver.Log.Info($"Updated - New: {result.New.MilligramsPerLiter:n0}mg/l, Old: {oldValue}");
+                if (result.Old is { } old)
+                {
+                    return (result.New - old).Abs().MilligramsPerLiter > 0.05; // returns true if > 0.1 mg/L.
+                }
+                return false;
+            } 
 
-            };
+            );
+            DOsensor.Subscribe(DOconsumer);
+
+
+
 
             return Task.CompletedTask;
         }
@@ -48,16 +76,17 @@ namespace Sensors.Environmental.DFRobotGravityDOMeter_Sample
         public override async Task Run()
         {
             Resolver.Log.Info("Run...");
-
             await ReadSensor();
-
-            sensor.StartUpdating(TimeSpan.FromSeconds(2));
+            DOsensor.StartUpdating(TimeSpan.FromSeconds(1));
+            tempSensor.StartUpdating(TimeSpan.FromSeconds(1));
         }
 
         protected async Task ReadSensor()
         {
-            var concentration = await sensor.Read();
-            Resolver.Log.Info($"Initial concentration: {concentration.MilligramsPerLiter:N0}mg/l");
+            DOsensor.WaterTemperature = await tempSensor.Read();
+            var concentration = await DOsensor.Read();
+            Resolver.Log.Info($"Water Temp: {DOsensor.WaterTemperature.Celsius:N2}°C");
+            Resolver.Log.Info($"Oxygen concentration: {concentration.MilligramsPerLiter:N0}mg/l");
         }
 
         //<!=SNOP=>
