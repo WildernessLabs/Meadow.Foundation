9c365bcc4f6396da161eca6b68d317bc4f0c5ad7
Jamie Boyd <jadobo@gmail.com>
2024-05-22 12:27:34 -0700

    Updating Conditions

--- Source/Meadow.Foundation.Peripherals/Sensors.Distance.A02yyuw/Samples/A02yyuw_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Distance.A02yyuw/Samples/A02yyuw_Sample/MeadowApp.cs
@@ -20,13 +20,14 @@ namespace A02yyuw_Sample
         //<!=SNIP=>
 
         A02yyuw a02yyuw;
-
+        protected TimeSpan updateInterval = TimeSpan.FromSeconds(10);
         public override Task Initialize()
         {
             Resolver.Log.Info("Initialize...");
 
             a02yyuw = new A02yyuw(Device, Device.PlatformOS.GetSerialPortName("COM4"), A02yyuw.MODE_UART_CONTROL);
 
+            /******* Using the Observer/consumer approach, with optional filter************
             var consumer = A02yyuw.CreateObserver(
                 handler: result =>
                 {
@@ -43,26 +44,32 @@ namespace A02yyuw_Sample
             );
             a02yyuw.Subscribe(consumer);
 
+            /********************** /Using the IChangeResult interface *******************
             a02yyuw.Updated += A02yyuw_DistanceUpdated;
-
+            ********************** IChangeResult interface *******************/
             return Task.CompletedTask;
         }
+
 
         public override async Task Run()
         {
             Resolver.Log.Info("Run...");
-
-            var distance = await a02yyuw.Read();
-            Resolver.Log.Info($"Initial distance is: {distance.Centimeters:N1}cm / {distance.Inches:N1}in");
+            var distance = await a02yyuw.Read(); // read once and log
+            Resolver.Log.Info($"Initial distance is: {distance.Centimeters:N1}cm");
 
             a02yyuw.StartUpdating(TimeSpan.FromSeconds(2));
+            while (true)
+            {
+                Resolver.Log.Info($"Last Distance: {a02yyuw.Conditions.Centimeters:N1} cm");
+                await Task.Delay(updateInterval);
+            }
         }
 
+        // function called when using the IChangeResult interface when distance measure is updated
         private void A02yyuw_DistanceUpdated(object sender, IChangeResult<Length> e)
         {
-            Resolver.Log.Info($"Distance: {e.New.Centimeters:N1}cm / {e.New.Inches:N1}in");
+            Resolver.Log.Info($"Distance: {e.New.Centimeters:N1}cm");
         }
-
         //<!=SNOP=>
     }
 }
