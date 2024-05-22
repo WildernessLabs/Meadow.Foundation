605dc6568826d6ef862cd39fd53ca56cd61d300d
Jamie Boyd <jadobo@gmail.com>
2024-05-22 11:41:19 -0700

    Updating Conditions

--- Source/Meadow.Foundation.Peripherals/Sensors.Temperature.Thermistor/Samples/Thermistor_Sample/MeadowApp.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Temperature.Thermistor/Samples/Thermistor_Sample/MeadowApp.cs
@@ -1,67 +1,65 @@
 ﻿using Meadow;
 using Meadow.Devices;
-using Meadow.Hardware;
 using Meadow.Foundation.Sensors.Temperature;
+using Meadow.Gateways.Bluetooth;
+using Meadow.Hardware;
 using Meadow.Units;
 using System;
 using System.Threading.Tasks;
-using Meadow.Cloud;
-using Meadow.Peripherals.Sensors;
 
 namespace Sensors.Temperature.Thermistor_Sample
 {
-    //public class MeadowApp : App<F7FeatherV2>
-    public class MeadowApp : App<F7CoreComputeV2> // for projectLab
+    public class MeadowApp : App<F7CoreComputeV2>
     {
         //<!=SNIP=>
 
         private SteinhartHartCalculatedThermistor thermistor;
-        protected IProjectLabHardware projectLab;
-        protected FilterableChangeObserver<Meadow.Units.Temperature> tempConsumer;
-
+        private IProjectLabHardware projectLab;
         public override Task Initialize()
         {
             Resolver.Log.Info("Initialize...");
             projectLab = ProjectLab.Create();
-            IPin thePin = projectLab.GroveAnalog.Pins.D0;
-            //IPin thePin = projectLab.GroveAnalog.GetPin("A00");
-            IAnalogInputPort thePort = thePin.CreateAnalogInputPort(10, TimeSpan.FromMilliseconds(2), new Voltage(3.3));
-            //thermistor = new SteinhartHartCalculatedThermistor(Device.CreateAnalogInputPort(Device.Pins.A00), new Resistance(10, Meadow.Units.Resistance.UnitType.Kiloohms));
-            thermistor = new SteinhartHartCalculatedThermistor(thePort, new Resistance(10, Meadow.Units.Resistance.UnitType.Kiloohms));
-            tempConsumer = SteinhartHartCalculatedThermistor.CreateObserver(
-             handler: result =>
-             {
-                Resolver.Log.Info($"Consumer Water Temp: {result.New.Celsius:N2}°C");
-             },
-            // only notify if the change is greater than 0.5°C
-            filter: result =>
+            thermistor = new SteinhartHartCalculatedThermistor(projectLab.GroveAnalog.Pins.D0.CreateAnalogInputPort(10), new Resistance(10, Meadow.Units.Resistance.UnitType.Kiloohms));
+
+            var consumer = SteinhartHartCalculatedThermistor.CreateObserver(
+            handler: result =>
             {
-                if (result.Old is { } old)
-                {
-                    return (result.New - old).Abs().Celsius > 0.1; // returns true if > 0.5°C change.
-                }
-                return false;
-            }
+                Resolver.Log.Info($"Handler Temperature New Value: {result.New.Celsius:N1} °C");
+                Resolver.Log.Info($"Handler From Thermistor {thermistor.Conditions.Celsius:N1} °C");
+                },
+                filter: null
             );
-            thermistor.Subscribe(tempConsumer);
+            thermistor.Subscribe(consumer);
 
+            thermistor.Updated += Thermistor_TempreatureUpdated;
             /*
-            thermistor.Updated += (object sender, IChangeResult<Meadow.Units.Temperature> e) =>
+            (object sender, IChangeResult<Meadow.Units.Temperature> e) =>
             {
-                Resolver.Log.Info($"Temperature Updated: {e.New.Fahrenheit:N1}F/{e.New.Celsius:N1}C");
-            };*/
+                //Resolver.Log.Info($"Updated Temperature: {e.New.Celsius:N1} °C");
 
-            return Task.CompletedTask;
+            };
+            */
+            return base.Initialize();
         }
 
+        
         public override async Task Run()
         {
             var temp = await thermistor.Read();
-            Resolver.Log.Info($"Current temperature: {temp.Fahrenheit:N1}F/{temp.Celsius:N1}C");
+            Resolver.Log.Info($"Initial temperature: {temp.Fahrenheit:N1}F/{temp.Celsius:N1}C");
+            Resolver.Log.Info($"Inital From therm: {thermistor.Conditions.Fahrenheit:N1}F/{thermistor.Conditions.Celsius:N1}C");
+            thermistor.StartUpdating(TimeSpan.FromSeconds(1d));
+        }
 
-            thermistor.StartUpdating(TimeSpan.FromSeconds(1));
+
+        // function called when using the IChangeResult interface when temp measure is updated
+        private void Thermistor_TempreatureUpdated(object sender, IChangeResult<Meadow.Units.Temperature> e)
+        {
+            Resolver.Log.Info($"IChangeResultTemperature: {e.New.Celsius:N1}°C");
+            Resolver.Log.Info($"IChangeResult from Thermistor: {thermistor.Conditions.Celsius:N1}°C");
         }
 
         //<!=SNOP=>
     }
+   
 }
