d88990119f135f195721b5db7ab4c641dc5e2aa3
Chris Tacke <ctacke@opennetcf.com>
2024-05-30 16:58:48 -0500

    bug fixes for MCP23008

--- Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Mcp23xxx/Driver/Mcp23xxx.DigitalInterruptPort.cs
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Mcp23xxx/Driver/Mcp23xxx.DigitalInterruptPort.cs
@@ -1,92 +1,97 @@
 ﻿using Meadow.Hardware;
 using System;
 
-namespace Meadow.Foundation.ICs.IOExpanders
+namespace Meadow.Foundation.ICs.IOExpanders;
+
+public partial class Mcp23xxx
 {
-    public partial class Mcp23xxx
+    /// <summary>
+    /// Represents an Mcp23xxx digital interrupt port
+    /// </summary>
+    public class DigitalInterruptPort : DigitalInterruptPortBase
     {
-        /// <summary>
-        /// Represents an Mcp23xxx digital interrupt port
-        /// </summary>
-        public class DigitalInterruptPort : DigitalInterruptPortBase
+        /// <inheritdoc/>
+        public override bool State => state;
+        private bool state = false;
+
+        private DateTime lastUpdate;
+
+        /// <inheritdoc/>
+        public override ResistorMode Resistor
         {
-            /// <inheritdoc/>
-            public override bool State => state;
-            private bool state = false;
+            get => portResistorMode;
+            set => throw new NotSupportedException("Cannot change port resistor mode after the port is created");
+        }
+        private readonly ResistorMode portResistorMode;
 
-            private DateTime lastUpdate;
+        /// <inheritdoc/>
+        public override TimeSpan DebounceDuration { get; set; } = TimeSpan.Zero;
 
-            /// <inheritdoc/>
-            public override ResistorMode Resistor
+        /// <inheritdoc/>
+        public override TimeSpan GlitchDuration
+        {
+            get => TimeSpan.FromMilliseconds(0.00015);
+            set => _ = value; //fail silently
+        }
+
+        /// <summary>
+        /// Create a new DigitalInterruptPort object
+        /// </summary>
+        /// <param name="pin">The interrupt pin</param>
+        /// <param name="interruptMode">The interrupt mode used for the interrupt pin</param>
+        /// <param name="resistorMode">The resistor mode used by the interrupt pin</param>
+        public DigitalInterruptPort(IPin pin, InterruptMode interruptMode = InterruptMode.None, ResistorMode resistorMode = ResistorMode.Disabled)
+            : base(pin, (IDigitalChannelInfo)pin.SupportedChannels![0], interruptMode)
+        {
+            portResistorMode = resistorMode;
+
+            // seed the initial state
+            if (Pin.Controller is Mcp23xxx mcp)
             {
-                get => portResistorMode;
-                set => throw new NotSupportedException("Cannot change port resistor mode after the port is created");
+                Update(mcp.ReadPort(Pin));
             }
-            private readonly ResistorMode portResistorMode;
+        }
 
-            /// <inheritdoc/>
-            public override TimeSpan DebounceDuration { get; set; } = TimeSpan.Zero;
-
-            /// <inheritdoc/>
-            public override TimeSpan GlitchDuration
+        /// <summary>
+        /// Update the port value 
+        /// </summary>
+        /// <param name="newState">The new port state</param>
+        internal void Update(bool newState)
+        {
+            if (DateTime.UtcNow - lastUpdate < DebounceDuration)
             {
-                get => TimeSpan.FromMilliseconds(0.00015);
-                set => _ = value; //fail silently
+                return;
             }
 
-            /// <summary>
-            /// Create a new DigitalInterruptPort object
-            /// </summary>
-            /// <param name="pin">The interrupt pin</param>
-            /// <param name="interruptMode">The interrupt mode used for the interrupt pin</param>
-            /// <param name="resistorMode">The resistor mode used by the interrupt pin</param>
-            public DigitalInterruptPort(IPin pin, InterruptMode interruptMode = InterruptMode.None, ResistorMode resistorMode = ResistorMode.Disabled)
-                : base(pin, (IDigitalChannelInfo)pin.SupportedChannels![0], interruptMode)
-            {
-                portResistorMode = resistorMode;
-            }
+            var now = DateTime.UtcNow;
 
-            /// <summary>
-            /// Update the port value 
-            /// </summary>
-            /// <param name="newState">The new port state</param>
-            internal void Update(bool newState)
+            if (newState != state)
             {
-                if (DateTime.UtcNow - lastUpdate < DebounceDuration)
+                switch (InterruptMode)
                 {
-                    return;
+                    case InterruptMode.EdgeFalling:
+                        if (newState)
+                        {
+                            RaiseChangedAndNotify(new DigitalPortResult(new DigitalState(false, now), new DigitalState(true, lastUpdate)));
+                        }
+                        break;
+                    case InterruptMode.EdgeRising:
+                        if (newState)
+                        {
+                            RaiseChangedAndNotify(new DigitalPortResult(new DigitalState(true, now), new DigitalState(false, lastUpdate)));
+                        }
+                        break;
+                    case InterruptMode.EdgeBoth:
+                        RaiseChangedAndNotify(new DigitalPortResult(new DigitalState(newState, now), new DigitalState(!newState, lastUpdate)));
+                        break;
+                    case InterruptMode.None:
+                    default:
+                        break;
                 }
-
-                var now = DateTime.UtcNow;
-
-                if (newState != state)
-                {
-                    switch (InterruptMode)
-                    {
-                        case InterruptMode.EdgeFalling:
-                            if (newState)
-                            {
-                                RaiseChangedAndNotify(new DigitalPortResult(new DigitalState(false, now), new DigitalState(true, lastUpdate)));
-                            }
-                            break;
-                        case InterruptMode.EdgeRising:
-                            if (newState)
-                            {
-                                RaiseChangedAndNotify(new DigitalPortResult(new DigitalState(true, now), new DigitalState(false, lastUpdate)));
-                            }
-                            break;
-                        case InterruptMode.EdgeBoth:
-                            RaiseChangedAndNotify(new DigitalPortResult(new DigitalState(newState, now), new DigitalState(!newState, lastUpdate)));
-                            break;
-                        case InterruptMode.None:
-                        default:
-                            break;
-                    }
-                }
-
-                state = newState;
-                lastUpdate = now;
             }
+
+            state = newState;
+            lastUpdate = now;
         }
     }
 }
