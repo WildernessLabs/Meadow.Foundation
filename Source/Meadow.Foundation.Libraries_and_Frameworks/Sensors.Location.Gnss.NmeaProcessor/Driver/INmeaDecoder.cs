0c2f6844cc2315fb60c30b89d3e7f2ef019f5faa
Chris Tacke <ctacke@opennetcf.com>
2024-06-09 10:42:12 -0500

    [API Change] NMEA and GNSS processing changed.  Uses units, events are more standard

--- Source/Meadow.Foundation.Libraries_and_Frameworks/Sensors.Location.Gnss.NmeaProcessor/Driver/INmeaDecoder.cs
+++ Source/Meadow.Foundation.Libraries_and_Frameworks/Sensors.Location.Gnss.NmeaProcessor/Driver/INmeaDecoder.cs
@@ -1,27 +1,26 @@
 ﻿using Meadow.Peripherals.Sensors.Location.Gnss;
 
-namespace Meadow.Foundation.Sensors.Location.Gnss
+namespace Meadow.Foundation.Sensors.Location.Gnss;
+
+/// <summary>
+/// Base class for NMEA sentence decoder classes
+/// </summary>
+public interface INmeaDecoder
 {
     /// <summary>
-    /// Base class for NMEA sentence decoder classes
+    /// Prefix for the decoder (text that occurs at the start of a GPS message
+    /// including the $ symbol - $GPGSA etc.)
     /// </summary>
-    public interface INmeaDecoder
-    {
-        /// <summary>
-        /// Prefix for the decoder (text that occurs at the start of a GPS message
-        /// including the $ symbol - $GPGSA etc.)
-        /// </summary>
-        string Prefix { get; }
+    string Prefix { get; }
 
-        /// <summary>
-        /// Friendly name for the decoder
-        /// </summary>
-        string Name { get; }
+    /// <summary>
+    /// Friendly name for the decoder
+    /// </summary>
+    string Name { get; }
 
-        /// <summary>
-        /// Process the message from the GPS
-        /// </summary>
-        /// <param name="sentence">String array of the elements of the message</param>
-        void Process(NmeaSentence sentence);
-    }
+    /// <summary>
+    /// Process the message from the GPS
+    /// </summary>
+    /// <param name="sentence">String array of the elements of the message</param>
+    void Process(NmeaSentence sentence);
 }
