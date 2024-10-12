ea39de186a7609de57699cf60076107cfd6e0893
Chris Tacke <ctacke@opennetcf.com>
2024-10-12 10:33:10 -0500

    Starting work on the AD7768 ADC

---
+++ Source/Meadow.Foundation.Peripherals/ICs.ADC.Ad7768/Driver/Ad7768.Registers.cs
@@ -0,0 +1,92 @@
+﻿namespace Meadow.Foundation.ICs.ADC
+{
+    public partial class Ad7768
+    {
+        internal enum Mask : byte
+        {
+            SleepMode = 1 << 7
+        }
+
+        internal enum SleepMode : byte
+        {
+            Active = 0,
+            Sleep = 1
+        }
+
+        internal enum PowerMode : byte
+        {
+            Eco = 0,
+            Median = 2,
+            Fast = 3
+        }
+
+        internal enum MCLKDivisor : byte
+        {
+            Div32 = 0,
+            Div8 = 2,
+            Div4 = 3
+        }
+
+        internal enum DCLKDivisor : byte
+        {
+            Div8 = 0,
+            Div4 = 1,
+            Div2 = 2,
+            Div1 = 3,
+        }
+
+        internal enum ConversionType : byte
+        {
+            Standard = 0,
+            OneShot = 1
+        }
+
+        internal enum ChannelState : byte
+        {
+            Enabled = 0,
+            StandBy = 1
+        }
+
+        internal enum ChannelMode : byte
+        {
+            A = 0,
+            B = 1
+        }
+
+        internal enum Registers : byte
+        {
+            CH_STANDBY = 0x00,
+            CH_MODE_A = 0x01,
+            CH_MODE_B = 0x02,
+            CH_MODE_SEL = 0x03,
+            PWR_MODE = 0x04,
+            GENERAL_CFG = 0x05,
+            DATA_CTRL = 0x06,
+            INTERFACE_CFG = 0x07,
+            BIST_CTRL = 0x08,
+            DEV_STATUS = 0x09,
+            REV_ID = 0x0A,
+            DEV_ID_MSB = 0x0B,
+            DEV_ID_LSB = 0x0C,
+            SW_REV_ID = 0x0D,
+            GPIO_CTRL = 0x0E,
+            GPIO_WR_DATA = 0x0F,
+            GPIO_RD_DATA = 0x10,
+            PRECHARGE_BUF_1 = 0x11,
+            PRECHARGE_BUF_2 = 0x12,
+            POS_REF_BUF = 0x13,
+            NEG_REF_BUF = 0x14,
+            //CH_OFFSET_1(ch)		=	(0x1E + (ch) * 3)           ,
+            //CH_OFFSET_2(ch)		=	(0x1F + (ch) * 3)           ,
+            //CH_OFFSET_3(ch)		=	(0x20 + (ch) * 3)           ,
+            //CH_GAIN_1(ch)		=	(0x36 + (ch) * 3)           ,
+            //CH_GAIN_2(ch)		=	(0x37 + (ch) * 3)           ,
+            //CH_GAIN_3(ch)		=	(0x38 + (ch) * 3)           ,
+            //CH_SYNC_OFFSET(ch)	=	(0x4E + (ch) * 3)           ,
+            DIAG_METER_RX = 0x56,
+            DIAG_CTRL = 0x57,
+            DIAG_MOD_DELAY_CTRL = 0x58,
+            DIAG_CHOP_CTRL = 0x59,
+        }
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/ICs.ADC.Ad7768/Driver/Ad7768.cs
@@ -0,0 +1,147 @@
+using Meadow.Hardware;
+using Meadow.Units;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Meadow.Foundation.ICs.ADC
+{
+    public partial class Ad7768 : PollingSensorBase<Voltage>, ISpiPeripheral
+    {
+        private ISpiBus spiBus;
+        private bool portsCreated = false;
+        private IDigitalOutputPort? csPort;
+        private IDigitalOutputPort? resetPort;
+        private IDigitalInterruptPort? dataReadyPort;
+
+        /// <inheritdoc/>
+        public SpiClockConfiguration.Mode DefaultSpiBusMode => SpiClockConfiguration.Mode.Mode3;
+        /// <inheritdoc/>
+        public Frequency DefaultSpiBusSpeed => 40_000_000.Hertz();
+
+        public Ad7768(
+            ISpiBus spiBus,
+            IPin? chipSelectPin,
+            IPin? resetPin,
+            IPin? dataReadyPin)
+        {
+            this.spiBus = spiBus;
+
+            if (chipSelectPin != null)
+            {
+                csPort = chipSelectPin.CreateDigitalOutputPort(true); // start high (disabled)
+            }
+            if (resetPin != null)
+            {
+                // this has an internal pull up.  Drive it low to reset (> 2ms)
+                resetPort = resetPin.CreateDigitalOutputPort(true);
+            }
+            if (dataReadyPin != null)
+            {
+                // also active low
+                dataReadyPort = dataReadyPin.CreateDigitalInterruptPort(InterruptMode.EdgeFalling, ResistorMode.InternalPullUp);
+            }
+
+            portsCreated = true;
+
+            Initialize();
+        }
+
+        private void WriteRegisterByte(Registers register, byte data)
+        {
+            Span<byte> buffer = stackalloc byte[2];
+            // high bit indicates read/write (off == write)
+            buffer[0] = (byte)(0x7f & (byte)register);
+            buffer[1] = data;
+            spiBus.Exchange(csPort, buffer, buffer);
+        }
+
+        private void WriteRegisterByte(Registers register, Mask mask, byte data)
+        {
+            WriteRegisterByte(register, (byte)mask, data);
+        }
+
+        private void WriteRegisterByte(Registers register, byte mask, byte data)
+        {
+            var value = ReadRegisterByte(register);
+            value &= (byte)~mask;
+            value |= data;
+            WriteRegisterByte(register, value);
+        }
+
+        private byte ReadRegisterByte(Registers register)
+        {
+            Span<byte> buffer = stackalloc byte[2];
+            // high bit indicates read/write (on == read)
+            buffer[0] = (byte)(0x80 | (0x7f & (byte)register));
+            spiBus.Exchange(csPort, buffer, buffer);
+
+            return buffer[1];
+        }
+
+        private byte ReadRegisterByte(Registers register, byte mask)
+        {
+            var value = ReadRegisterByte(register);
+            return (byte)(value & mask);
+        }
+
+        private void SetSleepMode(SleepMode sleepMode)
+        {
+            WriteRegisterByte(Registers.PWR_MODE, Mask.SleepMode, (byte)sleepMode);
+        }
+
+        private void ResetChip()
+        {
+            if (resetPort != null)
+            {
+                resetPort.State = false;
+                Thread.Sleep(5);
+                resetPort.State = true;
+                Thread.Sleep(10);
+            }
+        }
+
+        private void Initialize()
+        {
+            ResetChip();
+            SetSleepMode(SleepMode.Active);
+            // SetMClck(
+            // SetCrcSelection(
+            // SetPowerMode(
+            // SetDClk(
+            // SetConversion
+            // SetOperatingMode
+
+            var channels = 4;
+
+            for (var channel = 0; channel < channels; channel++)
+            {
+                // SetChannelState(
+                // SetCoupling(
+                // SetIepe(
+            }
+        }
+
+
+
+
+        /// <inheritdoc/>
+        public Frequency SpiBusSpeed
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+
+        public SpiClockConfiguration.Mode SpiBusMode
+        {
+            get => spiBus.Configuration.SpiMode;
+            set => throw new NotSupportedException();
+        }
+
+        protected override Task<Voltage> ReadSensor()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
