ad20b59ed923a9f7fa09c06c8e840dbfee6e63f1
Chris Tacke <ctacke@opennetcf.com>
2024-11-18 20:47:24 -0600

    basic keller sim

--- Source/Meadow.Foundation.Peripherals/Sensors.Environmental.Keller.XLine/Driver/SimulatedKellerTransducer.cs
+++ Source/Meadow.Foundation.Peripherals/Sensors.Environmental.Keller.XLine/Driver/SimulatedKellerTransducer.cs
@@ -1,5 +1,4 @@
 ﻿using Meadow.Units;
-using System;
 using System.Threading.Tasks;
 
 namespace Meadow.Foundation.Sensors.Environmental;
@@ -8,11 +7,11 @@ public class SimulatedKellerTransducer : IKellerTransducer
 {
     public Task<Pressure> ReadPressure(PressureChannel channel)
     {
-        throw new NotImplementedException();
+        return Task.FromResult(new Pressure(123, Pressure.UnitType.Millibar));
     }
 
     public Task<Units.Temperature> ReadTemperature(TemperatureChannel channel)
     {
-        throw new NotImplementedException();
+        return Task.FromResult(new Units.Temperature(42.1, Units.Temperature.UnitType.Fahrenheit));
     }
 }
