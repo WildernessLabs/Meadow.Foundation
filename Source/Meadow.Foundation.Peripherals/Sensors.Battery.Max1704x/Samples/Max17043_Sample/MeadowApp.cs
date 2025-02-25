28a810735112d596b5ffc909f96a7416c456f7c5
Chris Tacke <ctacke@opennetcf.com>
2025-02-25 10:33:41 -0600

    MAX1704x driver

---
+++ Source/Meadow.Foundation.Peripherals/Sensors.Battery.Max1704x/Samples/Max17043_Sample/MeadowApp.cs
@@ -0,0 +1,35 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Sensors.Battery;
+using System.Threading.Tasks;
+
+namespace Sensors.Battery.Max17043_Sample
+{
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        //<!=SNIP=>
+
+        private Max17043 sensor;
+
+        public override Task Initialize()
+        {
+            Resolver.Log.Info("Initializing...");
+
+            sensor = new Max17043(Device.CreateI2cBus());
+
+            return Task.CompletedTask;
+        }
+
+        public override async Task Run()
+        {
+            while (true)
+            {
+                Resolver.Log.Info($"Voltage: {sensor.ReadVoltage().Volts:N2}V ");
+                Resolver.Log.Info($"SoC: {sensor.ReadStateOfCharge():N0}% ");
+                await Task.Delay(1000);
+            }
+        }
+
+        //<!=SNOP=>
+    }
+}
