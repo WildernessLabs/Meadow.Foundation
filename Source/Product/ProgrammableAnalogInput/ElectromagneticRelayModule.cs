5b37b5ab165d02a4f25411190914330571bcea7e
Chris Tacke <ctacke@opennetcf.com>
2025-03-12 10:22:12 -0500

    programmable analog input

---
+++ Source/Product/ProgrammableAnalogInput/ElectromagneticRelayModule.cs
@@ -0,0 +1,12 @@
+﻿using Meadow.Hardware;
+
+namespace Meadow.Foundation;
+
+public partial class ProgrammableAnalogInputModule
+{
+    public ProgrammableAnalogInputModule(II2cBus bus)
+    {
+
+    }
+
+}
