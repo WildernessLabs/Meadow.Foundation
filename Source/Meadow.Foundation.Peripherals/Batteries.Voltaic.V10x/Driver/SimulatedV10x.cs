42660965c8f79fbf14b55a6cb1197afaf2d01c47
Adrian Stevens <adrian.stevens@gmail.com>
2026-03-11 14:40:22 -0700

    Add XML comments

--- Source/Meadow.Foundation.Peripherals/Batteries.Voltaic.V10x/Driver/SimulatedV10x.cs
+++ Source/Meadow.Foundation.Peripherals/Batteries.Voltaic.V10x/Driver/SimulatedV10x.cs
@@ -2,6 +2,9 @@
 
 namespace Meadow.Foundation.Batteries.Voltaic;
 
+/// <summary>
+/// Simulated implementation of the V10x battery controller for testing purposes
+/// </summary>
 public class SimulatedV10x : IV10x
 {
     private readonly Voltage _batteryVoltage;
@@ -12,6 +15,9 @@ public class SimulatedV10x : IV10x
     private readonly Temperature _environmentTemp;
     private readonly Temperature _controllerTemp;
 
+    /// <summary>
+    /// Creates a new SimulatedV10x instance with default simulated values
+    /// </summary>
     public SimulatedV10x()
     {
         _batteryVoltage = new Voltage(12.6, Voltage.UnitType.Volts);
@@ -23,17 +29,24 @@ public class SimulatedV10x : IV10x
         _controllerTemp = new Temperature(35.0, Temperature.UnitType.Celsius);
     }
 
+    /// <inheritdoc/>
     public Voltage BatteryVoltage => _batteryVoltage;
 
+    /// <inheritdoc/>
     public Voltage InputVoltage => _inputVoltage;
 
+    /// <inheritdoc/>
     public Current InputCurrent => _inputCurrent;
 
+    /// <inheritdoc/>
     public Voltage LoadVoltage => _loadVoltage;
 
+    /// <inheritdoc/>
     public Current LoadCurrent => _loadCurrent;
 
+    /// <inheritdoc/>
     public Temperature EnvironmentTemp => _environmentTemp;
 
+    /// <inheritdoc/>
     public Temperature ControllerTemp => _controllerTemp;
 }
