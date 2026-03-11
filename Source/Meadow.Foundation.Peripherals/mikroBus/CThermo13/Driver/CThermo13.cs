3e8598894bc0454e95bec448558b975025dcc40c
Adrian Stevens <adrian.stevens@gmail.com>
2026-03-11 13:41:40 -0700

    Add mikrobus ctor

--- Source/Meadow.Foundation.Peripherals/mikroBus/CThermo13/Driver/CThermo13.cs
+++ Source/Meadow.Foundation.Peripherals/mikroBus/CThermo13/Driver/CThermo13.cs
@@ -1,7 +1,7 @@
 ﻿using Meadow.Foundation.Sensors.Atmospheric;
 using Meadow.Hardware;
 
-namespace Meadow.Foundation.mikroBUS
+namespace Meadow.Foundation.mikroBUS.Sensors.Atmospheric
 {
     /// <summary>
     /// Represents a mikroBUS Thermo 13 Click board
@@ -9,12 +9,19 @@ namespace Meadow.Foundation.mikroBUS
     public class CThermo13 : Bh1900Nux
     {
         /// <summary>
-        /// Creates a Thermo13 driver
+        /// Creates a CThermo13 driver
         /// </summary>
-        /// <param name="i2cBus"></param>
+        /// <param name="i2cBus">The I2C bus</param>
         public CThermo13(II2cBus i2cBus)
             : base(i2cBus, Addresses.Default)
-        {
-        }
+        { }
+
+        /// <summary>
+        /// Creates a CThermo13 driver using a MikroBus connector
+        /// </summary>
+        /// <param name="connector">The MikroBus connector</param>
+        public CThermo13(MikroBusConnector connector)
+            : base(connector.I2cBus, Addresses.Default)
+        { }
     }
 }
