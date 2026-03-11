bd5e05487bf3617c536707febea5c076a7c722f5
Adrian Stevens <adrian.stevens@gmail.com>
2026-03-11 13:39:56 -0700

    Minor cleanup

--- Source/Meadow.Foundation.Peripherals/mikroBus/C8800Retro/Driver/C8800Retro.cs
+++ Source/Meadow.Foundation.Peripherals/mikroBus/C8800Retro/Driver/C8800Retro.cs
@@ -20,6 +20,15 @@ namespace Meadow.Foundation.mikroBUS.Displays
         { }
 
         /// <summary>
+        /// Creates an Altair 8800 retro click board object using a MikroBus connector
+        /// </summary>
+        /// <param name="connector">The MikroBus connector</param>
+        /// <param name="address">The I2C address</param>
+        public C8800Retro(MikroBusConnector connector, byte address = 0)
+            : base(connector.I2cBus, connector.Pins.INT, address)
+        { }
+
+        /// <summary>
         /// Get the button for a given row and column
         /// </summary>
         /// <param name="column">The column of the button (1-4)</param>
@@ -27,52 +36,42 @@ namespace Meadow.Foundation.mikroBUS.Displays
         /// <returns>The IButton object</returns>
         public IButton GetButton(ButtonColumn column, ButtonRow row)
         {
-            KeyScanButtonType buttonType = KeyScanButtonType.None;
-
-            if (row == ButtonRow.A)
+            var buttonType = row switch
             {
-                buttonType = column switch
+                ButtonRow.A => column switch
                 {
                     ButtonColumn._1 => KeyScanButtonType.Button1,
                     ButtonColumn._2 => KeyScanButtonType.Button2,
                     ButtonColumn._3 => KeyScanButtonType.Button3,
                     ButtonColumn._4 => KeyScanButtonType.Button4,
                     _ => KeyScanButtonType.None
-                };
-            }
-            if (row == ButtonRow.B)
-            {
-                buttonType = column switch
+                },
+                ButtonRow.B => column switch
                 {
                     ButtonColumn._1 => KeyScanButtonType.Button5,
                     ButtonColumn._2 => KeyScanButtonType.Button6,
                     ButtonColumn._3 => KeyScanButtonType.Button7,
                     ButtonColumn._4 => KeyScanButtonType.Button8,
                     _ => KeyScanButtonType.None
-                };
-            }
-            if (row == ButtonRow.C)
-            {
-                buttonType = column switch
+                },
+                ButtonRow.C => column switch
                 {
                     ButtonColumn._1 => KeyScanButtonType.Button9,
                     ButtonColumn._2 => KeyScanButtonType.Button10,
                     ButtonColumn._3 => KeyScanButtonType.Button11,
                     ButtonColumn._4 => KeyScanButtonType.Button12,
                     _ => KeyScanButtonType.None
-                };
-            }
-            if (row == ButtonRow.D)
-            {
-                buttonType = column switch
+                },
+                ButtonRow.D => column switch
                 {
                     ButtonColumn._1 => KeyScanButtonType.Button13,
                     ButtonColumn._2 => KeyScanButtonType.Button14,
                     ButtonColumn._3 => KeyScanButtonType.Button15,
                     ButtonColumn._4 => KeyScanButtonType.Button16,
                     _ => KeyScanButtonType.None
-                };
-            }
+                },
+                _ => KeyScanButtonType.None
+            };
 
             return KeyScanButtons![buttonType];
         }
