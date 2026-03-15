e34b15cf24a7f10dca08489a92e0bb519bf82d3c
Adrian Stevens <adrian.stevens@gmail.com>
2026-03-15 14:43:01 -0700

    Cancel previous loop when calling StartUpdating a 2nd time

--- Source/Meadow.Foundation.Core/PollingSensorBase.cs
+++ Source/Meadow.Foundation.Core/PollingSensorBase.cs
@@ -32,7 +32,11 @@ namespace Meadow.Foundation
         {
             lock (samplingLock)
             {
-                if (IsSampling && updateInterval == UpdateInterval) { return; }
+                if (IsSampling)
+                {
+                    if (updateInterval == null || updateInterval == UpdateInterval) { return; }
+                    SamplingTokenSource?.Cancel();
+                }
 
                 IsSampling = true;
 
